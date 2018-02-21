59fa204e6df0febd59577b20930573a53b43b5f8
Joakim Lindquister <joakim.lindquister@bekk.no>
2018-02-21 09:43:11 +0100

    Oppsett med create-react-app

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import './App.css';
+
+const logo = require('./logo.svg');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Modia personoversikt</h1>
+        </header>
+        <p className="App-intro">
+          Velkommen til nye personoversikten!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
