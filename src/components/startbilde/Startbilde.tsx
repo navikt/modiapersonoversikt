fdd62dea79286f8781bf204a923d0b8f958a913e
Joakim Lindquister <joakim.lindquister@bekk.no>
2018-02-26 11:32:41 +0100

    Legg til manglende space

---
+++ src/components/startbilde/Startbilde.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+function Startbilde() {
+    return (
+        <div className="startbilde">
+            <button>Hent oppgave</button>
+        </div>
+    );
+}
+
+export default Startbilde;
--- src/components/visittkort/Visittkort.tsx
+++ src/components/visittkort/Visittkort.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Person} from "../../models/person";
+import { Person } from "../../models/person";
 
 interface VisittkortProps {
     person: Person
