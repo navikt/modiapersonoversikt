09cc28b82fd0b45aefaeff445355f686df04bc5e
Joakim Lindquister <joakim.lindquister@bekk.no>
2018-03-07 15:07:11 +0100

    Legg til ekspanderbart nedtrekkspanel for visittkort
    Det er dratt inn fra nav-frontend-moduler. Per nå så opplever jeg litt
    krøll med komponenten, sannsynligvis fordi den ikke er skrevet i React.
    InteliJ rapporterer at 'EkspanderbartpanelBase does not have any
    construct or call signature'. Det funker fint å kompilere og kjøre, men skriptet
    feiler ved hot-reload av visittkort-komponenten.
    
    Det kan være relatert til denne:
    https://github.com/Microsoft/TypeScript/issues/14558. Denne har blitt
    merget inn i master, men er ikke ute i en release enda.
    
    Det eksisterer også en PR i nav-frontend på å skrive om panelet til
    typecript som forhåpentligvis vil løse problemet:
    https://github.com/navikt/nav-frontend-moduler/pull/233

---
+++ src/@types/nav-frontend-ekspanderbartpanel.d.ts
@@ -0,0 +1,11 @@
+declare module 'nav-frontend-ekspanderbartpanel' {
+    import * as React from "react";
+
+    export interface EkspanderbartpanelProps {
+    }
+    export default class EkspanderbartPanel extends React.Component<EkspanderbartpanelProps, {}>{}
+
+    export class EkspanderbartpanelBase extends React.Component<EkspanderbartpanelProps, {}>{
+
+    }
+}
