29ba2619de40fa97ea7e6637e023d3539b236fc0
Joakim Lindquister <joakim.lindquister@bekk.no>
2018-03-14 10:42:30 +0100

    Fjern ubrukt kode

--- src/mock/utils.ts
+++ src/mock/utils.ts
@@ -41,86 +41,8 @@ export function respondWith(handler: (url: string, config: Config, request: obje
 }
 
 let seed = 42;
-export function seedRandom(newSeed: number) {
-    seed = newSeed;
-}
 
 export function random(): number {
     const x = Math.sin(seed++) * 100000;
     return x - Math.floor(x);
-}
-
-export function randRange(fromInclusive: number, toExclusive: number): number {
-    const range = toExclusive - fromInclusive;
-    return (Math.floor(random() * 10000) % range) + fromInclusive;
-}
-
-export function getSeedFromString(text: string): number {
-    return text.split('')
-        .map((c: string) => c.charCodeAt(0))
-        .reduce((sum: number, elem: number) => sum + elem, 0);
-}
-
-export function randomIssue(): string {
-    const validPrefixes = ['FO', 'PUS'];
-    const prefix = validPrefixes[randRange(0, validPrefixes.length)];
-    return `${prefix}-${randRange(1, 200)}`;
-}
-
-export function shuffleList<T>(elems: T[]): T[] {
-    const copy = [...elems];
-    const newList: T[] = [];
-    while (copy.length > 0) {
-        newList.push(copy.splice(randRange(0, copy.length), 1)[0]);
-    }
-    return newList;
-}
-
-export function randomMessage(): string {
-    const commitMessages = [
-        'Fix tslint issues',
-        'Ikke vis listevisning på veiledersiden',
-        'Reduserer antall søyler i diagrammet',
-        'Bruk slice i stede for splice',
-        'Bytter sorteringsfelt til aapunntakukerigjen',
-        'Sender med filter ved lasting av portefølje',
-        'Fikser sonarfeil',
-        'Bruker uker igjen i visningen av aap unntak',
-        'Legg til listevisning på min oversikt',
-        'Setter private=true i package.json',
-        'Adding UU-definition file',
-        'Må støtte at brukere kommer inn med ugyldig enhet i urlen',
-        'Oppdaterer lockfile',
-        'Legg til nyc',
-        'Fjerner camel-casing da solr-feltene er lowercase',
-        'Renaming - konsistent naming av aapUnntakDagerIgjen',
-        'Viser feilmodal når bytte av enhet feiler pga kontekstholder',
-        'Bugfiks der alertstripe er angitt med feil navn',
-        'Fjerner ubrukt import',
-        'Skriver litt flere tester',
-        'Fikser bug i utledning av status på arbeidserfaring',
-        'Må bruker Tom og ikke Fom for arbeidsperiode',
-        'Fikser diverse feil',
-        'la til policy som sjekker at vi har faste versjoner i package.json',
-        'Kunne finnt og fordele nye brukere',
-        'Legge til masse nye features',
-        'Ta i bruk masse feature-toggles',
-        'Slette masse feature-toggles',
-        'Legge til rette for digitale brukere',
-        'Gjøre masse designendinger',
-        'Legge til animasjoner',
-        'Fjerne en KVP-toggle'
-    ];
-    return commitMessages[randRange(0, commitMessages.length)];
-}
-
-export function randomAuthor(): string {
-    const authors = [
-        'Dummy the cat',
-        'Ole Brumm',
-        'Pusur',
-        'Donald Duck',
-        'Mikke Mus'
-    ];
-    return authors[randRange(0, authors.length)];
 }
