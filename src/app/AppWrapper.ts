f936a2969de036815287e0c61c9b609ca528ac90
Daniel Winsvold <daniel.winsvold@nav.no>
2018-04-05 08:26:56 +0200

    refaktorer styling og variabelnavn for ryddigere kode

--- src/app/AppWrapper.ts
+++ src/app/AppWrapper.ts
@@ -1,14 +1,9 @@
 import styled from 'styled-components';
 
 const AppWrapper = styled.div`
-  position: relative;
-  height: 0; // IE11-hack for at flexbox skal virke
-  min-height: 100vh;
+  height: 100vh;
   display: flex;
   flex-flow: column nowrap;
-  > #header {
-    flex-grow: 0;
-  }
 `;
 
 export default AppWrapper;
--- src/app/personside/MainLayout.tsx
+++ src/app/personside/MainLayout.tsx
@@ -1,58 +1,51 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import ComponentPlaceholder from '../../components/component-placeholder/ComponentPlaceHolder';
 import VisittkortContainer from './visittkort/VisittkortContainer';
 import Lameller from './lameller/Lameller';
 import DialogPanel from './dialogpanel/DialogPanel';
 
-const Wrapper = styled.div`
-        width: 100%;
-        flex-grow: 1;
-        @media (${props => props.theme.media.wideScreen}) {
-          display: flex;
-          flex-flow: row nowrap;
-        }
-        padding: ${props => props.theme.margin.layout};
-        overflow-y: scroll;
-    `;
+const LayoutWrapper = styled.div`
+    flex-grow: 1;
+    overflow-y: scroll;
+    padding: ${props => props.theme.margin.layout};
+    @media (${props => props.theme.media.wideScreen}) {
+      display: flex;
+      flex-flow: row nowrap;
+    }
+`;
 
-const PersonOversiktsPanel = styled.section`
-        margin-bottom: ${props => props.theme.margin.layout};
-        @media (${props => props.theme.media.wideScreen}) {
-          flex: 1 0.1 60%;
-          margin-right: ${props => props.theme.margin.layout};
-        }
-        > * {
-          box-shadow: ${props => props.theme.boxShadow.layout};
-          margin-bottom: ${props => props.theme.margin.layout};
-        }
-    `;
+const Kolonne = styled.section`
+    > * {
+      box-shadow: ${props => props.theme.boxShadow.layout};
+      margin-bottom: ${props => props.theme.margin.layout};
+      background-color: white;
+    }
+`;
 
-const DialogPanelWrapper = styled.section`
-        min-height: 700px;
-        display: flex;
-        flex-flow: column nowrap;
-        @media(${props => props.theme.media.wideScreen}) {
-          flex: 1 0.1 20%;
-        }        
-        > * {
-          box-shadow: ${props => props.theme.boxShadow.layout};
-          background-color: white;
-        }
-    `;
+const VenstreKolonne = Kolonne.extend`
+    @media (${props => props.theme.media.wideScreen}) {
+      flex: 1 0.1 60%;
+      margin-right: ${props => props.theme.margin.layout};
+    }
+`;
+
+const HøyreKolonne = Kolonne.extend`
+    @media(${props => props.theme.media.wideScreen}) {
+      flex: 1 0.1 20%;
+    }
+`;
 
 function MainLayout() {
     return (
-        <Wrapper>
-            <PersonOversiktsPanel>
-                <VisittkortContainer/>
-                <Lameller/>
-            </PersonOversiktsPanel>
-            <DialogPanelWrapper>
-                <DialogPanel/>
-                <ComponentPlaceholder height={'100%'} name={'Dialog Panel'}/>
-            </DialogPanelWrapper>
-        </Wrapper>
+        <LayoutWrapper>
+            <VenstreKolonne>
+                <VisittkortContainer />
+                <Lameller />
+            </VenstreKolonne>
+            <HøyreKolonne>
+                <DialogPanel />
+            </HøyreKolonne>
+        </LayoutWrapper>
     );
 }
 
