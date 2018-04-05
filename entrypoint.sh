dee3f534d0c0e3c6a594de5259385ae77b2c5f31
Eigil Skjæveland <eigil.skjeveland@nav.no>
2018-04-05 19:15:30 +0200

    Fiks entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/sh -e
 
 sed -i s,___HODE_URL___,$IADECORATOR_JS_URL, /usr/share/nginx/html/index.html
 sed -i s,___API_BASE_URI___,$PERSONOVERSIKTAPI_URL, /usr/share/nginx/html/index.html
