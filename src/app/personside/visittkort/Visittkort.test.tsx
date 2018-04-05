0bd28e89cc8b88ed31261072af13e946129fec18
Daniel Winsvold <daniel.winsvold@nav.no>
2018-04-05 12:05:55 +0200

    mer lesbare tester og lettere vedlikeholdbar kode med jest-enzyme

--- src/app/personside/visittkort/Visittkort.test.tsx
+++ src/app/personside/visittkort/Visittkort.test.tsx
@@ -3,12 +3,14 @@ import * as React from 'react';
 import Visittkort from './Visittkort';
 import { aremark } from '../../../mock/person-mock';
 import { mount } from 'enzyme';
+import VisittkortHeader from './header/VisittkortHeader';
+import VisittkortBody from './body/VisittkortBody';
 
 test('viser visittkortheader når visittkort først rendres', () => {
     const visittkort = mount(<Visittkort person={aremark} />);
 
-    expect(visittkort.find('VisittkortHeader').length).toEqual(1);
-    expect(visittkort.find('VisittkortBody').length).toEqual(0);
+    expect(visittkort).toContainReact(<VisittkortHeader person={aremark} />);
+    expect(visittkort).not.toContainReact(<VisittkortBody person={aremark} />);
 });
 
 test('viser visittkortheader og visitkortbody når visittkort åpnes med museklikk', () => {
@@ -16,6 +18,6 @@ test('viser visittkortheader og visitkortbody når visittkort åpnes med musekli
 
     visittkort.find('button.ekspanderbartPanel__hode').simulate('click');
 
-    expect(visittkort.find('VisittkortHeader').length).toEqual(1);
-    expect(visittkort.find('VisittkortBody').length).toEqual(1);
+    expect(visittkort).toContainReact(<VisittkortHeader person={aremark} />);
+    expect(visittkort).toContainReact(<VisittkortBody person={aremark} />);
 });
--- src/testSetup.ts
+++ src/testSetup.ts
@@ -1,4 +1,5 @@
 import { configure } from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
+import 'jest-enzyme';
 
 configure({ adapter: new Adapter() });
