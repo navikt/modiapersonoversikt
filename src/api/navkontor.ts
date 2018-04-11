e6749c2ef697dbed697036bd02a72d8b1f8acd2a
Daniel Winsvold <daniel.winsvold@nav.no>
2018-04-11 16:29:57 +0200

    skrive om api til ny backend

--- src/api/navkontor.ts
+++ src/api/navkontor.ts
@@ -1,8 +1,9 @@
 import { apiBaseUri } from './config';
 import { NavKontorInterface } from '../models/navkontor';
 
-export function getNavkontor(geografiskTilknytning: string): Promise<NavKontorInterface> {
-    const uri = `${apiBaseUri}/navkontor/${geografiskTilknytning}`;
+export function getNavkontor(geografiskTilknytning: string, diskresjonsKode?: string): Promise<NavKontorInterface> {
+    const uri =
+        `${apiBaseUri}/enheter/geo/${geografiskTilknytning}${diskresjonsKode ? '?dkode=' + diskresjonsKode : ''}`;
     return fetch(uri)
         .then((response) => response.json());
 }
