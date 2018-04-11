37719a1d644909b52c031be9e11aeb020e19b483
Daniel Winsvold <daniel.winsvold@nav.no>
2018-04-11 16:29:01 +0200

    oppdatere navkontormock til nytt interface

--- src/mock/navkontor-mock.ts
+++ src/mock/navkontor-mock.ts
@@ -1,8 +1,38 @@
-import { NavKontorInterface } from '../models/navkontor';
+import { KontaktInformasjon, NavKontorInterface } from '../models/navkontor';
+import { UkeDag } from '../models/ukedager';
+
+const mockKontaktInfo: KontaktInformasjon = {
+        gateAdresse: {
+            gatenavn: 'Islandsgate',
+            husnummer: '5',
+            husbokstav: 'A',
+            postnummer: '0658',
+            poststed: 'Oslo'
+        },
+        apningsTider: [
+            {
+                ukedag: UkeDag.LØRDAG,
+                apentFra: {
+                    time: '10',
+                    minutt: '30',
+                    sekund: '00'
+                },
+                apentTil: {
+                    time: '15',
+                    minutt: '30',
+                    sekund: '00'
+                }
+            }
+        ]
+    }
+;
 
 export function getNavKontor(geografiskTilknytning: string): NavKontorInterface {
     return {
-        navn: 'NAV Vålerenga',
-        nummer: '0666'
+        enhetNavn: 'Nav Vålerenga',
+        enhetId: '0666',
+        kontaktInformasjon: [
+            mockKontaktInfo
+        ]
     };
 }
