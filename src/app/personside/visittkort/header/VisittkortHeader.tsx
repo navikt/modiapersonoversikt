c083caa83469ddd63b8cbdbccf01684cfefec3d7
Joakim Lindquister <joakim.lindquister@bekk.no>
2018-04-13 15:09:44 +0200

    Fjern ubrukt import

--- src/app/personside/visittkort/header/VisittkortHeader.tsx
+++ src/app/personside/visittkort/header/VisittkortHeader.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import Undertittel from 'nav-frontend-typografi/lib/undertittel';
-import Undertekst from 'nav-frontend-typografi/lib/undertekst';
 import NavKontorContainer from './NavKontorContainer';
 
 import { erDød, Person } from '../../../../models/person';
