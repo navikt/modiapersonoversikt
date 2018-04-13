ca3998a78378c96bf87983791fb1c2374179be77
Joakim Lindquister <joakim.lindquister@nav.no>
2018-04-13 13:30:16 +0200

    Sjekk om kallet gikk bra før vi prøver å parse json

--- src/api/kontaktinformasjon-api.ts
+++ src/api/kontaktinformasjon-api.ts
@@ -4,5 +4,11 @@ import { apiBaseUri } from './config';
 export function getKontaktinformasjon(fodselsnummer: string): Promise<Kontaktinformasjon> {
     const uri = `${apiBaseUri}/person/${fodselsnummer}/kontaktinformasjon`;
     return fetch(uri, {credentials: 'include'})
-        .then((response) => response.json());
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw response.statusText;
+            }
+        });
 }
