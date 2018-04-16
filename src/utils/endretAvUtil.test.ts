ff04777e949342bca1480a1a553954a27414b542
Ketil Velle <ketil.s.velle@nav.no>
2018-04-16 14:42:37 +0100

    Fikset feil sjekk av pensjons-brukere for endretAv-funksjon.
    OPP-396

--- src/utils/endretAvUtil.test.ts
+++ src/utils/endretAvUtil.test.ts
@@ -1,35 +1,35 @@
 import { endretAvTekst } from './endretAvUtil';
 
 it('Formaterer endretAv tall + BD03 (brukerprofil-app = bruker) tekst til ønsket visningsformat', () => {
-    const rawString = '1010800 BD03';
+    const rawString = '1010800, BD03';
     const formatertTekst =  endretAvTekst(rawString);
 
     expect(formatertTekst).toEqual('av bruker');
 });
 
 it('Formaterer endretAv tall + PP01 (PSelv-app - bruker) tekst til ønsket visningsformat', () => {
-    const rawString = '1010800 PP01';
+    const rawString = 'Srvpsel, PP01';
     const formatertTekst =  endretAvTekst(rawString);
 
     expect(formatertTekst).toEqual('av bruker');
 });
 
 it('Formaterer endretAv ident + BD06 (modiabrukerdialog-app - NAV) tekst til ønsket visningsformat', () => {
-    const rawString = 'X108000 BD06';
+    const rawString = 'X108000, BD06';
     const formatertTekst =  endretAvTekst(rawString);
 
     expect(formatertTekst).toEqual('av NAV');
 });
 
 it('Formaterer endretAv ident + PP01 (modiabrukerdialog-app - NAV) tekst til ønsket visningsformat', () => {
-    const rawString = 'X108001 PP01';
+    const rawString = 'srvPensjon, PP01';
     const formatertTekst =  endretAvTekst(rawString);
 
     expect(formatertTekst).toEqual('av NAV');
 });
 
 it('Formaterer endretAv ident + FS21 (Gosys-app - NAV) tekst til ønsket visningsformat', () => {
-    const rawString = 'X108001 FS21';
+    const rawString = 'X108001, FS21';
     const formatertTekst =  endretAvTekst(rawString);
 
     expect(formatertTekst).toEqual('av NAV');
