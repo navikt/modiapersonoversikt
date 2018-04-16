c0dc7c8171e881de4448d1e0e1a545caf4e6c53a
Ketil Velle <ketil.s.velle@nav.no>
2018-04-16 12:52:42 +0100

    Fikset feil i endretAv-metode.
    OPP-396

--- src/utils/endretAvUtil.test.ts
+++ src/utils/endretAvUtil.test.ts
@@ -15,7 +15,14 @@ it('Formaterer endretAv tall + PP01 (PSelv-app - bruker) tekst til ønsket visni
 });
 
 it('Formaterer endretAv ident + BD06 (modiabrukerdialog-app - NAV) tekst til ønsket visningsformat', () => {
-    const rawString = '1010800 BD06';
+    const rawString = 'X108000 BD06';
+    const formatertTekst =  endretAvTekst(rawString);
+
+    expect(formatertTekst).toEqual('av NAV');
+});
+
+it('Formaterer endretAv ident + PP01 (modiabrukerdialog-app - NAV) tekst til ønsket visningsformat', () => {
+    const rawString = 'X108001 PP01';
     const formatertTekst =  endretAvTekst(rawString);
 
     expect(formatertTekst).toEqual('av NAV');
--- src/utils/endretAvUtil.ts
+++ src/utils/endretAvUtil.ts
@@ -1,7 +1,7 @@
 export function endretAvTekst(rawString: string): string {
     if (rawString.match('[0-9]{7} BD03') || rawString.match('[0-9]{7} PP01')) {
         return 'av bruker';
-    } else if (rawString.match('[0-9]{7} BD06') || rawString.match('[A-Z]{1}[0-9]{7} PP01') ) {
+    } else if (rawString.match('[A-Z]{1}[0-9]{6} BD06') || rawString.match('[A-Z]{1}[0-9]{6} PP01') ) {
         return 'av NAV';
     } else if (rawString.match('SKD')) {
         return 'i Folkeregisteret';
