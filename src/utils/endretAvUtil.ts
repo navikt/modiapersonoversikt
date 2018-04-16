f115baff5b89a49bb8e34a0afd6086daa2cb8eb6
Ketil Velle <ketil.s.velle@nav.no>
2018-04-16 13:20:40 +0100

    Forenklet endretAv-sjekk ved å bare sjekke etter ident eller tall.
    Dermed er vi mer sikkre mot endringer og konsumenter vi ikke kjenner til
    her og nå.
    
    OPP-396

--- src/utils/endretAvUtil.ts
+++ src/utils/endretAvUtil.ts
@@ -1,11 +1,18 @@
 export function endretAvTekst(rawString: string): string {
-    if (rawString.match('[0-9]{7} BD03') || rawString.match('[0-9]{7} PP01')) {
+    if (endretAvBruker(rawString)) {
         return 'av bruker';
-    } else if (rawString.match('[A-Z]{1}[0-9]{6} BD06') || rawString.match('[A-Z]{1}[0-9]{6} FS21') || rawString.match('[A-Z]{1}[0-9]{6} PP01') ) {
+    } else if (endretIFagsystem(rawString) ) {
         return 'av NAV';
     } else if (rawString.match('SKD')) {
         return 'i Folkeregisteret';
     } else {
         return rawString;
     }
+}
+
+function endretIFagsystem(rawString: string) {
+    return rawString.match('[A-Z][0-9]{6}');
+}
+function endretAvBruker(rawString: string) {
+    return rawString.match('[0-9]{7}');
 }
