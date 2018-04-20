05649c321f42a0b12e85f08ef104364959ac07c1
Joakim Lindquister <joakim.lindquister@bekk.no>
2018-04-20 09:00:09 +0200

    Bruk riktig casing på statsborgerskap

--- src/app/personside/visittkort/header/status/Statsborgerskap.tsx
+++ src/app/personside/visittkort/header/status/Statsborgerskap.tsx
@@ -4,13 +4,28 @@ interface Props {
     statsborgerskap?: string;
 }
 
-export function Statsborgerskap({statsborgerskap}: Props) {
+function uppercaseFørsteBokstav(ord: string) {
+    return ord.substring(0, 1).toUpperCase() + ord.substring(1, ord.length);
+}
+
+function formaterMedRiktigCasing(statsborgerskap: string): string {
+    return statsborgerskap.toLowerCase()
+        .split(' ').map(uppercaseFørsteBokstav).join(' ')
+        .split('-').map(uppercaseFørsteBokstav).join('-');
+}
+
+function Statsborgerskap({statsborgerskap}: Props) {
     if (!statsborgerskap) {
         return (
             <>Ingen statsborgerskap registrert i NAV</>
         );
     }
+
+    const formatertStatsborgerskap = formaterMedRiktigCasing(statsborgerskap);
+
     return (
-        <>{statsborgerskap}</>
+        <>{formatertStatsborgerskap}</>
     );
 }
+
+export default Statsborgerskap;
