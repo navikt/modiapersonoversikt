3b6d27ac4ac9c16dc367b8c8935b0e7b0f0ad15f
Joakim Lindquister <joakim.lindquister@bekk.no>
2018-05-03 12:36:49 +0200

    Feilhåndtering når person-kallet feiler

--- src/api/person-api.ts
+++ src/api/person-api.ts
@@ -4,5 +4,11 @@ import { apiBaseUri } from './config';
 export function getPerson(fodselsnummer: string): Promise<Person> {
     const uri = `${apiBaseUri}/person/${fodselsnummer}`;
     return fetch(uri, {credentials: 'include'})
-        .then((response) => response.json());
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw response.statusText;
+            }
+        });
 }
