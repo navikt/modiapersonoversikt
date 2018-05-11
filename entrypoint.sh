5ca457a13a7c16b548d0c1c5e9c97177cc6d258e
Joakim Lindquister <joakim.lindquister@bekk.no>
2018-05-11 18:55:19 +0200

    Use port from environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 
 sed -i s,%REACT_APP_HODE_URL%,$IADECORATOR_JS_URL, /usr/share/nginx/html/modiapersonoversikt/index.html
 sed -i s,%REACT_APP_MODIA_URL%,$PERSONOVERSIKTAPI_URL, /usr/share/nginx/html/modiapersonoversikt/index.html
+sed -i s,%DOCKER_PORT%,$PORT, /etc/nginx/conf.d/default.conf
 
 exec "$@"
