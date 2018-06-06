8340d3da2ceac05ef1466655a51296fb15fd2bca
Daniel Winsvold <daniel.winsvold@nav.no>
2018-06-06 09:51:16 +0200

    oppdatere snapshot

--- src/svg/Sikkerhetstiltak.js
+++ src/svg/Sikkerhetstiltak.js
@@ -6,11 +6,11 @@ export default function Sikkerhetstiltak(props) {
           <defs>
               <mask id="a">
                   <rect width="100%" height="100%" fill="#fff" />
-                  <line x1="50" y1="35" x2="50" y2="67" stroke="#000" stroke-width="4" stroke-linecap="round" />
+                  <line x1="50" y1="35" x2="50" y2="67" stroke="#000" strokeWidth="4" strokeLinecap="round" />
                   <circle cx="50" cy="82" r="4.5" fill="#000" />
               </mask>
           </defs>
-          <polygon mask="url(#a)" fill="#BA3A26" points="50,5 95,95 5,95" stroke-width="4" stroke-linejoin="round" stroke="#BA3A26" />
+          <polygon mask="url(#a)" fill="#BA3A26" points="50,5 95,95 5,95" strokeWidth="4" strokeLinejoin="round" stroke="#BA3A26" />
       </svg>
   );
 }
