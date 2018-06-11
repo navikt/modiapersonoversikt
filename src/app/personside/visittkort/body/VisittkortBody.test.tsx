ecd5145cab5cddf9812ae219a1a17881f6c55f4e
Daniel Winsvold <daniel.winsvold@nav.no>
2018-06-11 16:48:51 +0200

    setter opp en testStore med aremark i setupTests

--- src/app/personside/visittkort/body/VisittkortBody.test.tsx
+++ src/app/personside/visittkort/body/VisittkortBody.test.tsx
@@ -3,24 +3,12 @@ import * as renderer from 'react-test-renderer';
 import VisittkortBody from './VisittkortBody';
 import { testStore } from '../../../../setupTests';
 import { Provider } from 'react-redux';
-import { getMockNavKontor } from '../../../../mock/navkontor-mock';
-import { actionNames as navKontorActionNames } from '../../../../redux/navkontor';
-import { actionNames as egenAnsattActionNames } from '../../../../redux/egenansatt';
-import { actionNames as vergeMålActionNames } from '../../../../redux/vergemal';
-import { kontaktinformasjonActionNames } from '../../../../redux/kontaktinformasjon';
 import { aremark } from '../../../../mock/person/aremark';
 import { StaticRouter } from 'react-router';
 import { ThemeProvider } from 'styled-components';
 import { personOversiktTheme } from '../../../../themes/personOversiktTheme';
-import { getMockKontaktinformasjon } from '../../../../mock/kontaktinformasjon-mock';
-import { getEgenAnsatt } from '../../../../api/egenansatt';
-import { mockVergemal } from '../../../../mock/vergemal-mocks';
 
 test('viser info om bruker i visittkortbody', () => {
-    testStore.dispatch({ type: navKontorActionNames.OK, data: getMockNavKontor('0118', undefined) });
-    testStore.dispatch({ type: kontaktinformasjonActionNames.OK, data: getMockKontaktinformasjon('10108000398') });
-    testStore.dispatch({ type: egenAnsattActionNames.OK, data: getEgenAnsatt('10108000398') });
-    testStore.dispatch({ type: vergeMålActionNames.OK, data: mockVergemal('10108000398') });
 
     const visittkortbody = renderer.create(
         <Provider store={testStore}>
