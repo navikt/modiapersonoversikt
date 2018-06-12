04d42fd929df2608ccf42e2b86fbd72f60cb8d23
Joakim Lindquister <joakim.lindquister@bekk.no>
2018-06-12 15:31:28 +0200

    Fjern react-router-redux - den er deprecated
    Flytter litt på logikken rundt routing. Lager en Eventlistener-komponent
    på toppnivå som er ansvarlig for å reagere på events fra
    navigasjonsmenyen. Videre brukes history fra react-router til å
    pushe/endre URLen. Dette blir brukt for å holde styr på hvilken person
    som er søkt opp.

---
+++ src/app/Eventlistener.tsx
@@ -0,0 +1,54 @@
+import { RouteComponentProps, withRouter } from 'react-router';
+import { connect } from 'react-redux';
+import { History } from 'history';
+import * as React from 'react';
+
+import { paths, settPersonIKontekst } from './routes/routing';
+import { AppState } from '../redux/reducer';
+
+interface StateProps {
+    history: History;
+}
+
+type Props = StateProps & RouteComponentProps<{}> ;
+
+class Eventlistener extends React.Component<Props> {
+
+    constructor(props: Props) {
+        super(props);
+
+        this.handlePersonsok = this.handlePersonsok.bind(this);
+        this.fjernPersonFraContext = this.fjernPersonFraContext.bind(this);
+    }
+
+    componentDidMount() {
+        document.addEventListener('dekorator-hode-fjernperson', this.fjernPersonFraContext);
+        document.addEventListener('dekorator-hode-personsok', this.handlePersonsok);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('dekorator-hode-fjernperson', this.fjernPersonFraContext);
+        document.removeEventListener('dekorator-hode-personsok', this.handlePersonsok);
+    }
+
+    handlePersonsok(event: object) {
+        const personsokEvent = event as DecoratorPersonsokEvent;
+        settPersonIKontekst(this.props.history, personsokEvent.fodselsnummer);
+    }
+
+    fjernPersonFraContext() {
+        this.props.history.push(`${paths.basePath}`);
+    }
+
+    render() {
+        return null;
+    }
+}
+
+function mapStateToProps(state: AppState, routeProps: RouteComponentProps<{}>): StateProps {
+    return {
+        history: routeProps.history
+    };
+}
+
+export default withRouter(connect(mapStateToProps, null) (Eventlistener));
--- src/redux/oppgaver.ts
+++ src/redux/oppgaver.ts
@@ -1,22 +1,11 @@
 import { plukkOppgaveFraServer } from '../api/oppgave-api';
 import { createActionsAndReducer } from './restReducer';
 import { Oppgave } from '../models/oppgave';
-import { settNyPersonIKontekst } from '../app/routes/routing';
-import { Dispatch } from 'react-redux';
-import { Action } from 'redux';
 
-const { reducer, action} = createActionsAndReducer('oppgave');
+const { reducer, action} = createActionsAndReducer<Oppgave[]>('oppgave');
 
-export function plukkOppgaver(dispatch: Dispatch<Action>, enhet: string, temagruppe: string) {
-    dispatch(action(() => plukkOppgaveFraServer(enhet, temagruppe)))
-        .then((oppgaver: Oppgave[]) => hentPersonBasertPåPlukketOppgaver(dispatch, oppgaver ));
-}
-
-function hentPersonBasertPåPlukketOppgaver(dispatch: Dispatch<Action>, oppgaver: Oppgave[]) {
-    const fødselsnummer = selectFodselsnummerfraOppgaver(oppgaver);
-    if (fødselsnummer) {
-        settNyPersonIKontekst(dispatch, fødselsnummer);
-    }
+export function plukkOppgaver(enhet: string, temagruppe: string) {
+    return action(() => plukkOppgaveFraServer(enhet, temagruppe));
 }
 
 export function selectFodselsnummerfraOppgaver(oppgaver: Oppgave[]) {
