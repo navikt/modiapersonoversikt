b47ea7f48766683ea09c2513763449a107813c1f
Joakim Lindquister <joakim.lindquister@bekk.no>
2018-06-26 10:41:48 +0200

    Trekk vanlige validator-regler ut i en egen util-fil
    Codeclimate klagde på duplicate-code. Flytter derfor ut vanlige regler
    ut i en funksjon.

--- src/app/brukerprofil/adresse/midlertidigAdresseNorge/matrikkeladresse/matrikkeladresseValidator.ts
+++ src/app/brukerprofil/adresse/midlertidigAdresseNorge/matrikkeladresse/matrikkeladresseValidator.ts
@@ -1,21 +1,13 @@
 import { Matrikkeladresse } from '../../../../../models/personadresse';
-import FormValidator, { Valideringsregel } from '../../../../../utils/forms/FormValidator';
-import { erIkkeTomStreng, erTall } from '../../../../../utils/string-utils';
+import FormValidator from '../../../../../utils/forms/FormValidator';
+import { lagErIkkeTomtFeltRegel, lagPostnummerRegel } from '../../../../../utils/forms/commonValidatorRegler';
 
-const eiendomsnavnRegel: Valideringsregel<Matrikkeladresse> = {
-    felt: 'eiendomsnavn',
-    feilmelding: 'Områdeadresse kan ikke være tom',
-    validator: (matrikkeladresse: Matrikkeladresse) =>
-        matrikkeladresse.eiendomsnavn ? erIkkeTomStreng(matrikkeladresse.eiendomsnavn) : false
-};
+const eiendomsnavnRegel = lagErIkkeTomtFeltRegel<Matrikkeladresse>(
+    'eiendomsnavn',
+    (matrikkeladresse) => matrikkeladresse.eiendomsnavn ? matrikkeladresse.eiendomsnavn : '',
+    'Områdeadresse kan ikke være tom');
+const postnummerRegel = lagPostnummerRegel('postnummer', matrikkeladresse => matrikkeladresse.postnummer );
 
-const postnummerRegel: Valideringsregel<Matrikkeladresse> = {
-    felt: 'postnummer',
-    feilmelding: 'Postnummer må ha fire tall',
-    validator: (matrikkeladresse: Matrikkeladresse) =>
-        erTall(matrikkeladresse.postnummer) && matrikkeladresse.postnummer.length === 4
-};
-
-export function validerMatrikkeladresse(gateadresse: Matrikkeladresse) {
-    return new FormValidator<Matrikkeladresse>([eiendomsnavnRegel, postnummerRegel]).valider(gateadresse);
+export function validerMatrikkeladresse(matrikkeladresse: Matrikkeladresse) {
+    return new FormValidator<Matrikkeladresse>([eiendomsnavnRegel, postnummerRegel]).valider(matrikkeladresse);
 }
