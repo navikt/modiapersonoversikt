d4df59b63fccada97d1c59db6ab2a47a08ccf741
Daniel Winsvold <daniel.winsvold@nav.no>
2018-07-04 09:31:00 +0200

    styling etikett mini for å funke i gamlemodia

--- src/components/EtikettMini.tsx
+++ src/components/EtikettMini.tsx
@@ -8,10 +8,11 @@ interface Props {
 }
 
 const FontSize = styled.span`
-    font-size: 0.7rem;
+    // Må sette font-size hardt her for at det skal funke i gamlemodia
+    font-size: 11px;
 `;
 
-const Wrapper = styled.span`
+const Wrapper = styled.div`
     margin-top: 3px;
     .typo-etikett-liten {
       line-height: 1rem;
