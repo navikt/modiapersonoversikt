39e7bfe4629142027539f73a58c1f4772ff43215
Ketil Velle <ketil.velle@bekk.no>
2018-07-13 08:01:32 +0200

    Satt maks lengde 4 på postboksnummer, TPS gjør rare ting dersom denne er lengre enn 4.

--- src/app/brukerprofil/adresse/midlertidigAdresseNorge/postboksadresse/postboksadresseValidator.ts
+++ src/app/brukerprofil/adresse/midlertidigAdresseNorge/postboksadresse/postboksadresseValidator.ts
@@ -1,13 +1,17 @@
 import { Postboksadresse } from '../../../../../models/personadresse';
 import FormValidator from '../../../../../utils/forms/FormValidator';
 import {
-    datoErGyldigValidatorRegel, lagDatoErInnenEtÅrRegel, lagErIkkeTomtFeltRegel,
+    datoErGyldigValidatorRegel, lagDatoErInnenEtÅrRegel, lagErIkkeTomtFeltRegel, lagPostboksnummerRegel,
     lagPostnummerRegel
 } from '../../../../../utils/forms/commonValidatorRegler';
 
-const postboksnummerRegel = lagErIkkeTomtFeltRegel(
+const postboksnummerObligatoriskRegel = lagErIkkeTomtFeltRegel(
     'postboksnummer',
     postboksadresse => postboksadresse.postboksnummer, 'Postboksnummer kan ikke være tom');
+
+const postboksnummerLengdeRegel = lagPostboksnummerRegel(
+    'postboksnummer',
+    postboksadresse => postboksadresse.postboksnummer);
 
 const datoGyldigRegel = datoErGyldigValidatorRegel<Postboksadresse>('periode', postboksadresse =>
     postboksadresse.periode ? postboksadresse.periode.til : '');
@@ -16,10 +20,17 @@ const datoErIfremtidenRegel = lagDatoErInnenEtÅrRegel<Postboksadresse>('periode
     postboksadresse.periode ? postboksadresse.periode.til : '');
 
 const postnummerRegel = lagPostnummerRegel('postnummer', postboksadresse =>
-    postboksadresse.postnummer );
+    postboksadresse.postnummer);
 
 export function validerPostboksadresse(postboksadresse: Postboksadresse) {
-    const regler = [postboksnummerRegel, postnummerRegel, datoGyldigRegel, datoErIfremtidenRegel];
+    const regler =
+        [
+            postboksnummerObligatoriskRegel,
+            postboksnummerLengdeRegel,
+            postnummerRegel,
+            datoGyldigRegel,
+            datoErIfremtidenRegel
+        ];
     return new FormValidator<Postboksadresse>(regler).valider(postboksadresse);
 }
 
