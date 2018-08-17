d9fa9877edd79ff5e88a701663fd1993077a0c9b
Richard Borge <Richard.Borge@nav.no>
2018-08-17 13:18:51 +0200

    Mock data

--- src/models/utbetalinger.ts
+++ src/models/utbetalinger.ts
@@ -17,8 +17,33 @@ export interface Utbetaling {
 
 export interface Ytelse {
     type: string;
+    ytelseskomponentListe?: Ytelseskomponent[];
+    ytelseskomponentersum: number;
+    trekkListe?: Trekk[];
+    trekksum: number;
+    skattListe?: Skatt[];
+    skattsum: number;
     nettobeløp: number;
     periode: YtelsePeriode;
+    bilagsnummer?: string;
+}
+
+export interface Ytelseskomponent {
+    ytelseskomponenttype: string,
+    satsbeløp?: number,
+    satstype?: string,
+    satsantall?: number,
+    ytelseskomponentbeløp: number
+}
+
+export interface Trekk {
+    trekktype: string;
+    trekkbeløp: number;
+    kreditor?: string;
+}
+
+export interface Skatt {
+    skattebeløp: number;
 }
 
 export interface YtelsePeriode {
