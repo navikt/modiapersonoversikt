1ada0aff452a00eb5205da4c5d5572d598b1682d
Daniel Winsvold <danielwinsvold@gmail.com>
2018-09-07 14:35:01 +0200

    deler opp i år for ytelsesperiode i sammendragene i totalt-utbetlat detaljer

--- src/app/personside/infotabs/utbetalinger/TotaltUtbetaltDetaljer.tsx
+++ src/app/personside/infotabs/utbetalinger/TotaltUtbetaltDetaljer.tsx
@@ -16,6 +16,8 @@ import {
 import { formaterDato } from '../../../../utils/dateUtils';
 import { Fragment } from 'react';
 import ErrorBoundary from '../../../../components/ErrorBoundary';
+import * as moment from 'moment';
+import { sorterAlfabetisk } from '../../../../utils/string-utils';
 
 const Wrapper = styled.div`
   margin: 1.5rem 0 .5rem;
@@ -122,13 +124,19 @@ function getAlleUtbetalteYtelserFraUtbetalinger(utbetalinger: Utbetaling[]) {
     return reduceUtbetlingerTilYtelser(utbetalteUtbetalinger);
 }
 
+function getTypeOgÅrFromYtelse(ytelse: Ytelse): string {
+    return getTypeFromYtelse(ytelse) + ' ' + moment(ytelse.periode.slutt).year();
+}
+
 function getYtelserSammendrag(utbetalinger: Utbetaling[]) {
     const alleUtbetalteYtelser: Ytelse[] = getAlleUtbetalteYtelserFraUtbetalinger(utbetalinger);
     const ytelserGruppertPåTema = groupArray(
         alleUtbetalteYtelser,
-        getTypeFromYtelse
+        getTypeOgÅrFromYtelse
     );
-    const ytelsesSammendrag = ytelserGruppertPåTema.map(gruppe => {
+    const ytelsesSammendrag = ytelserGruppertPåTema
+        .sort((a, b) => sorterAlfabetisk(a.category, b.category))
+        .map(gruppe => {
         const ytelser = gruppe.array;
         const ytelsesType = gruppe.category;
         const periode = getPeriodeFromYtelser(ytelser);
