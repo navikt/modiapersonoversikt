24e56d54ea5cdf53da6e339360c128e3934a6135
Richard Borge <Richard.Borge@nav.no>
2018-09-27 13:46:03 +0200

    Javascriptifisert svg-er

---
+++ src/svg/DokumentIkkeTilgangMerket.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+function DokumentIkkeTilgangMerket(props) {
+    return (
+        <svg viewBox="0 0 24 24" xmlns='http://www.w3.org/2000/svg' {...props}>
+            <g fill='#0067c5'>
+                <path d='M17 10V5.5l-.1-.4-5-5-.4-.1H.5C.2 0 0 .2 0 .5v21c0 .3.2.5.5.5h11A7.5 7.5 0 0 1 17 10zM11.5.5l5 5h-5v-5z'/>
+                <path d='M17.5 11a6.5 6.5 0 0 0-5 10.7l9.2-9.1a6.5 6.5 0 0 0-4.2-1.6zM22.4 13.3l-9.1 9.1c1.1 1 2.6 1.6 4.2 1.6a6.5 6.5 0 0 0 5-10.7z'/>
+            </g>
+        </svg>
+    );
+}
+
+export default DokumentIkkeTilgangMerket;
