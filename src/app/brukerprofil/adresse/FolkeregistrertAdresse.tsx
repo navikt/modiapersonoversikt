9a322dcb12960ad499d25b95db3d15dd57f32650
Richard Borge <Richard.Borge@nav.no>
2018-10-09 11:26:03 +0200

    Lagt til visning av både bostedsadresse og postadresse

--- src/app/brukerprofil/adresse/FolkeregistrertAdresse.tsx
+++ src/app/brukerprofil/adresse/FolkeregistrertAdresse.tsx
@@ -2,10 +2,32 @@ import * as React from 'react';
 
 import { formatterRiktigAdresse } from '../../personside/visittkort/body/kontaktinformasjon/adresse/Adresse';
 import { Person } from '../../../models/person/person';
+import Element from 'nav-frontend-typografi/lib/element';
+import Normaltekst from 'nav-frontend-typografi/lib/normaltekst';
+import styled from 'styled-components';
+
+const AdresseWrapper = styled.div`
+    padding-top: 1rem;
+`;
 
 export default function FolkeregistrertAdresse({person}: {person: Person}) {
     const adresse = person.folkeregistrertAdresse ?
         formatterRiktigAdresse(person.folkeregistrertAdresse)
-        : 'Ikke registrert';
-    return <>{adresse}</>;
+        : <Normaltekst>Ikke registrert</Normaltekst>;
+    const post = person.postadresse ?
+        formatterRiktigAdresse(person.postadresse)
+        : <Normaltekst>Ikke registrert</Normaltekst>;
+
+    return (
+        <>
+            <AdresseWrapper>
+                <Element>Bostedsadresse fra folkeregisteret</Element>
+                {adresse}
+            </AdresseWrapper>
+            <AdresseWrapper>
+                <Element>Postadresse fra folkeregisteret</Element>
+                {post}
+            </AdresseWrapper>
+        </>
+    );
 }
