1a3a1ea975d7723e35375bfd60e257dad946cafc
Daniel Winsvold <danielwinsvold@gmail.com>
2018-10-12 16:06:58 +0200

    hurtigvalg for datovelger

--- src/components/forms/Datovelger.tsx
+++ src/components/forms/Datovelger.tsx
@@ -3,6 +3,9 @@ import NavDatovelger, { Avgrensninger } from 'nav-datovelger';
 import { SkjemaelementFeil } from 'nav-frontend-skjema/lib/skjemaelement-feilmelding';
 import { formaterTilISO8601Date } from '../../utils/dateUtils';
 import { Feilmelding } from '../../utils/Feilmelding';
+import styled from 'styled-components';
+import KnappBase from 'nav-frontend-knapper';
+import * as moment from 'moment';
 
 interface Props {
     dato: Date | undefined;
@@ -13,6 +16,15 @@ interface Props {
     innenEtÅr?: boolean;
 }
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  > *:not(:first-child) {
+    margin-bottom: .5rem;
+    margin-left: .5rem;
+  }
+`;
+
 export function tilPeriode(gyldigTil: Date) {
     return {
         fra: formaterTilISO8601Date(new Date()),
@@ -20,32 +32,39 @@ export function tilPeriode(gyldigTil: Date) {
     };
 }
 
-function getAvgrensninger(): Avgrensninger {
-    const iDag = new Date(Date.now());
+const omEtÅr = moment().add(1, 'year').toDate();
+const iMorgen = moment().add(1, 'day').toDate();
+const omEnUke = moment().add(1, 'week').toDate();
+const omTreMåneder = moment().add(3, 'month').toDate();
 
-    const iMorgen = new Date(iDag);
-    iMorgen.setDate(iDag.getDate() + 1);
-
-    const omEtÅr = new Date(iDag);
-    omEtÅr.setDate(iDag.getDate() + 365);
-
-    return {
-        minDato: iMorgen,
-        maksDato: omEtÅr
-    };
-}
+const avgrensninger: Avgrensninger = {
+    minDato: iMorgen,
+    maksDato: omEtÅr
+};
 
 export default function Datovelger({dato, id, onChange, feil, children, innenEtÅr}: Props) {
-    const avgrensninger: Avgrensninger | undefined = innenEtÅr ? getAvgrensninger() : undefined;
+
+    function handleHurtigvalg(newDate: Date) {
+        return (event: React.MouseEvent<HTMLButtonElement>) => {
+            event.preventDefault();
+            onChange(newDate);
+        };
+    }
+
     return (
         <>
             <label htmlFor={id} className={'skjemaelement__label'}>{children}</label>
-            <NavDatovelger
-                dato={dato}
-                id={id}
-                avgrensninger={avgrensninger}
-                onChange={onChange}
-            />
+            <Wrapper>
+                <NavDatovelger
+                    dato={dato}
+                    id={id}
+                    avgrensninger={innenEtÅr ? avgrensninger : undefined}
+                    onChange={onChange}
+                />
+                <KnappBase type="flat" mini={true} onClick={handleHurtigvalg(omEnUke)}>Om en uke</KnappBase>
+                <KnappBase type="flat" mini={true} onClick={handleHurtigvalg(omTreMåneder)}>Om tre måneder</KnappBase>
+                <KnappBase type="flat" mini={true} onClick={handleHurtigvalg(omEtÅr)}>Om et år</KnappBase>
+            </Wrapper>
             <Feilmelding feil={feil}/>
         </>
     );
