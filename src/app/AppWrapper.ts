e019610bb6a6946cb6f01dcfbfea0ad7cfe4608b
Jørund Amsen <jorund.amsen@bekk.no>
2018-10-26 12:50:27 +0200

    Liten fix så vanlig ctrl/cmd + P fungerer bedre

--- src/app/AppWrapper.ts
+++ src/app/AppWrapper.ts
@@ -3,6 +3,9 @@ import { theme } from '../styles/personOversiktTheme';
 
 const AppWrapper = styled.div`
   height: 100vh;
+  @media print {
+    height: auto;
+  }
   display: flex;
   flex-flow: column nowrap;
   background-color: ${theme.color.bakgrunn};
@@ -13,6 +16,9 @@ const AppWrapper = styled.div`
 
 export const Content = styled.div`
   height: 0; // IE11-hack for at flex skal funke
+  @media print {
+    height: auto;
+  }
   flex-grow: 1;
   display: flex;
   overflow: auto;
