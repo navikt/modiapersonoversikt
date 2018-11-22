a1a3e82b28e05c3314878247c1dec9e8bbe575db
Jørund Amsen <jorundamsen@nav.no>
2018-11-22 14:08:15 +0100

    Fikser litt formatering for Viktig å vite-tekstene
    Oppdaterer tester
    Oppdaterer package-lock

--- src/app/personside/infotabs/saksoversikt/viktigavite/DAGInnhold.tsx
+++ src/app/personside/infotabs/saksoversikt/viktigavite/DAGInnhold.tsx
@@ -8,7 +8,7 @@ export function ViktigÅViteDAGInnhold(): JSX.Element {
             <Normaltekst>
                 Vi forventer at du selv aktivt forsøker å skaffe deg jobb. Du må derfor søke jobber. Dersom du ikke
                 finner ledige stillinger innenfor det du ønsker å arbeide med, må du utvide jobbsøket ditt. Det gjør du
-                selv på Ditt NAV.
+                selv på Ditt NAV.&nbsp;
                 <a
                     href="https://tjenester.nav.no/stillinger/"
                     target="_blank"
@@ -46,7 +46,7 @@ export function ViktigÅViteDAGInnhold(): JSX.Element {
                 Er det andre områder du kan bruke kompetansen din på? Ikke la deg begrense av geografi eller bransje.
             </Normaltekst>
             <Normaltekst>
-                Hvilken jobb kan passe for deg?
+                Hvilken jobb kan passe for deg?&nbsp;
                 <a
                     href="https://www.nav.no/no/Person/Arbeid/Test+deg+for+jobb+og+utdanning"
                     target="_blank"
@@ -54,7 +54,7 @@ export function ViktigÅViteDAGInnhold(): JSX.Element {
                     Interessetester og kartleggingstester
                 </a>
             </Normaltekst>
-            <Normaltekst>Som hovedregel kan du ikke motta dagpenger under utdanning.
+            <Normaltekst>Som hovedregel kan du ikke motta dagpenger under utdanning.&nbsp;
                 <a
                     href="https://www.nav.no/no/Person/Arbeid/Dagpenger+ved+arbeidsl%C3%B8shet+og+permittering/
                     Relatert+informasjon/Dagpenger+og+utdanning.345930.cms"
@@ -76,7 +76,7 @@ export function ViktigÅViteDAGInnhold(): JSX.Element {
             <br/>
             <Undertittel>Dine plikter mens du mottar dagpenger</Undertittel>
             <Normaltekst>
-                <strong>Meldekort</strong> - du skal sende meldekort hver 14. dag.
+                <strong>Meldekort</strong> - du skal sende meldekort hver 14. dag.&nbsp;
                 <a
                     href="https://www.nav.no/no/Person/Arbeid/Dagpenger+ved+arbeidsl%C3%B8shet+og+permittering/
                     Informasjon+om+meldekort"
@@ -84,7 +84,8 @@ export function ViktigÅViteDAGInnhold(): JSX.Element {
                 >
                     Les mer om meldekort
                 </a>
-                <strong>Ferie</strong> - hvis du planlegger ferie eller frav ær, må du søke om det.
+                <br/>
+                <strong>Ferie</strong> - hvis du planlegger ferie eller frav ær, må du søke om det.&nbsp;
                 <a
                     href="https://www.nav.no/no/Person/Arbeid/Sykmeldt%2C+arbeidsavklaringspenger+og+yrkesskade/
                     Relatert+informasjon/Ferie+med+ytelser+fra+NAV.351695.cms"
@@ -92,8 +93,10 @@ export function ViktigÅViteDAGInnhold(): JSX.Element {
                 >
                     Les mer om ferie
                 </a>
-                <strong>Annet fravær eller endringer</strong>-du er selv ansvarlig for å gi beskjed til NAV om endringer
-                som har betydning for jobbsøkingen din.
+                <br/>
+                <strong>Annet fravær eller endringer</strong>-
+                du er selv ansvarlig for å gi beskjed til NAV om endringer
+                som har betydning for jobbsøkingen din.&nbsp;
                 <a
                     href="https://www.nav.no/no/Person/Flere+tema/Arbeid+og+opphold+i+utlandet/Nyttig+a+vite/
                     Du+har+plikt+til+%C3%A5+gi+NAV+riktige+opplysninger.351762.cms"
--- src/app/personside/infotabs/saksoversikt/viktigavite/INDInnhold.tsx
+++ src/app/personside/infotabs/saksoversikt/viktigavite/INDInnhold.tsx
@@ -7,7 +7,7 @@ export function ViktigÅViteINDInnhold(): JSX.Element {
             <Undertittel>Informasjon om meldekort</Undertittel>
             <Normaltekst>
                 Hvis du mottar tiltakspenger må du regelmessig holde NAV orientert om situasjonen din. Det gjør du ved å
-                fylle ut og sende inn meldekort hver 14. dag.
+                fylle ut og sende inn meldekort hver 14. dag.&nbsp;
                 <a
                     href="https://www-q1.nav.no/no/Person/Arbeid/Dagpenger+og+stonader+ved+arbeidsloshet/
                 Informasjon+om+meldekort"
@@ -20,7 +20,7 @@ export function ViktigÅViteINDInnhold(): JSX.Element {
             <Undertittel>Meld fra om endringer</Undertittel>
             <Normaltekst>
                 Hvis du får endringer i inntekt, familiesituasjon og/eller jobbsituasjon, eller planlegger opphold i
-                utlandet, kan det ha betydning for beløpet du får utbetalt fra NAV. I slike tilfeller må du derfor
+                utlandet, kan det ha betydning for beløpet du får utbetalt fra NAV. I slike tilfeller må du derfor&nbsp;
                 <a
                     href="https://www-q1.nav.no/no/Person/Flere+tema/Arbeid+og+opphold+i+utlandet/Nyttig+a+vite/
                 Du+har+plikt+til+%C3%A5+gi+NAV+riktige+opplysninger.351762.cms"
@@ -33,7 +33,7 @@ export function ViktigÅViteINDInnhold(): JSX.Element {
             <Undertittel>Jobbsøkertips</Undertittel>
             <Normaltekst>
                 Her finner du en oversikt med nyttige tips og råd til deg som skal søke jobb. Målet er å hjelpe deg
-                til å bli en god jobbsøker slik at du har større sjanser til å få en jobb du trives med.
+                til å bli en god jobbsøker slik at du har større sjanser til å få en jobb du trives med.&nbsp;
                 <a
                     href="https://www-q1.nav.no/no/Person/Arbeid/Arbeidsledig+og+jobbsoker/Jobbsokertips"
                     target="_blank"
@@ -45,7 +45,7 @@ export function ViktigÅViteINDInnhold(): JSX.Element {
             <Undertittel>Om stønader ved tiltak</Undertittel>
             <Normaltekst>
                 Arbeidsrettede tiltak er et virkemiddel for å styrke tiltaksdeltakerens muligheter til å få eller
-                beholde arbeid. Deltar du på et arbeidsrettet tiltak har du rett til enkelte økonomiske ytelser.
+                beholde arbeid. Deltar du på et arbeidsrettet tiltak har du rett til enkelte økonomiske ytelser.&nbsp;
                 <a
                     href="https://www-q1.nav.no/no/Person/Arbeid/Dagpenger+og+stonader+ved+arbeidsloshet/
                 St%C3%B8nader+ved+tiltak.1073747255.cms"
