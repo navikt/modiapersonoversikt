30c6adbe20d446b9f933c6a656b746a8df00b794
Daniel Winsvold <danielwinsvold@gmail.com>
2018-11-28 13:05:59 +0100

    innholdslaster i PlukkRestData

--- src/app/personside/infotabs/ytelser/pleiepenger/PlukkRestData.tsx
+++ src/app/personside/infotabs/ytelser/pleiepenger/PlukkRestData.tsx
@@ -1,18 +1,22 @@
 import * as React from 'react';
 import { ReactNode } from 'react';
 import { isLoaded, Loaded, RestReducer } from '../../../../../redux/restReducers/restReducer';
+import Innholdslaster, { InnholdslasterProps } from '../../../../../components/Innholdslaster';
 
-interface Props<T> {
+interface OwnProps<T> {
     restReducer: RestReducer<T>;
     children: (data: T) => ReactNode;
 }
 
+type Props<T> = OwnProps<T> & Pick<InnholdslasterProps, 'spinnerSize' | 'returnOnPending' | 'returnOnError'>;
+
 class PlukkRestData<T> extends React.Component<Props<T>> {
     render() {
-        if (!isLoaded(this.props.restReducer)) {
-            return 'Not loaded';
+        const {children, restReducer, ...innholdsLasterProps } = this.props;
+        if (!isLoaded(restReducer)) {
+            return <Innholdslaster avhengigheter={[this.props.restReducer]} {...innholdsLasterProps}/>;
         }
-        return this.props.children((this.props.restReducer as Loaded<T>).data);
+        return children((restReducer as Loaded<T>).data);
     }
 }
 
