e3ddee63d0a8a9301093afce43ce92f2cd93f407
Daniel Winsvold <danielwinsvold@gmail.com>
2018-12-19 13:41:04 +0100

    mulighet for å velge å ikke se et tooltips igjen

--- src/components/tooltip/ToolTip.tsx
+++ src/components/tooltip/ToolTip.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { Normaltekst } from 'nav-frontend-typografi';
+import { Normaltekst, Undertekst } from 'nav-frontend-typografi';
 import { Bold } from '../common-styled-components';
 import ShowThenHide from '../ShowThenHide';
+import { removeWhitespace } from '../../utils/string-utils';
+import * as Cookies from 'js-cookie';
 
 interface Props {
     children: string;
+}
+
+interface State {
+    show: boolean;
 }
 
 const Style = styled.div`
@@ -22,19 +28,66 @@ const Style = styled.div`
   opacity: .8;
   pointer-events: none;
   z-index: 1000;
+  text-align: right;
 `;
 
-function ToolTip(props: Props) {
-    if (props.children === '' || !props.children) {
-        return null;
+const Button = styled.button`
+  background-color: transparent;
+  border: none;
+  color: white;
+  pointer-events: auto;
+  > *:hover {
+    text-decoration: underline;
+  }
+`;
+
+class ToolTip extends React.Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props);
+        this.state = {show: true};
     }
-    return (
-        <ShowThenHide>
-            <Style>
-                <Normaltekst><Bold>{props.children}</Bold></Normaltekst>
-            </Style>
-        </ShowThenHide>
-    );
+
+    getCookieNavn() {
+        return 'tooltip' + removeWhitespace(this.props.children);
+    }
+
+    dontShowAgain() {
+        Cookies.set(this.getCookieNavn(), 'dontShow');
+    }
+
+    handleButton() {
+        this.dontShowAgain();
+        this.setState({show: false});
+    }
+
+    userHasUnsubscribed() {
+        return Cookies.get(this.getCookieNavn());
+    }
+
+    render() {
+        if (
+            !this.props.children ||
+            this.props.children === '' ||
+            !this.state.show ||
+            this.userHasUnsubscribed()
+        ) {
+            return null;
+        }
+
+        return (
+            <ShowThenHide>
+                <Style>
+                    <Normaltekst><Bold>{this.props.children}</Bold></Normaltekst>
+                    <Button onClick={() => this.handleButton()}>
+                        <Undertekst>
+                            Ikke vis dette tipset igjen
+                        </Undertekst>
+                    </Button>
+                </Style>
+            </ShowThenHide>
+        );
+    }
 }
 
 export default ToolTip;
