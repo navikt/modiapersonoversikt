26bfb283e3ff39fbd94541036f13a79c367d6583
Daniel Winsvold <danielwinsvold@gmail.com>
2019-01-10 11:02:03 +0100

    refaktorerer for mer lesbar kode

--- src/redux/featureToggle/FeatureToggle.test.tsx
+++ src/redux/featureToggle/FeatureToggle.test.tsx
@@ -3,10 +3,11 @@ import { createStore } from 'redux';
 import reducers from '../reducers';
 import { setFeatureToggleOff, setFeatureToggleOn } from './featureToggleReducer';
 import FeatureToggle from './FeatureToggle';
-import { Provider } from 'react-redux';
 import { mount } from 'enzyme';
-import FeatureToggleOff from './FeatureToggleOff';
+import DisplayWhenFeatureToggleOff from './DisplayWhenFeatureToggleOff';
 import LazySpinner from '../../components/LazySpinner';
+import DisplayWhenFeatureToggleOn from './DisplayWhenFeatureToggleOn';
+import TestProvider from '../../test/Testprovider';
 
 const toggleId = 'Min første feature-toggle';
 
@@ -17,49 +18,37 @@ test('viser innhold i FeatureToggle dersom feature-toggle er på', () => {
     store.dispatch(setFeatureToggleOn(toggleId));
 
     const result = mount(
-        <Provider store={store}>
-            <FeatureToggle toggleID={toggleId}>Her er jeg</FeatureToggle>
-        </Provider>
+        <TestProvider customStore={store}>
+            <DisplayWhenFeatureToggleOn toggleID={toggleId}>Jeg er på</DisplayWhenFeatureToggleOn>
+            <DisplayWhenFeatureToggleOff toggleID={toggleId}>Jeg er av</DisplayWhenFeatureToggleOff>
+        </TestProvider>
     );
 
-    expect(result.text()).toContain('Her er jeg');
+    expect(result.text()).toContain('Jeg er på');
 });
 
-test('viser innhold i FeatureToggleOff dersom feature-toggle er av', () => {
+test('viser innhold i DisplayWhenFeatureToggleOff dersom feature-toggle er av', () => {
     const store = createStore(reducers);
     store.dispatch(setFeatureToggleOff(toggleId));
 
     const result = mount(
-        <Provider store={store}>
-            <FeatureToggleOff toggleID={toggleId}>Her er jeg</FeatureToggleOff>
-        </Provider>
+        <TestProvider customStore={store}>
+            <DisplayWhenFeatureToggleOn toggleID={toggleId}>Jeg er på</DisplayWhenFeatureToggleOn>
+            <DisplayWhenFeatureToggleOff toggleID={toggleId}>Jeg er av</DisplayWhenFeatureToggleOff>
+        </TestProvider>
     );
 
-    expect(result.text()).toContain('Her er jeg');
-});
-
-test('viser ikke innhold dersom feature-toggle ikke er satt', () => {
-    const store = createStore(reducers);
-
-    const result = mount(
-        <Provider store={store}>
-            <>
-                <FeatureToggle toggleID={toggleId}>Her er jeg</FeatureToggle>
-                <FeatureToggleOff toggleID={toggleId}>Og her er jeg</FeatureToggleOff>
-            </>
-        </Provider>
-    );
-
-    expect(result.html()).toBeNull();
+    expect(result.text()).toContain('Jeg er av');
 });
 
 test('viser LazySpinner dersom feature-toggle ikke er satt', () => {
     const store = createStore(reducers);
 
     const result = mount(
-        <Provider store={store}>
-            <FeatureToggle toggleID={toggleId}>Her er jeg</FeatureToggle>
-        </Provider>
+        <TestProvider customStore={store}>
+            <DisplayWhenFeatureToggleOn toggleID={toggleId}>Jeg er på</DisplayWhenFeatureToggleOn>
+            <DisplayWhenFeatureToggleOff toggleID={toggleId}>Jeg er av</DisplayWhenFeatureToggleOff>
+        </TestProvider>
     );
 
     expect(result.contains(<LazySpinner type="S" delay={1000}/>)).toBe(true);
