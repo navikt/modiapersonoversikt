9628cdb8ecbf2fc1c4d567022cd242d1fd3c9106
Daniel Winsvold <danielwinsvold@gmail.com>
2019-01-15 09:11:43 +0100

    tooltip forsvinner ikke på timeout men når man trykker utenfor tooltip. Stopper også eventPropagation slik at klikk på tooltip ikke trigger onClick events i container

--- src/components/tooltip/ToolTip.tsx
+++ src/components/tooltip/ToolTip.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import styled from 'styled-components';
 import { Normaltekst, Undertekst } from 'nav-frontend-typografi';
 import { Bold } from '../common-styled-components';
-import ShowThenHide from '../ShowThenHide';
 import { removeWhitespace } from '../../utils/string-utils';
 import * as Cookies from 'js-cookie';
+import { eventTagetIsInsideRef } from '../../utils/reactRefUtils';
 
 interface Props {
     children: string;
@@ -22,7 +22,7 @@ const Position = styled.div`
   z-index: 1000;
 `;
 
-const Style = styled.div`
+const Style = styled.aside`
   background-color: #333;
   border: .2rem white solid;
   border-radius: 1rem;
@@ -30,7 +30,6 @@ const Style = styled.div`
   color: white;
   box-shadow: 0 .2rem .5rem rgba(0, 0, 0, 0.5);
   opacity: .8;
-  pointer-events: none;
   text-align: right;
 `;
 
@@ -38,7 +37,6 @@ const Button = styled.button`
   background-color: transparent;
   border: none;
   color: white;
-  pointer-events: auto;
   > *:hover {
     text-decoration: underline;
   }
@@ -46,18 +44,28 @@ const Button = styled.button`
 
 class ToolTip extends React.PureComponent<Props, State> {
 
+    private ref = React.createRef<HTMLDivElement>();
+
     constructor(props: Props) {
         super(props);
         this.state = {show: true};
         this.handleButton = this.handleButton.bind(this);
+        this.handleClickEvent = this.handleClickEvent.bind(this);
     }
 
-    getCookieNavn() {
-        return 'tooltip' + removeWhitespace(this.props.children);
+    componentDidMount() {
+        setTimeout(() => addEventListener('click', this.handleClickEvent), 100);
     }
 
-    dontShowAgain() {
-        Cookies.set(this.getCookieNavn(), 'dontShow');
+    componentWillUnmount() {
+        removeEventListener('click', this.handleClickEvent);
+    }
+
+    handleClickEvent(event: MouseEvent) {
+        const clickIsInsideTooltip = eventTagetIsInsideRef(event, this.ref);
+        if (!clickIsInsideTooltip) {
+            this.setState({show: false});
+        }
     }
 
     handleButton(event: React.MouseEvent<HTMLButtonElement>) {
@@ -68,6 +76,14 @@ class ToolTip extends React.PureComponent<Props, State> {
 
     userHasUnsubscribed() {
         return Cookies.get(this.getCookieNavn());
+    }
+
+    dontShowAgain() {
+        Cookies.set(this.getCookieNavn(), 'dontShow');
+    }
+
+    getCookieNavn() {
+        return 'tooltip' + removeWhitespace(this.props.children);
     }
 
     render() {
@@ -81,17 +97,15 @@ class ToolTip extends React.PureComponent<Props, State> {
         }
 
         return (
-            <Position>
-                <ShowThenHide>
-                    <Style>
-                        <Normaltekst><Bold>{this.props.children}</Bold></Normaltekst>
-                        <Button onClick={this.handleButton}>
-                            <Undertekst>
-                                Ikke vis dette tipset igjen
-                            </Undertekst>
-                        </Button>
-                    </Style>
-                </ShowThenHide>
+            <Position ref={this.ref} onClick={event => event.stopPropagation()}>
+                <Style aria-live="polite" aria-description="Tips">
+                    <Normaltekst><Bold>{this.props.children}</Bold></Normaltekst>
+                    <Button onClick={this.handleButton}>
+                        <Undertekst>
+                            Ikke vis dette tipset igjen
+                        </Undertekst>
+                    </Button>
+                </Style>
             </Position>
         );
     }
