f4d059421935eabedab3babf7205e13019cd93e7
Daniel Winsvold <danielwinsvold@gmail.com>
2019-01-22 17:01:57 +0100

    fikser knapper i saksoversikten så de matcher resten av appen bedre

--- src/components/VisMerChevron.tsx
+++ src/components/VisMerChevron.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { theme } from '../../../../../styles/personOversiktTheme';
+import { theme } from '../styles/personOversiktTheme';
 import NavFrontendChevron from 'nav-frontend-chevron';
 
 interface Props {
@@ -9,7 +9,7 @@ interface Props {
     children?: React.ReactNode;
 }
 
-const KnappWrapper = styled.button`
+const ChevronWrapper = styled.button`
   border: none;
   padding: .1rem;
   border-radius: 0.5em;
@@ -20,13 +20,13 @@ const KnappWrapper = styled.button`
   }
 `;
 
-function DetaljerKnapp(props: Props) {
+function VisMerChevron(props: Props) {
     return (
-        <KnappWrapper onClick={props.onClick} aria-expanded={props.open} aria-label="Detaljer">
+        <ChevronWrapper onClick={props.onClick} aria-expanded={props.open} aria-label="Detaljer">
             <NavFrontendChevron type={props.open ? 'opp' : 'ned'}/>
             {props.children}
-        </KnappWrapper>
+        </ChevronWrapper>
     );
 }
 
-export default DetaljerKnapp;
+export default VisMerChevron;
