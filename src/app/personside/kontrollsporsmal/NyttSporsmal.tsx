cb04cecfb239de2ccaf3091526c8b75209c12410
Jørund Amsen <jorundamsen@nav.no>
2019-01-28 10:33:31 +0100

    Nedre del av kontrollspørsmål. Inneholder knapp for å vise neste spørsmål
    Lagt til logg-event for å få oversikt over hvor mange brukere som bruker knappen over hurtigtasten
    
    OPP-575

---
+++ src/app/personside/kontrollsporsmal/NyttSporsmal.tsx
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { AsyncDispatch } from '../../../redux/ThunkTypes';
+import theme from '../../../styles/personOversiktTheme';
+import { roterKontrollSpørsmål } from '../../../redux/restReducers/kontrollSporsmal/actions';
+import KnappBase from 'nav-frontend-knapper';
+import { loggEvent } from '../../../utils/frontendLogger';
+import { getSaksbehandlerIdent } from '../../../utils/loggInfo/getSaksbehandlerIdent';
+
+export interface DispatchProps {
+    nyttSpørsmål: () => void;
+}
+
+const NyttSpørsmålStyling = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-right: ${theme.margin.px10};
+  > * {
+    text-transform: none; 
+  }
+`;
+
+class NyttSporsmal extends React.PureComponent<DispatchProps> {
+    constructor(props: DispatchProps) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    render() {
+        return (
+            <NyttSpørsmålStyling>
+                <KnappBase
+                    id="nyttKontrollsporsmalKnapp"
+                    type="standard"
+                    onClick={this.handleClick}
+                >
+                    Nytt spørsmål
+                </KnappBase>
+            </NyttSpørsmålStyling>
+        );
+    }
+
+    private handleClick() {
+        loggEvent('Knapp', 'Kontrollsporsmal', {type: 'Nytt'}, {ident: getSaksbehandlerIdent()});
+        this.props.nyttSpørsmål();
+    }
+
+}
+
+function mapDispatchToProps(dispatch: AsyncDispatch): DispatchProps {
+    return {
+        nyttSpørsmål: () => dispatch(roterKontrollSpørsmål())
+    };
+}
+
+export default connect(null, mapDispatchToProps)(NyttSporsmal);
