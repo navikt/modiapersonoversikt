78a6d23403a1598d06a10f0d456c4103c0d03345
Jørund Amsen <jorundamsen@nav.no>
2019-01-28 10:57:59 +0100

    Legger til hotkey for å lukke kontrollspørsmål-boksen
    Hurtigtasten ALT + L lukker nå kontrollspørsmål
    Logger også for metrikker
    
    OPP-575

---
+++ src/app/personside/kontrollsporsmal/HandleKontrollSporsmalHotkeys.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { loggEvent } from '../../../utils/frontendLogger';
+import { getSaksbehandlerIdent } from '../../../utils/loggInfo/getSaksbehandlerIdent';
+import { Action, Dispatch } from 'redux';
+import { toggleKontrollSpørsmål } from '../../../redux/restReducers/kontrollSporsmal/actions';
+import { connect } from 'react-redux';
+
+interface DispatchProps {
+    toggleKontrollSpørsmål: () => void;
+}
+
+class HandleKontrollSporsmalHotkeys extends React.Component<DispatchProps> {
+
+    constructor(props: DispatchProps) {
+        super(props);
+        this.handleKontrollSpørsmålHotkey = this.handleKontrollSpørsmålHotkey.bind(this);
+    }
+
+    componentDidMount() {
+        document.addEventListener('keydown', this.handleKontrollSpørsmålHotkey);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.handleKontrollSpørsmålHotkey);
+    }
+
+    render() {
+        return null;
+    }
+
+    private handleKontrollSpørsmålHotkey(event: KeyboardEvent) {
+        const key = event.code ? event.code.replace('Key', '').toLowerCase() : event.key;
+
+        if (key === 'l' && event.altKey) {
+            loggEvent('Hurtigtast', 'Kontrollsporsmal', {type: 'Alt + L'}, {ident: getSaksbehandlerIdent()});
+            this.props.toggleKontrollSpørsmål();
+        }
+    }
+}
+
+function mapDispatchToProps(dispatch: Dispatch<Action>): DispatchProps {
+    return {
+        toggleKontrollSpørsmål: () => dispatch(toggleKontrollSpørsmål())
+    };
+}
+
+export default connect(null, mapDispatchToProps)(HandleKontrollSporsmalHotkeys);
