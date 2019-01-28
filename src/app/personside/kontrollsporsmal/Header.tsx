41cfb7e8a9f9baec35bced4b661b4bd89b6ea805
Jørund Amsen <jorundamsen@nav.no>
2019-01-28 10:31:58 +0100

    Skriv HEADER-delen av kontrollspørsmål-boksen
    Denne inneholder tittel og lukk-knapp
    Verdt å merke at selv om redux-actionen er "toggle" vil funksjonaliteten kun tillate å _lukke_ boksen per nå, ettersom knappen ikke er tilgjengelig. Den vil kunne åpnes igjen ved et nytt dispatch av toggleSynlig.
    
    OPP-575

---
+++ src/app/personside/kontrollsporsmal/Header.tsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+import * as React from 'react';
+import Undertittel from 'nav-frontend-typografi/lib/undertittel';
+import { connect } from 'react-redux';
+import { AsyncDispatch } from '../../../redux/ThunkTypes';
+import theme from '../../../styles/personOversiktTheme';
+import { toggleKontrollSpørsmål } from '../../../redux/restReducers/kontrollSporsmal/actions';
+import Lukknapp from 'nav-frontend-lukknapp';
+import { loggEvent } from '../../../utils/frontendLogger';
+import { getSaksbehandlerIdent } from '../../../utils/loggInfo/getSaksbehandlerIdent';
+
+interface DispatchProps {
+    toggleKontrollSpørsmål: () => void;
+}
+
+const HeaderStyling = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  margin: ${theme.margin.px10};
+`;
+
+class Header extends React.PureComponent<DispatchProps> {
+    constructor(props: DispatchProps) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    render() {
+        return (
+            <HeaderStyling>
+                <Undertittel tag="h1">Kontrollspørsmål</Undertittel>
+                <Lukknapp
+                    ariaLabel={'Skjul kontrollspørsmål'}
+                    onClick={this.handleClick}
+                    overstHjorne={true}
+                >
+                    Skjul kontrollspørsmål
+                </Lukknapp>
+            </HeaderStyling>
+        );
+    }
+
+    private handleClick() {
+        loggEvent('Knapp', 'Kontrollsporsmal', {type: 'Lukk'}, {ident: getSaksbehandlerIdent()});
+        this.props.toggleKontrollSpørsmål();
+    }
+}
+
+function mapDispatchToProps(dispatch: AsyncDispatch): DispatchProps {
+    return {
+        toggleKontrollSpørsmål: () => dispatch(toggleKontrollSpørsmål())
+    };
+}
+
+export default connect(null, mapDispatchToProps)(Header);
