7fd334bb23098bf1c29f2e5b4da92bf95d35ea4f
Ketil Velle <ketil.s.velle@nav.no>
2019-02-07 13:56:40 +0100

    Spike på saf-intergrasjon direkte fra frontend.

---
+++ src/redux/restReducers/saf/safReducer.ts
@@ -0,0 +1,16 @@
+import { createActionsAndReducer } from '../restReducer';
+import { SafRequest } from "./safRequest";
+import { postSaf } from "../../../api/saf";
+
+const { reducer, action, tilbakestillReducer, actionNames } = createActionsAndReducer('saf');
+
+export function saf(request: SafRequest) {
+    return action(() => postSaf(request));
+}
+
+export function reset() {
+    return tilbakestillReducer;
+}
+
+export { actionNames };
+export default reducer;
