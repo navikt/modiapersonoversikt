84afc403478c43cd773cfba6fb2d5dcb7ea2265c
Ketil Velle <ketil.s.velle@nav.no>
2019-02-11 13:01:44 +0100

    Revert "Endret url fra q1 til q6"
    This reverts commit b438b30ffbb49c36feb6f5eefb82d5f0a76c221b.

--- src/api/saf.ts
+++ src/api/saf.ts
@@ -2,6 +2,6 @@ import { SafRequest } from "../redux/restReducers/saf/safRequest";
 import { post } from './api';
 
 export function postSaf(request: SafRequest): Promise<{}> {
-    const uri = "https://saf-q6.nais.preprod.local/graphql";
+    const uri = "https://saf-q1.nais.preprod.local/graphql";
     return post(uri, request);
 }
