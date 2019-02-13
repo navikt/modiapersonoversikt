d5f460af8620862070e2691ff7049fd73911552c
Ketil Velle <ketil.s.velle@nav.no>
2019-02-13 13:47:03 +0100

    Endret saf-url
    OPP-663

--- src/api/saf.ts
+++ src/api/saf.ts
@@ -2,6 +2,6 @@ import { SafRequest } from '../redux/restReducers/saf/safRequest';
 import { post } from './api';
 
 export function postSaf(request: SafRequest): Promise<{}> {
-    const uri = 'https://saf-q6.nais.preprod.local/graphql';
+    const uri = 'https://app-q6.adeo.no/saf/graphql';
     return post(uri, request);
 }
