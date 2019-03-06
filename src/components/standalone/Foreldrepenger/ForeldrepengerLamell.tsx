f9f31f886ade53490c7789b77aa25b6a3b11ed68
Jan Eirik B. Nævdal <jan.eirik.b.navdal@nav.no>
2019-03-06 15:15:07 +0100

    Opp-670 Foreldrepenger i modia

---
+++ src/components/standalone/Foreldrepenger/ForeldrepengerLamell.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { Provider } from 'react-redux';
+import ErrorBoundary from '../../ErrorBoundary';
+import { applyMiddleware, createStore } from 'redux';
+import reducers from '../../../redux/reducers';
+import thunkMiddleware from 'redux-thunk';
+import { mockEnabled } from '../../../api/config';
+import { setupMock } from '../../../mock/setup-mock';
+import ForeldrepengerLaster from './ForeldrepengerLaster';
+
+interface Props {
+    fødselsnummer: string;
+    barnetsFødselsnummer: string;
+}
+
+const store = createStore(
+    reducers,
+    applyMiddleware(thunkMiddleware)
+);
+
+if (mockEnabled) {
+    setupMock();
+}
+
+class PleiepengerLamell extends React.PureComponent<Props> {
+
+    render() {
+        return (
+            <ErrorBoundary boundaryName="PleiepengeLamell">
+                <Provider store={store}>
+                    <ForeldrepengerLaster
+                        fødselsnummer={this.props.fødselsnummer}
+                        barnetsFødselsnummer={this.props.barnetsFødselsnummer}
+                    />
+                </Provider>
+            </ErrorBoundary>);
+    }
+}
+
+export default PleiepengerLamell;
