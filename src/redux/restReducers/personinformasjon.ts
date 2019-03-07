8a8203205b0eae090de2137801a76fea472c114d
Daniel Winsvold <danielwinsvold@gmail.com>
2019-03-07 13:30:44 +0100

    trekker ut hentAllPersondata() i egen fil og renamer til oppslagNyBruker()

--- src/redux/restReducers/personinformasjon.ts
+++ src/redux/restReducers/personinformasjon.ts
@@ -1,46 +1,15 @@
 import { getPerson } from '../../api/person-api';
 import { createActionsAndReducer, isLoaded, Loaded, RestReducer } from './restReducer';
-import { hentKontaktinformasjon } from './kontaktinformasjon';
-import { erEgenAnsatt } from './egenansatt';
-import { hentVergemal } from './vergemal';
-import { resetNavKontorReducer } from './navkontor';
-import { resetUtbetalingerReducer } from './utbetalinger';
-import { resetSykepengerReducer } from './ytelser/sykepenger';
-import { resetPleiepengerReducer } from './ytelser/pleiepenger';
-import { resetForeldrepengerReducer } from './ytelser/foreldrepenger';
-import { erGyldigFødselsnummer } from 'nav-faker/dist/personidentifikator/helpers/fodselsnummer-utils';
-import { AsyncDispatch } from '../ThunkTypes';
-import { resetUtførteUtbetalingerReducer } from './ytelser/utførteUtbetalinger';
-import { hentFeatureToggles } from './featureToggles';
-import { resetKontrollSpørsmål } from '../kontrollSporsmal/actions';
 import { Person, PersonRespons } from '../../models/person/person';
 
 const { reducer, action, actionNames, reload } = createActionsAndReducer('personinformasjon');
 
-function hentPerson(fødselsnummer: string) {
+export function hentPerson(fødselsnummer: string) {
     return action(() => getPerson(fødselsnummer));
 }
 
 export function reloadPerson(fødselsnummer: string) {
     return reload(() => getPerson(fødselsnummer));
-}
-
-export function hentAllPersonData(dispatch: AsyncDispatch, fødselsnummer: string) {
-    if (!erGyldigFødselsnummer(fødselsnummer)) {
-        console.warn('Ugyldig fødselsnummer: ', fødselsnummer);
-    }
-    dispatch(hentPerson(fødselsnummer));
-    dispatch(hentKontaktinformasjon(fødselsnummer));
-    dispatch(erEgenAnsatt(fødselsnummer));
-    dispatch(hentVergemal(fødselsnummer));
-    dispatch(hentFeatureToggles());
-    dispatch(resetNavKontorReducer());
-    dispatch(resetUtbetalingerReducer());
-    dispatch(resetSykepengerReducer());
-    dispatch(resetPleiepengerReducer());
-    dispatch(resetForeldrepengerReducer());
-    dispatch(resetUtførteUtbetalingerReducer());
-    dispatch(resetKontrollSpørsmål());
 }
 
 export function isLoadedPerson(person: RestReducer<PersonRespons>): person is Loaded<Person> {
