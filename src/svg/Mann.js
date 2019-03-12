6eda7cbb6fd0a99eeb285e1fec11653ce1e494a8
Daniel Winsvold <danielwinsvold@gmail.com>
2019-03-12 14:16:08 +0100

    OPP-745 accessibility, info om kjønn i visittkortheader, fokus på visittkortbody bruker skjult header istdenenfor fokus på region for å gi bedre plassering av fokus for videre navigering inne i visittkortdetaljer

--- src/svg/Mann.js
+++ src/svg/Mann.js
@@ -2,7 +2,15 @@ import * as React from 'react';
 
 export default function Mann(props) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width={64} height={64} {...props}>
+        <svg
+            alt="Mann"
+            aria-label="Mann"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 64 64"
+            width={64}
+            height={64}
+            {...props}
+        >
             <path
                 fill="#3385D1"
                 d="M32 0a32 32 0 1 0 0 64 32 32 0 0 0 0-64zm0 8a3.9 3.9 0 1 1 0 7.7 3.9 3.9 0 0 1 0-7.8zm10 15.5v10.8c0 .9-.7 1.6-1.6 1.6-1 0-1.8-.7-1.8-1.6v-8.9c0-.4-.3-.8-.7-.8-.4 0-.7.3-.7.7v28.5a2.2 2.2 0 1 1-4.5 0v-18a.7.7 0 0 0-1.4 0v18a2.2 2.2 0 0 1-4.4 0V25.4a.7.7 0 0 0-1.5 0V34.3c0 .9-.8 1.6-1.8 1.6s-1.7-.7-1.7-1.6V23.5v-.7c0-3.4 3.6-5.8 7-6H35c3.4.2 7 2.6 7 6v.7z"
