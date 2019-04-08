c06d90e7bb7dd3f576c9e48c35b870a84b54bbf3
Daniel Winsvold <danielwinsvold@gmail.com>
2019-04-08 13:48:43 +0200

    følger opp kommentarer fra PR. Typing av RestResourceConsumer legges i samme fil som den utypede versjonen da den utypede versjonen aldri skal brukes alene

--- src/rest/consumer/RestResourceConsumer.tsx
+++ src/rest/consumer/RestResourceConsumer.tsx
@@ -1,9 +1,71 @@
 import * as React from 'react';
-import RestResourceConsumerUntyped, { RestResourceConsumerOwnProps } from './RestResourceConsumerUntyped';
+import { ReactNode } from 'react';
+import { connect } from 'react-redux';
+import { AsyncDispatch } from '../../redux/ThunkTypes';
+import { RestEndepunkter } from '../../redux/restReducers/restReducers';
+import { AppState } from '../../redux/reducers';
+import { isFailed, isLoaded, isNotStarted, RestResource } from '../utils/restResource';
+import { RestRestourceFeilmelding } from './utils';
+import LazySpinner from '../../components/LazySpinner';
+
+export type RestResourceConsumerOwnProps<T> = {
+    getRestResource: (restResources: RestEndepunkter) => RestResource<T>;
+    children: (data: T) => ReactNode;
+    returnOnError?: ReactNode;
+    returnOnPending?: ReactNode;
+    spinnerSize?: 'XXS' | 'XS' | 'S' | 'M' | 'L' | 'XL' | 'XXL' | 'XXXL';
+};
+
+interface StateProps<T> {
+    restResource: RestResource<T>;
+}
+
+interface DispatchProps {
+    dispatch: AsyncDispatch;
+}
+
+type Props<T> = RestResourceConsumerOwnProps<T> & DispatchProps & StateProps<T>;
+
+class RestResourceConsumerUntyped<T> extends React.Component<Props<T>> {
+    render() {
+        const { restResource, dispatch, spinnerSize, returnOnPending, returnOnError, children } = this.props;
+        if (isNotStarted(restResource)) {
+            dispatch(restResource.actions.fetch);
+        }
+        if (isFailed(restResource)) {
+            return returnOnError || <RestRestourceFeilmelding />;
+        }
+        if (!isLoaded(restResource)) {
+            return returnOnPending || <LazySpinner type={spinnerSize || 'L'} />;
+        }
+        return children(restResource.data);
+    }
+}
+
+function mapDispatchToProps<T>(dispatch: AsyncDispatch): DispatchProps {
+    return {
+        dispatch: dispatch
+    };
+}
+
+function mapStateToProps<T>(state: AppState, ownProps: RestResourceConsumerOwnProps<T>): StateProps<T> {
+    return {
+        restResource: ownProps.getRestResource(state.restResources)
+    };
+}
+
+const ConnectedRestResourceConsumerUntyped = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(RestResourceConsumerUntyped);
 
 function RestResourceConsumer<T>(props: RestResourceConsumerOwnProps<T>) {
     const { children, ...resten } = props;
-    return <RestResourceConsumerUntyped {...resten}>{data => children(data as T)}</RestResourceConsumerUntyped>;
+    return (
+        <ConnectedRestResourceConsumerUntyped {...resten}>
+            {data => children(data as T)}
+        </ConnectedRestResourceConsumerUntyped>
+    );
 }
 
 export default RestResourceConsumer;
