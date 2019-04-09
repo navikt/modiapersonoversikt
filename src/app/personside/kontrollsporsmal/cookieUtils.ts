0eb8709e515e5bd12494e518dd93d8e46f0f312a
Daniel Winsvold <danielwinsvold@gmail.com>
2019-04-09 15:47:26 +0200

    mer forklarende navn på cookies, lettere å skjønne bruk når man ser dem i devTools

--- src/app/personside/kontrollsporsmal/cookieUtils.ts
+++ src/app/personside/kontrollsporsmal/cookieUtils.ts
@@ -1,18 +1,18 @@
 import moment from 'moment';
 import * as Cookies from 'js-cookie';
 
-const skjulForCookieNavn = 'COOKIE_SkjulForBruker';
-const skjulVedPlukkCookieNavn = 'COOKIE_JobberMedSpmOgSvar';
+const kontrollSpmErLukketForBrukerCookieNavn = 'KontrollSpmErLukketForBruker';
+const jobberMedSpmOgSvarCookieNavn = 'JobberMedSpmOgSvar';
 
 export function settSkjulKontrollspørsmålPåTversAvVinduerForBrukerCookie(fnr: string) {
     const omEnTime = moment()
         .add(1, 'hour')
         .toDate();
-    Cookies.set(skjulForCookieNavn, fnr, { expires: omEnTime });
+    Cookies.set(kontrollSpmErLukketForBrukerCookieNavn, fnr, { expires: omEnTime });
 }
 
 export function kontrollspørsmålHarBlittLukketForBruker(fnr: string): boolean {
-    const cookie = Cookies.get(skjulForCookieNavn);
+    const cookie = Cookies.get(kontrollSpmErLukketForBrukerCookieNavn);
     if (cookie && cookie === fnr) {
         return true;
     }
@@ -20,15 +20,15 @@ export function kontrollspørsmålHarBlittLukketForBruker(fnr: string): boolean 
 }
 
 export function settJobberMedSpørsmålOgSvar() {
-    Cookies.set(skjulVedPlukkCookieNavn, 'true');
+    Cookies.set(jobberMedSpmOgSvarCookieNavn, 'true');
 }
 
 export function settJobberIkkeMedSpørsmålOgSvar() {
-    Cookies.remove(skjulVedPlukkCookieNavn);
+    Cookies.remove(jobberMedSpmOgSvarCookieNavn);
 }
 
 export function jobberMedSpørsmålOgSvar() {
-    const cookie = Cookies.get(skjulVedPlukkCookieNavn);
+    const cookie = Cookies.get(jobberMedSpmOgSvarCookieNavn);
     if (cookie && cookie === 'true') {
         return true;
     }
