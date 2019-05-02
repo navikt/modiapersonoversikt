e853e455183e84e2e8000339830706df924c0d77
Daniel Winsvold <danielwinsvold@gmail.com>
2019-05-02 14:29:59 +0200

    dersom et felt er tallet 0 i DescriptionList vises det som 0 og ikke som emdash

--- src/components/DescriptionList.test.tsx
+++ src/components/DescriptionList.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { DescriptionListEntries, fjernEntriesUtenVerdi } from './DescriptionList';
+import DescriptionList, { DescriptionListEntries, fjernEntriesUtenVerdi } from './DescriptionList';
+import { mount } from 'enzyme';
 
 test('fjerner tomme properties fra objekt med DescriptionListEntries', () => {
     const test: DescriptionListEntries = {
@@ -13,4 +14,14 @@ test('fjerner tomme properties fra objekt med DescriptionListEntries', () => {
     const resultat = fjernEntriesUtenVerdi(test);
 
     expect(resultat).toEqual({ streng: 'daniel', nummer: 10, reactKomponent: <div>Hei</div> });
+});
+
+test('0 blir vist som 0, og ikke som strek', () => {
+    const entries: DescriptionListEntries = {
+        talletNull: 0
+    };
+
+    const resultat = mount(<DescriptionList entries={entries} />);
+
+    expect(resultat.find('dd').contains('0')).toBe(true);
 });
