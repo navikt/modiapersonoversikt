e4da09859e53e2cf0b2a4936ef1c8d3867db914a
Daniel Winsvold <danielwinsvold@gmail.com>
2019-05-06 08:24:50 +0200

    skriver om utbetalinger på vent til å bruke tabell

--- src/app/personside/infotabs/ytelser/sykepenger/utbetalingerpåvent/UtbetalingerPåVentListe.tsx
+++ src/app/personside/infotabs/ytelser/sykepenger/utbetalingerpåvent/UtbetalingerPåVentListe.tsx
@@ -3,37 +3,46 @@ import { AlertStripeInfo } from 'nav-frontend-alertstriper';
 import styled from 'styled-components';
 import { AlignTextCenter } from '../../../../../../components/common-styled-components';
 import { Undertittel } from 'nav-frontend-typografi';
-import UtbetalingPåVentElement from './UtbetalingPåVentElement';
 import { UtbetalingPåVent } from '../../../../../../models/ytelse/ytelse-utbetalinger';
+import { utledUtbetalingPåVentÅrsak } from './utledUtbetalingerPåVentÅrsak';
+import { periodeEllerNull, prosentEllerNull } from '../../../../../../utils/stringFormatting';
+import { createTable } from '../../../../../../utils/tableUtils';
+import theme from '../../../../../../styles/personOversiktTheme';
 
 interface Props {
     utbetalingerPåVent: UtbetalingPåVent[];
 }
 
-const Padding = styled.div`
-    padding: 0.5rem;
+const Style = styled.section`
+    > *:not(:last-child) {
+        margin-bottom: 1rem;
+    }
+    ${theme.table}
 `;
 
 function getInnhold(utbetalingerpåVent: UtbetalingPåVent[]) {
     if (utbetalingerpåVent.length === 0) {
         return <AlertStripeInfo>Ingen utbetalinger på vent funnet</AlertStripeInfo>;
     }
-    const utbetalingerPåVent = utbetalingerpåVent.map((utbetaling, index) => (
-        <UtbetalingPåVentElement key={index} utbetalingPåVent={utbetaling} />
-    ));
-    return <ol>{utbetalingerPåVent}</ol>;
+    const tittelRekke = ['Årsak', 'Utbetalingsperiode', 'Utbetalingsgrad'];
+    const rows = utbetalingerpåVent.map(utbetaling => [
+        utledUtbetalingPåVentÅrsak(utbetaling),
+        periodeEllerNull(utbetaling.vedtak),
+        prosentEllerNull(utbetaling.utbetalingsgrad)
+    ]);
+    const table = createTable(tittelRekke, rows);
+
+    return table;
 }
 
 function UtbetalingerPåVentListe(props: Props) {
     return (
-        <section>
-            <Padding>
-                <AlignTextCenter>
-                    <Undertittel tag="h4">Utbetalinger på vent</Undertittel>
-                </AlignTextCenter>
-            </Padding>
+        <Style>
+            <AlignTextCenter>
+                <Undertittel tag="h4">Utbetalinger på vent</Undertittel>
+            </AlignTextCenter>
             {getInnhold(props.utbetalingerPåVent)}
-        </section>
+        </Style>
     );
 }
 
