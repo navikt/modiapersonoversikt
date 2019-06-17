ec98a16ec2ddc0b91b00bfe07ab45b7babd2b201
Richard Borge <richard.borge@nav.no>
2019-06-17 12:17:09 +0200

    Fjerner persong av response fordi vi alltid får json som svar

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,18 +3,9 @@ import { postConfig } from './config';
 export function post(uri: string, body: object) {
     return fetch(uri, postConfig(body)).then(response => {
         if (response.ok && !response.redirected) {
-            return parseResponse(response);
+            return response.json();
         } else {
             throw response.statusText;
         }
     });
-}
-
-function parseResponse(response: Response): Promise<object> {
-    const contentType = response.headers.get('content-type');
-    if (contentType && contentType.indexOf('application/json') !== -1) {
-        return response.json();
-    } else {
-        return Promise.resolve({});
-    }
 }
