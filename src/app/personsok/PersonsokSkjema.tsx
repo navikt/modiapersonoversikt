4b1c79b1240c0cd01715e4937a96992701f2186d
Richard Borge <richard.borge@nav.no>
2019-06-17 12:16:22 +0200

    Brukergrensesnitt for avansert søk, kun for å sy sammen

---
+++ src/app/personsok/PersonsokSkjema.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { FormEvent } from 'react';
+import { PersonsokRequest } from '../../models/person/personsok';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppState } from '../../redux/reducers';
+import { Hovedknapp } from 'nav-frontend-knapper';
+
+function PersonsokSkjema() {
+    const dispatch = useDispatch();
+    const personsokResource = useSelector((state: AppState) => state.restResources.personsok);
+
+    const submitHandler = (event: FormEvent) => {
+        event.preventDefault();
+        const request: PersonsokRequest = {};
+        dispatch(personsokResource.actions.post(request));
+    };
+
+    return (
+        <form onSubmit={submitHandler}>
+            <Hovedknapp role={'submit'}>Send</Hovedknapp>
+        </form>
+    );
+}
+
+export default PersonsokSkjema;
