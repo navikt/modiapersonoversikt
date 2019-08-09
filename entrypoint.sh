9a2702d37ec1bd9412bc7050d76eb3f6c4e7209d
Nicklas Utgaard <nutgaard@gmail.com>
2019-08-09 10:28:32 +0200

    KAIZEN-0 sed-hack for å få opp heroku appen

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 
-sed -i sg,%REACT_APP_HODE_URL%,${REACT_APP_HODE_URL:-"/internarbeidsflatedecorator"}, /usr/share/nginx/html/modiapersonoversikt/index.html
+sed -i s,%REACT_APP_HODE_URL%,${REACT_APP_HODE_URL:-"/internarbeidsflatedecorator"}, /usr/share/nginx/html/modiapersonoversikt/index.html
+sed -i s,%REACT_APP_HODE_URL%,${REACT_APP_HODE_URL:-"/internarbeidsflatedecorator"}, /usr/share/nginx/html/modiapersonoversikt/index.html
 sed -i s,%REACT_APP_MODIA_URL%,${REACT_APP_MODIA_URL:-"/modiabrukerdialog/rest"}, /usr/share/nginx/html/modiapersonoversikt/index.html
 sed -i s,%REACT_APP_MOCK_ENABLED%,$REACT_APP_MOCK_ENABLED, /usr/share/nginx/html/modiapersonoversikt/index.html
 sed -i s,%DOCKER_PORT%,${PORT:-80}, /etc/nginx/conf.d/default.conf
