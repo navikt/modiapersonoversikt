abcd97a1a19aa80c2776925ae1a8cb8f6f7ac00f
Daniel Winsvold <danielwinsvold@gmail.com>
2019-08-22 13:16:26 +0200

    oppdaterer tester og snapshots

--- src/app/personside/dialogpanel/fortsettDialog/FortsettDialog.test.tsx
+++ src/app/personside/dialogpanel/fortsettDialog/FortsettDialog.test.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 import TestProvider from '../../../../test/Testprovider';
-import FortsettDialog from './FortsettDialog';
 import { getTestStore } from '../../../../test/testStore';
 import { setDialogpanelTraad } from '../../../../redux/oppgave/actions';
 import { statiskTraadMock } from '../../../../mock/meldinger/statiskTraadMock';
+import FortsettDialogContainer from './FortsettDialogContainer';
 
 test('viser fortsett dialog', () => {
     const testStore = getTestStore();
     testStore.dispatch(setDialogpanelTraad(statiskTraadMock));
     const dialogPanelBody = renderer.create(
         <TestProvider customStore={testStore}>
-            <FortsettDialog />
+            <FortsettDialogContainer traad={statiskTraadMock} />
         </TestProvider>
     );
 
--- src/app/personside/dialogpanel/sendMelding/SendNyMelding.test.tsx
+++ src/app/personside/dialogpanel/sendMelding/SendNyMelding.test.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
-import SendNyMelding from './SendNyMelding';
 import TestProvider from '../../../../test/Testprovider';
+import SendNyMeldingContainer from './SendNyMeldingContainer';
+
+beforeEach(() => {
+    Date.prototype.getTime = jest.fn(() => 0);
+    Date.parse = jest.fn(() => 0);
+});
 
 test('viser send ny melding', () => {
     const dialogPanelBody = renderer.create(
         <TestProvider>
-            <SendNyMelding />
+            <SendNyMeldingContainer />
         </TestProvider>
     );
 
