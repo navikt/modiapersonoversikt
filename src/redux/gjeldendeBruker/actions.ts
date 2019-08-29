d678766e1e4cf4c322b0868b6801307540f1d6e7
Daniel Winsvold <danielwinsvold@gmail.com>
2019-08-29 13:48:05 +0200

    oppdaterer kommentar så den blir mer forklarende

--- src/redux/gjeldendeBruker/actions.ts
+++ src/redux/gjeldendeBruker/actions.ts
@@ -1,7 +1,7 @@
 import { SetNyGjeldendeBrukerAction, SetNyGjeldendeBrukerActionTypes } from './types';
 
 // Det er neppe denne du har lyst til å bruke
-// Denne brukes kun til å sette gjeldende fnr i redux
+// Denne brukes kun til å sette gjeldende fnr i redux, den vil ikke oppdatere url med nytt fnr
 // Sansynligvis ønsker du å bruke setNyBrukerIPath som propagerer videre til resten av appen
 export default function setGjeldendeBrukerIRedux(fødselsnummer: string): SetNyGjeldendeBrukerAction {
     return {
