07202f6d9813ce5bfffc8957fcf5afbe641e0237
Nicklas Utgaard <nicklas.utgaard@nav.no>
2019-09-02 12:39:49 +0200

    [KAIZEN-0] sletter all redux-state ved endring av fnr

--- src/app/PersonOppslagHandler/LyttPåNyttFnrIReduxOgHentAllPersoninfo.tsx
+++ src/app/PersonOppslagHandler/LyttPåNyttFnrIReduxOgHentAllPersoninfo.tsx
@@ -1,52 +1,22 @@
-import { resetKontrollSpørsmål } from '../../redux/kontrollSporsmal/actions';
 import { useEffect } from 'react';
-import { AppState } from '../../redux/reducers';
-import { useDispatch, useSelector } from 'react-redux';
-import { Action } from 'redux';
-import { AsyncAction } from '../../redux/ThunkTypes';
-import { useFetchFeatureTogglesOnNewFnr } from './FetchFeatureToggles';
+import { reset } from '../../redux/reducer-utils';
+import { useDispatch } from 'react-redux';
 import { cache } from '@nutgaard/use-fetch';
-import { setIngenValgtTraadDialogpanel } from '../../redux/oppgave/actions';
-
-function useDispatchOnNewFnr(action: Action | AsyncAction, fnr: string) {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(action);
-    }, [action, fnr, dispatch]);
-}
+import { useGjeldendeBruker } from '../../redux/gjeldendeBruker/types';
+import { fetchAllFeatureToggles } from '../../redux/restReducers/featureToggles';
+import { useRestResource } from '../../utils/customHooks';
 
 function LyttPåNyttFnrIReduxOgHentAllPersoninfo() {
-    const fnr = useSelector((state: AppState) => state.gjeldendeBruker.fødselsnummer);
-    const restResources = useSelector((state: AppState) => state.restResources);
-
-    useDispatchOnNewFnr(resetKontrollSpørsmål, fnr);
-    useDispatchOnNewFnr(restResources.personinformasjon.actions.fetch, fnr);
-    useDispatchOnNewFnr(restResources.kontaktinformasjon.actions.fetch, fnr);
-    useDispatchOnNewFnr(restResources.vergemal.actions.fetch, fnr);
-    useDispatchOnNewFnr(restResources.egenAnsatt.actions.fetch, fnr);
-    useDispatchOnNewFnr(restResources.tildDelteOppgaver.actions.fetch, fnr);
-
-    useDispatchOnNewFnr(restResources.brukersNavKontor.actions.reset, fnr);
-    useDispatchOnNewFnr(restResources.utbetalinger.actions.reset, fnr);
-    useDispatchOnNewFnr(restResources.pleiepenger.actions.reset, fnr);
-    useDispatchOnNewFnr(restResources.sykepenger.actions.reset, fnr);
-    useDispatchOnNewFnr(restResources.foreldrepenger.actions.reset, fnr);
-    useDispatchOnNewFnr(restResources.sendReferat.actions.reset, fnr);
-    useDispatchOnNewFnr(restResources.sendSpørsmål.actions.reset, fnr);
-    useDispatchOnNewFnr(restResources.sendSvar.actions.reset, fnr);
-    useDispatchOnNewFnr(restResources.leggTilbakeOppgave.actions.reset, fnr);
-    useDispatchOnNewFnr(restResources.tråderOgMeldinger.actions.reset, fnr);
-    useDispatchOnNewFnr(restResources.brukersVarsler.actions.reset, fnr);
-    useDispatchOnNewFnr(restResources.sakstema.actions.reset, fnr);
-    useDispatchOnNewFnr(restResources.oppfolging.actions.reset, fnr);
-    useDispatchOnNewFnr(restResources.opprettOppgave.actions.reset, fnr);
-
-    useFetchFeatureTogglesOnNewFnr();
-    useDispatchOnNewFnr(setIngenValgtTraadDialogpanel(), fnr);
+    const dispatch = useDispatch();
+    const fnr = useGjeldendeBruker();
+    const setFeatureToggleData = useRestResource(restReducers => restReducers.featureToggles).actions.setData;
 
     useEffect(() => {
         cache.clear();
-    }, [fnr]);
+        dispatch(reset());
+
+        fetchAllFeatureToggles().then(toggles => dispatch(setFeatureToggleData(toggles)));
+    }, [dispatch, reset, fnr]);
 
     return null;
 }
