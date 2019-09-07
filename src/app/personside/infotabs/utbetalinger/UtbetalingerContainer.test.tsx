d92448ff9f379948415350fbbf13da1cdcfa612b
Daniel Winsvold <danielwinsvold@gmail.com>
2019-09-07 16:46:06 +0200

    oppdaterer utbetalingercontainertest til å bare inneholde 1 utbetaling, hvis ikke blir snapshotet på flere tusen linjer

--- src/app/personside/infotabs/utbetalinger/UtbetalingerContainer.test.tsx
+++ src/app/personside/infotabs/utbetalinger/UtbetalingerContainer.test.tsx
@@ -2,10 +2,19 @@ import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 import TestProvider from '../../../../test/Testprovider';
 import UtbetalingerContainer from './UtbetalingerContainer';
+import { getTestStore } from '../../../../test/testStore';
+import { statiskMockUtbetaling } from '../../../../mock/statiskMockUtbetaling';
 
 test('Viser utbetalingercontainer med alt innhold', () => {
+    const testStore = getTestStore();
+    testStore.dispatch(
+        testStore.getState().restResources.utbetalinger.actions.setData({
+            utbetalinger: [statiskMockUtbetaling],
+            periode: { sluttDato: '1986-12-28', startDato: '1905-01-01' }
+        })
+    );
     const visittkortheader = renderer.create(
-        <TestProvider>
+        <TestProvider customStore={testStore}>
             <UtbetalingerContainer />
         </TestProvider>
     );
