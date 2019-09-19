0ac96b16b67fa4a475888296fc187e262dd7d7c9
Andreas Bergman <andreasbergman93@gmail.com>
2019-09-19 10:37:50 +0200

    oppdatere statisk mock traader

--- src/app/personside/infotabs/dyplenkeTest/meldingerDyplenker.test.tsx
+++ src/app/personside/infotabs/dyplenkeTest/meldingerDyplenker.test.tsx
@@ -5,10 +5,17 @@ import InfoTabs from '../InfoTabs';
 import { BrowserRouter } from 'react-router-dom';
 import { INFOTABS } from '../InfoTabEnum';
 import { getAktivTab, meldingerTest } from './utils';
+import { getTestStore } from '../../../../test/testStore';
+import { getMockTraader } from '../../../../mock/meldinger/meldinger-mock';
+import { aremark } from '../../../../mock/person/aremark';
 
 test('bytter til riktig tab og setter fokus på riktig melding ved bruk av dyplenke fra oversikt', () => {
+    const store = getTestStore();
+    store.dispatch(
+        store.getState().restResources.tråderOgMeldinger.actions.setData(getMockTraader(aremark.fødselsnummer))
+    );
     const infoTabs = mount(
-        <TestProvider>
+        <TestProvider customStore={store}>
             <BrowserRouter>
                 <InfoTabs />
             </BrowserRouter>
