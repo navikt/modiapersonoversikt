7a9c37a177898ffbe8398939af4b5ac184a67099
Nicklas Utgaard <nicklas.utgaard@nav.no>
2019-10-01 12:33:25 +0200

    [OPP-937] fjerner delay etter fiks av caching-busting backend

--- src/app/personside/dialogpanel/fortsettDialog/FortsettDialogContainer.tsx
+++ src/app/personside/dialogpanel/fortsettDialog/FortsettDialogContainer.tsx
@@ -80,9 +80,7 @@ function FortsettDialogContainer(props: Props) {
         const callback = () => {
             dispatch(resetPlukkOppgaveResource);
             dispatch(reloadTildelteOppgaver);
-            setTimeout(() => {
-                dispatch(reloadMeldinger);
-            }, 2000); // TODO delay bør ikke være nødvendig her, sjekk backend!
+            dispatch(reloadMeldinger);
         };
         const erOppgaveTilknyttetAnsatt = state.oppgaveListe === OppgavelisteValg.MinListe;
         const oppgaveId = props.tilknyttetOppgave ? props.tilknyttetOppgave.oppgaveid : undefined;
