967477991cf5155a64cd9a106925b85863885010
Daniel Winsvold <daniel.winsvold@nav.no>
2019-10-10 14:24:31 +0200

    knapp for gå til neste spørsmål dersom det er igjen flere oppgaver på bruker

--- src/app/personside/dialogpanel/fellesStyling.tsx
+++ src/app/personside/dialogpanel/fellesStyling.tsx
@@ -9,6 +9,10 @@ import { Meldingstype } from '../../../models/meldinger/meldinger';
 import { meldingstypeTekst } from '../infotabs/meldinger/utils/meldingstekster';
 import { FailedPostResource } from '../../../rest/utils/postResource';
 import { useDispatch } from 'react-redux';
+import useTildelteOppgaver from '../../../utils/hooks/useTildelteOppgaver';
+import { setValgtTraadDialogpanel } from '../../../redux/oppgave/actions';
+import { useRestResource } from '../../../utils/customHooks';
+import { hasData } from '../../../rest/utils/restResource';
 
 export const FormStyle = styled.form`
     display: flex;
@@ -24,6 +28,8 @@ export const FormStyle = styled.form`
 `;
 
 export const DialogpanelKvitteringStyling = styled.div`
+    display: flex;
+    flex-direction: column;
     > *:not(:first-child) {
         margin-top: 1rem;
     }
@@ -49,6 +55,25 @@ export function DialogpanelKvittering(props: {
     meldingstype: Meldingstype;
     lukk: () => void;
 }) {
+    const tildelteOppgaver = useTildelteOppgaver();
+    const dispatch = useDispatch();
+    const traaderResource = useRestResource(resources => resources.tråderOgMeldinger);
+
+    const nesteOppgavePåBruker = tildelteOppgaver.paaBruker[0];
+    const gaaTilNesteSporsmaal = () => {
+        if (!nesteOppgavePåBruker || !hasData(traaderResource)) {
+            return;
+        }
+        const traadTilknyttetOppgave = traaderResource.data.find(
+            traad => traad.traadId === nesteOppgavePåBruker.henvendelseid
+        );
+        if (!traadTilknyttetOppgave) {
+            return;
+        }
+        props.lukk();
+        dispatch(setValgtTraadDialogpanel(traadTilknyttetOppgave));
+    };
+
     return (
         <DialogpanelKvitteringStyling>
             <VisuallyHiddenAutoFokusHeader tittel={props.tittel} />
@@ -57,6 +82,11 @@ export function DialogpanelKvittering(props: {
             <KnappBase type="standard" onClick={props.lukk}>
                 Lukk
             </KnappBase>
+            {nesteOppgavePåBruker && (
+                <KnappBase type="standard" onClick={gaaTilNesteSporsmaal}>
+                    Gå til neste spørsmål
+                </KnappBase>
+            )}
         </DialogpanelKvitteringStyling>
     );
 }
