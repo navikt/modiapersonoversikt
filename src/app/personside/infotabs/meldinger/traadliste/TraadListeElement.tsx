75dc6a06191ac6fd036c591263a78eacde6be432
Richard Borge <richard.borge@nav.no>
2019-10-21 12:53:50 +0200

    Legger til label for slettet tråd

--- src/app/personside/infotabs/meldinger/traadliste/TraadListeElement.tsx
+++ src/app/personside/infotabs/meldinger/traadliste/TraadListeElement.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { ReactNode } from 'react';
 import { Element, Normaltekst } from 'nav-frontend-typografi';
 import { Traad } from '../../../../../models/meldinger/meldinger';
 import VisMerKnapp from '../../../../../components/VisMerKnapp';
@@ -7,14 +8,14 @@ import { theme } from '../../../../../styles/personOversiktTheme';
 import { formatterDatoTid } from '../../../../../utils/dateUtils';
 import { erDelvisBesvart, erMonolog, meldingstittel, nyesteMelding } from '../utils/meldingerUtils';
 import Meldingsikon from '../utils/Meldingsikon';
-import { EtikettFokus, EtikettInfo, EtikettSuksess } from 'nav-frontend-etiketter';
+import { EtikettAdvarsel, EtikettFokus, EtikettInfo, EtikettSuksess } from 'nav-frontend-etiketter';
 import { useAppState, useOnMount } from '../../../../../utils/customHooks';
 import { UnmountClosed } from 'react-collapse';
 import useTildelteOppgaver from '../../../../../utils/hooks/useTildelteOppgaver';
 import { useInfotabsDyplenker } from '../../dyplenker';
 import { meldingerTest } from '../../dyplenkeTest/utils';
-import { ReactNode } from 'react';
 import { delAvStringMedDots } from '../../../../../utils/string-utils';
+import { Temagruppe } from '../../../../../models/Temagrupper';
 
 interface Props {
     traad: Traad;
@@ -102,6 +103,7 @@ function TraadListeElement(props: Props) {
                             </UnmountClosed>
                             {erDelvisBesvart(props.traad) && <EtikettInfo>Delvis besvart</EtikettInfo>}
                             <TildeltSaksbehandlerEtikett traadId={props.traad.traadId} />
+                            <SlettetEtikett temagruppe={sisteMelding.temagruppe} />
                         </EtikettStyling>
                     </div>
                 </PanelStyle>
@@ -115,6 +117,14 @@ function TildeltSaksbehandlerEtikett({ traadId }: { traadId: string }) {
 
     if (tildelteOppgaver.paaBruker.map(oppgave => oppgave.henvendelseid).includes(traadId)) {
         return <EtikettSuksess>Tildelt meg</EtikettSuksess>;
+    }
+
+    return null;
+}
+
+function SlettetEtikett({ temagruppe }: { temagruppe: Temagruppe }) {
+    if (temagruppe === Temagruppe.Null) {
+        return <EtikettAdvarsel>Slettet</EtikettAdvarsel>;
     }
 
     return null;
