16b71632c41dd38d10e20ddd79e2f58e992a3021
Andreas Bergman <andreas.bergman@nav.no>
2019-10-23 13:59:07 +0200

    boxshadow rundt feilmeldinger i ie og chrome

--- src/mock/meldinger/meldinger-mock.ts
+++ src/mock/meldinger/meldinger-mock.ts
@@ -32,7 +32,14 @@ export function getMockTraader(fødselsnummer: string): Traad[] {
         traadArray[2].traadId = MOCKED_TRAADID_3;
     }
 
-    return traadArray;
+    return traadArray.map(traad => {
+        const meldinger = traad.meldinger;
+        meldinger[0].id = traad.traadId;
+        return {
+            ...traad,
+            meldinger
+        };
+    });
 }
 
 export function getMockTraad(): Traad {
