2910d2823279fd3e18ec278c62d08b470da0a3d4
Daniel Winsvold <danielwinsvold@gmail.com>
2019-10-24 09:50:27 +0200

    fikser oversikt.test etter dato-sorteringsfix

--- src/app/personside/infotabs/oversikt/Oversikt.test.tsx
+++ src/app/personside/infotabs/oversikt/Oversikt.test.tsx
@@ -3,10 +3,6 @@ import * as renderer from 'react-test-renderer';
 import TestProvider from '../../../../test/Testprovider';
 import Oversikt from './Oversikt';
 
-beforeEach(() => {
-    Date.prototype.getTime = jest.fn(() => 0);
-});
-
 test('Viser oversikt med alt innhold', () => {
     const container = renderer.create(
         <TestProvider>
