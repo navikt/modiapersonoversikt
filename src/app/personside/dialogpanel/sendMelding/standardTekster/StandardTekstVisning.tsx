2e8e46ce5a13621b4739b2fbb4106e0733d3795f
Daniel Winsvold <danielwinsvold@gmail.com>
2019-11-12 11:04:16 +0100

    tilrettelegging for skjermleserbrukere i standardtekster

--- src/app/personside/dialogpanel/sendMelding/standardTekster/StandardTekstVisning.tsx
+++ src/app/personside/dialogpanel/sendMelding/standardTekster/StandardTekstVisning.tsx
@@ -166,6 +166,7 @@ function Preview({ tekst, locale, sokefelt, highlightRule }: PreviewProps) {
     }
     return (
         <PreviewWrapper>
+            <h3 className="sr-only">Forhåndsvisning</h3>
             <Systemtittel className="blokk-xs">{tekst && tekst.overskrift}</Systemtittel>
             <Tekstomrade rules={[ParagraphRule, highlightRule, LinkRule]} className="typo-normal blokk-m">
                 {tekst && tekst.innhold[locale]}
@@ -188,6 +189,7 @@ function StandardTekstVisning(props: Props) {
 
     return (
         <Container>
+            <h3 className="sr-only">Standardtekster</h3>
             <Liste className="standardtekster__liste">{tekstElementer}</Liste>
             <PreviewContainer>
                 <Preview
