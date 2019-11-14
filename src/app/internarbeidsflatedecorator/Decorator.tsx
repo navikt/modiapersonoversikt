b84201c6da292d57f84fcde6f5395511a458e81a
Nicklas Utgaard <nicklas.utgaard@nav.no>
2019-11-14 16:22:00 +0100

    [KAIZEN-0] henter sokFnr fra queryParam

--- src/app/internarbeidsflatedecorator/Decorator.tsx
+++ src/app/internarbeidsflatedecorator/Decorator.tsx
@@ -11,10 +11,12 @@ import { RouteComponentProps, withRouter } from 'react-router';
 import { getSaksbehandlerEnhet } from '../../utils/loggInfo/saksbehandlersEnhetInfo';
 import './personsokKnapp.less';
 import { useRestResource } from '../../utils/customHooks';
+import { parseQueryParams } from '../../utils/url-utils';
 
 const InternflateDecorator = NAVSPA.importer<DecoratorProps>('internarbeidsflatefs');
 
 function lagConfig(
+    sokFnr: string | undefined | null,
     fnr: string | undefined | null,
     enhet: string | undefined | null,
     history: History,
@@ -22,7 +24,7 @@ function lagConfig(
 ): DecoratorProps {
     return {
         appname: 'Modia personoversikt',
-        fnr,
+        fnr: sokFnr || fnr,
         enhet,
         toggles: {
             visEnhet: false,
@@ -52,12 +54,16 @@ function lagConfig(
         markup: {
             etterSokefelt:
                 '<button class="personsok-button" id="toggle-personsok" aria-label="Åpne avansert søk" title="Åpne avansert søk" data-apne="Åpne avansert søk" data-lukke="Lukk avansert søk"> <span> A <span class="personsok-pil"></span> </span> </button>'
-        }
+        },
+        autoSubmit: true
     };
 }
 
-function Decorator({ history }: RouteComponentProps<{}>) {
+function Decorator({ location, history }: RouteComponentProps<{}>) {
+    const queryParams = parseQueryParams(location.search);
+    const sokFnr = queryParams.sokFnr === '0' ? '' : queryParams.sokFnr;
     const fnr = useSelector((state: AppState) => state.gjeldendeBruker.fødselsnummer);
+
     const [enhet, settEnhet] = useState(getSaksbehandlerEnhet());
     const reloadMeldinger = useRestResource(resources => resources.tråderOgMeldinger.actions.reload);
     const dispatch = useDispatch();
@@ -65,7 +71,14 @@ function Decorator({ history }: RouteComponentProps<{}>) {
         dispatch(reloadMeldinger);
         settEnhet(enhet);
     };
-    const config = useCallback(lagConfig, [fnr, enhet, history, handleSetEnhet])(fnr, enhet, history, handleSetEnhet);
+    const config = useCallback(lagConfig, [sokFnr, fnr, enhet, history, handleSetEnhet])(
+        sokFnr,
+        fnr,
+        enhet,
+        history,
+        handleSetEnhet
+    );
+
     return (
         <nav id="header">
             <InternflateDecorator {...config} />
