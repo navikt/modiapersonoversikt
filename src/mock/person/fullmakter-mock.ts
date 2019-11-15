a1e025b17062d136514b3ffdb32bdf483d847370
Richard Borge <richard.borge@nav.no>
2019-11-15 14:35:37 +0100

    OPP-981 Modell + mock

---
+++ src/mock/person/fullmakter-mock.ts
@@ -0,0 +1,39 @@
+import { Fullmakt } from '../../models/person/fullmakter';
+import moment from 'moment';
+
+import { backendDatoformat, fyllRandomListe, vektetSjanse } from '../utils/mock-utils';
+import NavFaker from 'nav-faker/dist/navfaker';
+
+export function getMockFullmakter(faker: Faker.FakerStatic, navfaker: NavFaker): Fullmakt[] | undefined {
+    if (vektetSjanse(faker, 0.5)) {
+        return undefined;
+    }
+    return fyllRandomListe(() => getMockFullmakt(faker, navfaker), 4);
+}
+
+function getMockFullmakt(faker: Faker.FakerStatic, navfaker: NavFaker): Fullmakt {
+    let fullmaktsgiver = null;
+    let fullmektig = null;
+
+    if (vektetSjanse(faker, 0.5)) {
+        fullmaktsgiver = navfaker.personIdentifikator.fødselsnummer();
+    } else {
+        fullmektig = navfaker.personIdentifikator.fødselsnummer();
+    }
+
+    return {
+        fullmaktsgiver: fullmaktsgiver,
+        fullmektig: fullmektig,
+        omraade: getOmraade(faker),
+        gyldigFraOgMed: moment(faker.date.soon()).format(backendDatoformat),
+        gyldigTilOgMed: moment(faker.date.future(2)).format(backendDatoformat)
+    };
+}
+
+function getOmraade(faker: Faker.FakerStatic): string[] {
+    if (vektetSjanse(faker, 0.5)) {
+        return ['*'];
+    } else {
+        return ['AAP', 'DAG'];
+    }
+}
---
+++ src/models/person/fullmakter.ts
@@ -0,0 +1,7 @@
+export interface Fullmakt {
+    fullmaktsgiver: string | null;
+    fullmektig: string | null;
+    omraade: string[];
+    gyldigFraOgMed: string;
+    gyldigTilOgMed: string;
+}
