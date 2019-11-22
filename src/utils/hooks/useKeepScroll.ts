851bfbf867ba8335bc1c878b5a6c0bacd4162a6f
Daniel Winsvold <danielwinsvold@gmail.com>
2019-11-22 12:26:25 +0100

    OPP-975 legger på litt debouncing for å forhindre for mange kjøringer av storeScroll

--- src/utils/hooks/useKeepScroll.ts
+++ src/utils/hooks/useKeepScroll.ts
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { isTest } from '../environment';
 
 let scrollStore: ScrollValue = {};
@@ -23,15 +23,20 @@ const storeScroll = (name: string, position: ScrollPosition) => {
 };
 
 function useKeepScroll(ref: React.RefObject<HTMLElement>, keepScrollId: string) {
+    const [debounceTimer, setDebounceTimer] = useState();
     useEffect(() => {
         const scroll = getStoredScroll(keepScrollId);
         !isTest() && ref.current && ref.current.scrollTo(scroll.x, scroll.y);
     }, [keepScrollId, ref]);
 
     return () => {
-        const scrollLeft = (ref.current && ref.current.scrollLeft) || 0;
-        const scrollTop = (ref.current && ref.current.scrollTop) || 0;
-        storeScroll(keepScrollId, { x: scrollLeft, y: scrollTop });
+        clearTimeout(debounceTimer);
+        const timer = setTimeout(() => {
+            const scrollLeft = (ref.current && ref.current.scrollLeft) || 0;
+            const scrollTop = (ref.current && ref.current.scrollTop) || 0;
+            storeScroll(keepScrollId, { x: scrollLeft, y: scrollTop });
+        }, 150);
+        setDebounceTimer(timer);
     };
 }
 
