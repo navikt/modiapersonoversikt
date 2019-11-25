1d4ba22a1dc4b60c1a0ece1ad70a22a7673ee1ee
Daniel Winsvold <danielwinsvold@gmail.com>
2019-11-25 09:41:50 +0100

    metrikk fanget opp feil oppførsel. Flytter den slik at den faktisk logger hvor mange klikk vi har på knappen

--- src/app/personside/EkspanderDilaogpanelKnapp.tsx
+++ src/app/personside/EkspanderDilaogpanelKnapp.tsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { AppState } from '../../redux/reducers';
 import styled from 'styled-components';
 import theme, { pxToRem } from '../../styles/personOversiktTheme';
+import { loggEvent } from '../../utils/frontendLogger';
 
 const Style = styled.div`
     padding: 0.4rem;
@@ -15,13 +16,19 @@ const Style = styled.div`
 function EkspanderDilaogpanelKnapp() {
     const dialogpanelErEkspandert = useSelector((state: AppState) => state.ui.dialogPanel.ekspandert);
     const dispatch = useDispatch();
+
+    const handleClick = () => {
+        loggEvent('Toggle', 'MinimerDialogpanel');
+        return dispatch(toggleDialogpanel());
+    };
+
     return (
         <Style>
             <PilKnapp
                 width={pxToRem(30)}
                 beskrivelse={dialogpanelErEkspandert ? 'Skjul oppgavepanel' : 'Vis oppgavepanel'}
                 direction={dialogpanelErEkspandert ? 'right' : 'left'}
-                onClick={() => dispatch(toggleDialogpanel())}
+                onClick={handleClick}
             />
         </Style>
     );
