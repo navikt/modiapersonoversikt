8477c42ec003c115129ca3ea217809ab851a1a18
Richard Borge <Richard.Borge@nav.no>
2019-11-28 10:23:30 +0100

    OPP-980 Fikser typo i feature toggle

--- src/components/featureToggle/toggleIDs.ts
+++ src/components/featureToggle/toggleIDs.ts
@@ -2,5 +2,5 @@ export enum FeatureToggles {
     Tooltip = 'tooltip',
     SaksoversiktNyttVindu = 'saksoversikt-nytt-vindu',
     VisTilbakemelding = 'vis-tilbakemelding',
-    NyPersonforvalter = 'pdl-ny-brukerprofi'
+    NyPersonforvalter = 'pdl-ny-brukerprofil'
 }
