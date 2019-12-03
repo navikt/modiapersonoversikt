4b10767c63f0c346bceeccaef831d431fa863460
Daniel Winsvold <danielwinsvold@gmail.com>
2019-12-03 13:54:05 +0100

    OPP-747 sørger for at samme lenke brukes til ny brukerprofil både i lenke og hotkeys

--- src/app/brukerprofil/useUrlNyPersonforvalter.ts
+++ src/app/brukerprofil/useUrlNyPersonforvalter.ts
@@ -1,10 +1,15 @@
-import { useRestResource } from '../../utils/customHooks';
+import { useFødselsnummer, useRestResource } from '../../utils/customHooks';
 import { hasData } from '../../rest/utils/restResource';
 import { hentBaseUrl } from '../../redux/restReducers/baseurls';
 
 function useUrlNyPersonforvalter() {
     const baseUrlReosurce = useRestResource(resources => resources.baseUrl);
-    return hasData(baseUrlReosurce) ? hentBaseUrl(baseUrlReosurce.data, 'personforvalter') : '';
+    const fnr = useFødselsnummer();
+    const baseUrl = hasData(baseUrlReosurce) ? hentBaseUrl(baseUrlReosurce.data, 'personforvalter') : '';
+    if (!baseUrl || baseUrl === '') {
+        return '';
+    }
+    return `${baseUrl}?aktoerId=${fnr}`;
 }
 
 export default useUrlNyPersonforvalter;
