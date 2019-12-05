900010bbbfad6e27612bd1e188e901e97630ece6
Daniel Winsvold <danielwinsvold@gmail.com>
2019-12-05 09:53:57 +0100

    OPP-1030 metrikker for å kartlegge hvor mye tid som brukes i modia-personoversikt

--- src/utils/timeSpentMetricks.ts
+++ src/utils/timeSpentMetricks.ts
@@ -1,1 +1,28 @@
-export function setupTimeSpentMetricks() {}
+import { Timer } from './timer';
+import { loggEvent } from './frontendLogger';
+
+const timer = new Timer();
+
+const reportTime = () => {
+    const timeSpent = timer.getTime();
+    loggEvent('Report', 'TimeSpent', undefined, { ms: timeSpent });
+};
+
+const startTimer = () => {
+    timer.startTimer();
+};
+
+const handleVisibilityChange = () => {
+    const wasHidden = document.hidden;
+    if (wasHidden) {
+        reportTime();
+    } else {
+        startTimer();
+    }
+};
+
+export function setupTimeSpentMetricks() {
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+    window.addEventListener('beforeunload', reportTime);
+    startTimer();
+}
