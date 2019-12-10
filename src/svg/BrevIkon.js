9159bb861c0dd4f55a810921b2e81e2ee56a2adb
Daniel Winsvold <danielwinsvold@gmail.com>
2019-12-10 11:47:08 +0100

    Vise brev-ikon i trådliste dersom bruker har sendt inn spørsmål som ikke er besvart

--- src/svg/BrevIkon.js
+++ src/svg/BrevIkon.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 export default function BrevIkon(props) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} {...props}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 3 24 17" {...props}>
             <g stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" fill="none">
                 <path d="M22.5 17.012c0 .828-.672 1.5-1.5 1.5h-18c-.828 0-1.5-.672-1.5-1.5v-11c0-.829.672-1.5 1.5-1.5h18c.828 0 1.5.671 1.5 1.5v11zM22 5.012l-10 8-10-8" />
             </g>
