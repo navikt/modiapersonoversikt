f45f77d023060844ccb6e42ce75ea8f97acdb860
Daniel Winsvold <danielwinsvold@gmail.com>
2019-12-18 23:35:22 +0100

    nisselue-easteregg 2.0

--- src/app/internarbeidsflatedecorator/EasterEggs/DecoratorEasterEgg.tsx
+++ src/app/internarbeidsflatedecorator/EasterEggs/DecoratorEasterEgg.tsx
@@ -1,23 +1,6 @@
 import * as React from 'react';
-import styled, { keyframes } from 'styled-components';
-import nisselue from './nisselue.svg';
-import moment from 'moment';
 import { useErKontaktsenter } from '../../../utils/enheterUtils';
-
-const dropDown = keyframes`
-  from {
-    top: -10rem;
-  }
-`;
-
-const StyledImg = styled.img`
-    position: absolute;
-    height: 3rem;
-    top: 0;
-    left: 1.1rem;
-    transform: rotateY(180deg);
-    animation: ${dropDown} 1.5s backwards 1.5s;
-`;
+import Nisselue from './Nisselue';
 
 function DecoratorEasterEgg() {
     const erKontaktsenter = useErKontaktsenter();
@@ -25,14 +8,11 @@ function DecoratorEasterEgg() {
         return null;
     }
 
-    const today = moment();
-    const erJul = today.month() === 11 && 17 < today.date() && today.date() < 28;
-
-    if (erJul) {
-        return <StyledImg src={nisselue} alt="nisselue" />;
-    }
-
-    return null;
+    return (
+        <>
+            <Nisselue />
+        </>
+    );
 }
 
 export default DecoratorEasterEgg;
---
+++ src/app/internarbeidsflatedecorator/EasterEggs/Nisselue.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { useState } from 'react';
+import moment from 'moment';
+import nisselue from './nisselue.svg';
+import styled, { css, keyframes } from 'styled-components';
+import Sno from './Sno';
+
+const dropDown = keyframes`
+  from {
+    top: -10rem;
+  }
+`;
+
+const shake = keyframes`
+  from {
+    margin-left: -.2rem;
+  }
+  to {
+    margin-left: .2rem;
+  }
+`;
+
+const Position = styled.div`
+    position: absolute;
+    left: 1.1rem;
+    top: 0;
+    animation: ${dropDown} 1.5s backwards 1.5s;
+`;
+
+const StyledImg = styled.img<{ shake: boolean }>`
+    height: 3rem;
+    transform: rotateY(180deg);
+    ${props =>
+        props.shake &&
+        css`
+            animation: ${shake} 0.1s linear -0.05s 4.5 alternate;
+        `};
+`;
+
+function Nisselue() {
+    const today = moment();
+    const erJul = today.month() === 11 && 17 <= today.date() && today.date() <= 28;
+
+    const [sno, setSno] = useState(false);
+
+    if (erJul) {
+        return (
+            <Position>
+                {sno && <Sno />}
+                <StyledImg shake={sno} onClick={() => setSno(true)} src={nisselue} alt="nisselue" />
+            </Position>
+        );
+    }
+
+    return null;
+}
+
+export default Nisselue;
