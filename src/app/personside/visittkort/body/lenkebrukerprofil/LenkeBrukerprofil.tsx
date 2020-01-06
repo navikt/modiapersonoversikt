5b5425d6c2bae71310169f51febc435672393e62
Andreas Bergman <andreasbergman93@gmail.com>
2020-01-06 15:16:23 +0100

    OPP-928 fjerne sjekk om ny brukerprofil

--- src/app/personside/visittkort/body/lenkebrukerprofil/LenkeBrukerprofil.tsx
+++ src/app/personside/visittkort/body/lenkebrukerprofil/LenkeBrukerprofil.tsx
@@ -1,39 +1,14 @@
 import * as React from 'react';
-import { Person } from '../../../../../models/person/person';
-import { paths } from '../../../../routes/routing';
-import { Link } from 'react-router-dom';
 import { Normaltekst } from 'nav-frontend-typografi';
 import useUrlNyPersonforvalter from '../../../../brukerprofil/useUrlNyPersonforvalter';
 
-function LenkeBrukerprofilVisning(props: { nyModiaPersonoversikt: boolean; person: Person }) {
+function LenkeBrukerprofilVisning() {
     const personforvalterUrl = useUrlNyPersonforvalter();
-
-    if (props.nyModiaPersonoversikt) {
-        return (
-            <a className="lenke" href={personforvalterUrl} target={'_blank'} rel="noreferrer noopener">
-                <Normaltekst tag="span">Endre personopplysninger</Normaltekst>
-            </a>
-        );
-        return (
-            <Link className="lenke" to={`${paths.brukerprofil}/${props.person.fødselsnummer}`}>
-                <Normaltekst tag="span">Endre personopplysninger</Normaltekst>
-            </Link>
-        );
-    } else {
-        return (
-            <a className="lenke" href={personforvalterUrl} target={'_blank'} rel="noreferrer noopener">
-                <Normaltekst tag="span">Endre personopplysninger</Normaltekst>
-            </a>
-        );
-        return (
-            <a
-                className="lenke"
-                href={`${paths.legacyPersonPath}/${props.person.fødselsnummer}${paths.legacyBrukerprofil}`}
-            >
-                <Normaltekst tag="span">Endre personopplysninger</Normaltekst>
-            </a>
-        );
-    }
+    return (
+        <a className="lenke" href={personforvalterUrl} target={'_blank'} rel="noreferrer noopener">
+            <Normaltekst tag="span">Endre personopplysninger</Normaltekst>
+        </a>
+    );
 }
 
 export default LenkeBrukerprofilVisning;
