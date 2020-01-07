786dbffb5141786b2ab4ca7063079d2c457750c8
Jan Eirik B. Nævdal <jan.eirik.b.navdal@nav.no>
2020-01-07 12:07:38 +0100

    OPP-1024 Legge til mockmulighet for ulike statuskoder

---
+++ src/mock/utils/failureMiddleware.ts
@@ -0,0 +1,29 @@
+import { HandlerArgument, Middleware, ResponseData } from 'yet-another-fetch-mock';
+
+const failures: ResponseData[] = [
+    {
+        status: 500,
+        statusText: 'Internal server error'
+    },
+    {
+        status: 403,
+        statusText: 'Forbidden'
+    },
+    {
+        status: 404,
+        statusText: 'Not Found'
+    }
+];
+
+export function failurerateMiddleware(probabilityOfFailure: number): Middleware {
+    return (request: HandlerArgument, response: ResponseData) => {
+        return new Promise<ResponseData>(resolve => {
+            const rnd = Math.random();
+            if (rnd < probabilityOfFailure) {
+                resolve(failures[Math.random() * failures.length]);
+            } else {
+                resolve(response);
+            }
+        });
+    };
+}
