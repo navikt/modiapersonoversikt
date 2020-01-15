6b85b3e8600406116dfbd3f6908f78172a509c9c
Daniel Winsvold <danielwinsvold@gmail.com>
2020-01-15 12:38:56 +0100

    fikser feil med at printknapp vises i kvittering i oppgavepanel pluss litt rydding

--- src/app/personside/infotabs/meldinger/traadvisning/verktoylinje/Verktoylinje.tsx
+++ src/app/personside/infotabs/meldinger/traadvisning/verktoylinje/Verktoylinje.tsx
@@ -10,7 +10,7 @@ import { createRef, useCallback, useEffect, useRef } from 'react';
 import EkspanderKnapp from '../../../../../../components/EkspanderKnapp';
 import { useFødselsnummer, usePrevious } from '../../../../../../utils/customHooks';
 import { meldingstittel, nyesteMelding } from '../../utils/meldingerUtils';
-import usePrinter, { Printer } from '../../../../../../utils/UsePrinter';
+import usePrinter from '../../../../../../utils/UsePrinter';
 import { Normaltekst } from 'nav-frontend-typografi';
 import { LenkeKnapp } from '../../../../../../components/common-styled-components';
 import { apiBaseUri } from '../../../../../../api/config';
@@ -54,7 +54,11 @@ enum VerktøyPanel {
     OPPGAVE,
     MERK
 }
-function Print({ props, printer }: { props: Props; printer: Printer }) {
+
+function Print(props: Props) {
+    const printer = usePrinter();
+    const PrinterWrapper = printer.printerWrapper;
+
     const GammelPrintKnapp = styled(LenkeKnapp.withComponent('a'))`
         text-decoration: none;
         color: #3e3832;
@@ -62,15 +66,28 @@ function Print({ props, printer }: { props: Props; printer: Printer }) {
     const fnr = useFødselsnummer();
     const meldingerPrintFT = useFeatureToggle(FeatureToggles.MeldingerPrint);
 
-    if (meldingerPrintFT.isOn && props.visPrinter) {
-        return <PrintKnapp onClick={() => printer?.triggerPrint()} />;
+    if (!props.visPrinter) {
+        return null;
     }
+
+    if (meldingerPrintFT.isOn) {
+        return (
+            <>
+                <PrintKnapp onClick={() => printer?.triggerPrint()} />
+                <PrinterWrapper>
+                    <MeldingerPrintMarkup valgtTraad={props.valgtTraad} />
+                </PrinterWrapper>
+            </>
+        );
+    }
+
     return (
         <GammelPrintKnapp href={`${apiBaseUri}/dialog/${fnr}/${props.valgtTraad.traadId}/print`} download>
             <Normaltekst>Skriv ut</Normaltekst>
         </GammelPrintKnapp>
     );
 }
+
 function Verktoylinje(props: Props) {
     const titleRef = createRef<HTMLHeadingElement>();
     const [aktivtPanel, settAktivtPanel] = React.useState<VerktøyPanel | null>(null);
@@ -94,8 +111,7 @@ function Verktoylinje(props: Props) {
     const visJournalforing = aktivtPanel === VerktøyPanel.JOURNALFORING;
     const visOppgave = aktivtPanel === VerktøyPanel.OPPGAVE;
     const visMerk = aktivtPanel === VerktøyPanel.MERK;
-    const printer = usePrinter();
-    const PrinterWrapper = printer.printerWrapper;
+
     return (
         <StyledArticle aria-describedby={tittelId.current}>
             <h3 id={tittelId.current} className="sr-only" ref={titleRef} tabIndex={-1}>
@@ -115,7 +131,7 @@ function Verktoylinje(props: Props) {
                     />
                     <SvartLenkeKnapp onClick={togglePanel(VerktøyPanel.MERK)} open={visMerk} tittel="Merk" />
                 </OppgaveknapperStyle>
-                <Print props={props} printer={printer} />
+                <Print {...props} />
             </KnapperPanelStyle>
             <UnmountClosed isOpened={visJournalforing} hasNestedCollapse={true}>
                 <JournalforingPanel traad={props.valgtTraad} lukkPanel={lukk} />
@@ -126,9 +142,6 @@ function Verktoylinje(props: Props) {
             <UnmountClosed isOpened={visMerk}>
                 <MerkPanel valgtTraad={props.valgtTraad} lukkPanel={lukk} />
             </UnmountClosed>
-            <PrinterWrapper>
-                <MeldingerPrintMarkup valgtTraad={props.valgtTraad} />
-            </PrinterWrapper>
         </StyledArticle>
     );
 }
