4de05ade5c981500d39ca6311de984c26a1fc2f7
Ankur Tade <Ankur.Tade@nav.no>
2020-01-23 12:52:09 +0100

    Konsmetik fikser

--- src/app/personside/infotabs/ytelser/YtelserListe.tsx
+++ src/app/personside/infotabs/ytelser/YtelserListe.tsx
@@ -1,19 +1,23 @@
 import * as React from 'react';
 import { CenteredLazySpinner } from '../../../../components/LazySpinner';
 import styled from 'styled-components/macro';
-import theme from '../../../../styles/personOversiktTheme';
+import theme, { pxToRem } from '../../../../styles/personOversiktTheme';
 import { Ytelse } from '../../../../models/ytelse/ytelse-utils';
 import YtelserListeElement from './YtelserListeElement';
+import { Undertittel } from 'nav-frontend-typografi';
 
 const Styling = styled.section`
     ${theme.hvittPanel};
-    padding: ${theme.margin.layout};
 `;
 
 const YtelserListeStyle = styled.ol`
     > *:not(:first-child) {
         border-top: ${theme.border.skille};
     }
+`;
+
+const TittelWrapper = styled.div`
+    padding: ${pxToRem(15)};
 `;
 
 interface Props {
@@ -25,6 +29,9 @@ function YtelseListe(props: Props) {
     const ytelser = props.ytelser.map(ytelse => <YtelserListeElement ytelse={ytelse} />);
     return (
         <Styling>
+            <TittelWrapper>
+                <Undertittel>Ytelser</Undertittel>
+            </TittelWrapper>
             <YtelserListeStyle role="tablist">{ytelser}</YtelserListeStyle>
             {props.pending && <CenteredLazySpinner />}
         </Styling>
