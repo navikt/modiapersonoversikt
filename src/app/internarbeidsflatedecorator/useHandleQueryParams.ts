62c26b0dd3cc018e43d78fc96343e293e91c0ecf
Andreas Bergman <andreasbergman93@gmail.com>
2020-01-24 10:37:36 +0100

    OPP908-retter opp i kommentarer fra pr

--- src/app/internarbeidsflatedecorator/useHandleQueryParams.ts
+++ src/app/internarbeidsflatedecorator/useHandleQueryParams.ts
@@ -10,28 +10,30 @@ import { usePostResource } from '../../rest/consumer/usePostResource';
 function useHandleQueryParams() {
     const history = useHistory();
     const queryParams = useQueryParams<{ sokFnr?: string; oppgaveid?: string; behandlingsid?: string }>();
-    const sokFnr = queryParams.behandlingsid ? undefined : queryParams.sokFnr === '0' ? '' : queryParams.sokFnr;
+    const sokFnr = queryParams.sokFnr === '0' ? '' : queryParams.sokFnr;
     const plukkOppgaverResource = usePostResource(resources => resources.plukkNyeOppgaver);
     const dispatch = useDispatch();
 
     useOnMount(() => {
-        if (queryParams.behandlingsid && queryParams.sokFnr && queryParams.oppgaveid) {
-            dispatch(
-                plukkOppgaverResource.actions.setResponse([
-                    {
-                        oppgaveId: queryParams.oppgaveid,
-                        fødselsnummer: queryParams.sokFnr,
-                        traadId: queryParams.behandlingsid,
-                        fraGosys: true
-                    }
-                ])
-            );
+        if (queryParams.behandlingsid && queryParams.sokFnr) {
             history.push(
                 `${paths.personUri}/${queryParams.sokFnr}/${INFOTABS.MELDINGER.toLowerCase()}/${
                     queryParams.behandlingsid
                 }`
             );
-            loggEvent('Oppgave', 'FraGosys');
+            if (queryParams.oppgaveid) {
+                dispatch(
+                    plukkOppgaverResource.actions.setResponse([
+                        {
+                            oppgaveId: queryParams.oppgaveid,
+                            fødselsnummer: queryParams.sokFnr,
+                            traadId: queryParams.behandlingsid,
+                            fraGosys: true
+                        }
+                    ])
+                );
+                loggEvent('Oppgave', 'FraGosys');
+            }
         } else if (queryParams.sokFnr) {
             loggEvent('Oppslag', 'Puzzle');
             setNyBrukerIPath(history, queryParams.sokFnr);
