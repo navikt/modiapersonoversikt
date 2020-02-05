11926a9347497a7de82ecc42245eab3f131e6f27
Andreas Bergman <andreasbergman93@gmail.com>
2020-02-05 14:34:50 +0100

    fjerner litt aria-label og heading som gjorde skjermleser repetativt med flere skrik av samme tittel samt css

--- src/app/personside/dialogpanel/fortsettDialog/tidligereMeldinger/TidligereMeldinger.tsx
+++ src/app/personside/dialogpanel/fortsettDialog/tidligereMeldinger/TidligereMeldinger.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
-import { useRef } from 'react';
 import { Melding, Meldingstype, Traad } from '../../../../../models/meldinger/meldinger';
 import styled from 'styled-components/macro';
 import EnkeltMelding from './EnkeltMelding';
 import { theme } from '../../../../../styles/personOversiktTheme';
-import { guid } from 'nav-frontend-js-utils';
 import ErrorBoundary from '../../../../../components/ErrorBoundary';
 import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
 
@@ -16,19 +14,24 @@ const StyledArticle = styled.article`
     .ekspanderbartPanel__innhold {
         padding: 0rem;
     }
+    > *:not(:last-child) {
+        margin-bottom: 0.5rem;
+    }
     margin-top: 0.7rem;
 `;
 
 const StyledLi = styled.li`
     position: relative;
-    border-bottom: 1px solid ${theme.color.navGra20};
 `;
 
 const StyledOl = styled.ol`
     margin-top: 0.1rem;
-    border: ${theme.border.skille};
+    ${theme.hvittPanel}
     > li:not(:last-child) {
         margin-bottom: 0.1rem;
+    }
+    > li:not(:only-child) {
+        border-top: ${theme.border.skilleSvak};
     }
 `;
 
@@ -57,7 +60,7 @@ function Traadpanel(props: { traad: Melding[]; tittel: string; defaultApen: bool
     if (flereMeldinger) {
         return (
             <Ekspanderbartpanel apen={false} tittel={props.tittel} tag="undertittel" border={true}>
-                <StyledOl aria-label={props.tittel}>{meldinger}</StyledOl>
+                <StyledOl>{meldinger}</StyledOl>
             </Ekspanderbartpanel>
         );
     } else {
@@ -72,16 +75,12 @@ function TidligereMeldinger(props: Props) {
     const delsvar = props.traad.meldinger.filter(
         melding => melding.meldingstype === Meldingstype.DELVIS_SVAR_SKRIFTLIG
     );
-    const tittelId = useRef(guid());
 
     const defaultApen = delsvar.length > 0 || traadUtenDelviseSvar.length === 1;
 
     return (
         <ErrorBoundary boundaryName="Tidligere meldinger">
             <StyledArticle>
-                <h3 tabIndex={-1} className="sr-only" id={tittelId.current}>
-                    Tidligere meldinger
-                </h3>
                 <Traadpanel traad={traadUtenDelviseSvar} tittel="Tidligere meldinger" defaultApen={defaultApen} />
                 <Traadpanel traad={delsvar} tittel="Delsvar" defaultApen={defaultApen} />
             </StyledArticle>
