4bef7b026952c2024003b29917bef1e4e23698a7
Andreas Bergman <andreasbergman93@gmail.com>
2020-02-07 09:52:38 +0100

    [KAIZEN-0] slår opp riktig tråd når bruker kommer med henvendelse fra gosys med url som inneholder behandlingside og fnr

--- src/app/internarbeidsflatedecorator/useHandleGosysUrl.ts
+++ src/app/internarbeidsflatedecorator/useHandleGosysUrl.ts
@@ -3,11 +3,15 @@ import { useOnMount } from '../../utils/customHooks';
 import { loggEvent } from '../../utils/logger/frontendLogger';
 import { useDispatch } from 'react-redux';
 import { usePostResource } from '../../rest/consumer/usePostResource';
+import { useHistory } from 'react-router';
+import { INFOTABS } from '../personside/infotabs/InfoTabEnum';
+import { paths } from '../routes/routing';
 
 function useHandleGosysUrl() {
     const queryParams = useQueryParams<{ sokFnr?: string; oppgaveid?: string; behandlingsid?: string }>();
     const plukkOppgaverResource = usePostResource(resources => resources.plukkNyeOppgaver);
     const dispatch = useDispatch();
+    const history = useHistory();
 
     useOnMount(() => {
         if (queryParams.oppgaveid && queryParams.behandlingsid && queryParams.sokFnr) {
@@ -19,6 +23,12 @@ function useHandleGosysUrl() {
             };
             dispatch(plukkOppgaverResource.actions.setResponse([oppgave]));
             loggEvent('Oppgave', 'FraGosys');
+        } else if (queryParams.sokFnr && queryParams.behandlingsid) {
+            history.replace(
+                `${paths.personUri}/${queryParams.sokFnr}/${INFOTABS.MELDINGER.toLowerCase()}/${
+                    queryParams.behandlingsid
+                }`
+            );
         }
     });
 }
