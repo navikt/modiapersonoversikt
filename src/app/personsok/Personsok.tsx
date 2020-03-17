3feec95c990694016a4722dcd9c2750e908726e8
Nicklas Utgaard <nutgaard@gmail.com>
2020-03-17 08:36:31 +0100

    [KAIZEN-0] fikset imports og kodeformattering

--- src/app/personsok/Personsok.tsx
+++ src/app/personsok/Personsok.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import PersonsokSkjema from './PersonsokSkjema';
 import PersonsokResultat from './PersonsokResultat';
-import { useState } from 'react';
-import { useEffect } from 'react';
 import ModalWrapper from 'nav-frontend-modal';
 import styled from 'styled-components/macro';
 import theme from '../../styles/personOversiktTheme';
 import { Innholdstittel } from 'nav-frontend-typografi';
-import { loggEvent } from '../../utils/logger/frontendLogger';
 import useListener from '../../utils/hooks/use-listener';
 
 const StyledModalWrapper = styled(ModalWrapper)`
