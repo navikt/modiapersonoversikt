a62c45221c38438aa0bc476aab61bddb9955462a
Nicklas Utgaard <nutgaard@gmail.com>
2020-04-27 10:06:43 +0200

    [KAIZEN-0] freeze av useFetch-options for å hindre mutering fra adrum

---
+++ src/utils/freeze.ts
@@ -0,0 +1,10 @@
+export default function freeze<T extends { [key: string]: any }>(t: T): T {
+    const propNames = Object.getOwnPropertyNames(t);
+    for (let name in propNames) {
+        let value = t[name];
+        if (value && typeof value === 'object') {
+            freeze(value);
+        }
+    }
+    return Object.freeze(t);
+}
