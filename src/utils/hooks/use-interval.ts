2fa31299a467450f0ac9ebd25df3d2e43fca85e2
Nicklas Utgaard <nutgaard@gmail.com>
2020-05-15 13:26:16 +0200

    [KAIZEN-0] refresher token i en kort periode etter ny bruker har blitt slått opp

---
+++ src/utils/hooks/use-interval.ts
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+
+export default function useInterval(callback: () => void, delay: number | null) {
+    useEffect(() => {
+        if (delay !== null) {
+            let id = setInterval(callback, delay);
+            return () => clearInterval(id);
+        } else {
+            return () => {};
+        }
+    }, [callback, delay]);
+}
