ef1db2a346deaf194c34c3d68ddda6900ffbd416
Nicklas Utgaard <nutgaard@gmail.com>
2020-06-02 23:43:52 +0200

    [OPP-1174] gjorde craco-plugin generisk og flyttet config inn i mock-mappen

--- craco.config.js
+++ craco.config.js
@@ -1,16 +1,24 @@
 const { ESLINT_MODES } = require('@craco/craco');
 const CracoLessPlugin = require('craco-less');
+const fs = require('fs');
 
 const HostFilesPlugin = {
-    overrideDevServerConfig: ({ devServerConfig }) => {
+    overrideDevServerConfig: ({ devServerConfig, pluginOptions }) => {
         const originalBefore = devServerConfig.before;
+        const config = pluginOptions.configFiles
+            .map(configFile => fs.readFileSync(__dirname + configFile, 'UTF-8'))
+            .map(configFile => JSON.parse(configFile))
+            .reduce((acc, list) => acc.concat(list), [])
+            .filter(config => config.file && config.url);
+
         devServerConfig.before = (app, server, compiler) => {
-            app.get(
-                '/modiapersonoversikt-api/rest/saker/:fnr/dokument/:journalpostId/:dokumentReferanse',
-                (req, resp) => {
-                    resp.sendFile(__dirname + '/src/mock/saksoversikt/mock-dokument.pdf');
-                }
-            );
+            config.forEach(({ url, file }) => {
+                app.get(url, (req, resp) => {
+                    console.log(`[HostingFile] ${url} --> ${file}`);
+                    resp.sendFile(__dirname + file);
+                });
+            });
+
             originalBefore(app, server, compiler);
         };
         return devServerConfig;
@@ -26,11 +34,17 @@ const DisableAsciiOnly = {
     }
 };
 
+const hostFilePluginConfig = {
+    plugin: HostFilesPlugin,
+    options: {
+        configFiles: ['/src/mock/saksoversikt/mock-files.json']
+    }
+};
 module.exports = function({ env }) {
     return {
         eslint: {
             mode: ESLINT_MODES.file
         },
-        plugins: [{ plugin: HostFilesPlugin }, { plugin: DisableAsciiOnly }, { plugin: CracoLessPlugin }]
+        plugins: [hostFilePluginConfig, { plugin: DisableAsciiOnly }, { plugin: CracoLessPlugin }]
     };
 };
