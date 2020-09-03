d1c29b3f5ef85656e7e08e8bfc502228ca1b2490
Vilde Stefferud <qwartz.ms@hotmail.com>
2020-09-03 12:28:35 +0200

    [OPP-788] Harg lagt til funksjonalitet for forrige/neste knappene

--- src/app/notifikasjon/Notifikasjoner.tsx
+++ src/app/notifikasjon/Notifikasjoner.tsx
@@ -1,26 +1,41 @@
 import React, { useState } from 'react';
-import { FormStyle } from '../personside/dialogpanel/fellesStyling';
 import useNotifikasjoner from './useNotifikasjoner';
 import { CenteredLazySpinner } from '../../components/LazySpinner';
 import { AlertStripeInfo, AlertStripeFeil } from 'nav-frontend-alertstriper';
 import EnkeltNotifikasjon from './Notifikasjon';
-import { Nesteknapp } from 'nav-frontend-ikonknapper';
+import { Nesteknapp, Tilbakeknapp } from 'nav-frontend-ikonknapper';
 import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    text-align: center;
+`;
 
 const StyledNesteknapp = styled(Nesteknapp)`
     float: right;
 `;
 
+const StyledTilbakeknapp = styled(Tilbakeknapp)`
+    float: left;
+`;
+
+function ForrigeKnapp({ indeks, onClick }: { indeks: number; onClick: () => void }) {
+    if (indeks < 1) {
+        return null;
+    }
+    return <StyledTilbakeknapp onClick={onClick} />;
+}
+
+function NesteKnapp({ indeks, lengde, onClick }: { indeks: number; lengde: number; onClick: () => void }) {
+    if (indeks >= lengde - 1) {
+        return null;
+    }
+    return <StyledNesteknapp onClick={onClick} />;
+}
+
 function Notifikasjoner() {
     const notifikasjoner = useNotifikasjoner();
 
     const [indeks, setIndeks] = useState(0);
-
-    const neste = () => {
-        setIndeks(indeks + 1);
-    };
-
-    console.log(notifikasjoner);
 
     if (notifikasjoner.error) {
         return <AlertStripeFeil>Notifikasjoner er nede, vennligst prøv igjen senere.</AlertStripeFeil>;
@@ -44,17 +59,26 @@ function Notifikasjoner() {
         return b.prioritet - a.prioritet;
     });
 
+    const neste = () => {
+        setIndeks(indeks + 1);
+    };
+
+    const forrige = () => {
+        return setIndeks(indeks - 1);
+    };
+
     const currentNotifikasjon = sortertNotifikasjonArray[indeks];
 
     return (
-        <form>
-            <FormStyle>
-                <section>
-                    <EnkeltNotifikasjon notifikasjon={currentNotifikasjon} />
-                </section>
-                <StyledNesteknapp onClick={neste} />
-            </FormStyle>
-        </form>
+        <>
+            <section>
+                <EnkeltNotifikasjon notifikasjon={currentNotifikasjon} />
+            </section>
+            <StyledDiv>
+                <ForrigeKnapp indeks={indeks} onClick={forrige} />
+                <NesteKnapp indeks={indeks} lengde={sortertNotifikasjonArray.length} onClick={neste} />
+            </StyledDiv>
+        </>
     );
 }
 
