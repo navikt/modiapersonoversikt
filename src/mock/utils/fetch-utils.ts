d08c3dd9a2b3f6e3854269f99bebee668d23835f
Nicklas Utgaard <nutgaard@gmail.com>
2020-10-28 11:00:50 +0100

    [OPP-1019] la til verifikasjons-mekanisme i mocken
    kall som vi forventer skal ha enhet som queryParameter vil feile i mocken om det mangler.

--- src/mock/utils/fetch-utils.ts
+++ src/mock/utils/fetch-utils.ts
@@ -8,6 +8,17 @@ export function withDelayedResponse(
     return (req, res, ctx) => res(ctx.delay(delay), ctx.status(statusCode(req)), ctx.json(genererMockData(req)));
 }
 
+export function verify(isInvalid: (req: MockRequest) => string | undefined, handler: MockHandler): MockHandler {
+    return (req, res, ctx) => {
+        const invalid = isInvalid(req);
+        if (invalid) {
+            return res(ctx.status(500), ctx.statusText(invalid));
+        } else {
+            return handler(req, res, ctx);
+        }
+    };
+}
+
 export function mockGeneratorMedFødselsnummer(fn: (fødselsnummer: string) => object | object[] | undefined) {
     return (args: MockRequest) => fn(args.pathParams.fodselsnummer);
 }
