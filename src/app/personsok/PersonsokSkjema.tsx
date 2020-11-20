a945cc1b5c15c044b4e635ed72c946b5d750bf38
vildestefferud <qwartz.ms@hotmail.com>
2020-11-20 14:48:55 +0100

    [KAIZEN-0] eendret beskrivelsen til søkefelt til utenlandskID

--- src/app/personsok/PersonsokSkjema.tsx
+++ src/app/personsok/PersonsokSkjema.tsx
@@ -238,7 +238,7 @@ function PersonsokSkjema(props: Props) {
                         {enabled && (
                             <Input
                                 bredde={'L'}
-                                label={'Utenlandsk ID'}
+                                label={'Utenlandsk ID (med spesialtegn)'}
                                 {...state.fields.utenlandskID.input}
                                 feil={feilmelding(state.fields.utenlandskID)}
                             />
