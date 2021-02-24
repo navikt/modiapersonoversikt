ae8ef346a0cec64edd617e8d908520d75516639e
vildestefferud <qwartz.ms@hotmail.com>
2021-02-24 10:00:46 +0100

    [OPP-1153] Fikser mapping til å matche pdl

--- src/app/personside/visittkort/body/vergemal/VergemalMapper.tsx
+++ src/app/personside/visittkort/body/vergemal/VergemalMapper.tsx
@@ -5,8 +5,8 @@ export const vergesakstypeMapper: VergemaalMapper = {
     ensligMindreaarigFlyktning: 'Enslig mindreårig flyktning inklusive midlertidige saker for denne gruppen',
     voksen: 'Voksen',
     midlertidigForVoksen: 'Voksen midlertidig',
-    mindreaarig: 'Mindreårig',
-    midlertidigForMindreaarig: 'Mindreårig midlertidig',
+    mindreaarig: 'Mindreårig (unntatt EMF)',
+    midlertidigForMindreaarig: 'Mindreårig midlertidig (unntatt EMF)',
     forvaltningUtenforVergemaal: 'Forvaltning utenfor vergemål',
     stadfestetFremtidsfullmakt: 'Fremtidsfullmakt',
     undefined: 'Ingen vergesakstype oppgitt'
@@ -14,7 +14,7 @@ export const vergesakstypeMapper: VergemaalMapper = {
 
 export const vergeOmfangMapper: VergemaalMapper = {
     utlendingssakerPersonligeOgOekonomiskeInteresser:
-        'Ivareta personens interesser innenfor det personlige og økonomiske området herunder utlendingssaken',
+        'Ivareta personens interesser innenfor det personlige og økonomiske området herunder utlendingssaken (kun for EMA)',
     personligeOgOekonomiskeInteresser: 'Ivareta personens interesser innenfor det personlige og økonomiske området',
     oekonomiskeInteresser: 'Ivareta personens interesser innenfor det økonomiske området',
     personligeInteresser: 'Ivareta personens interesser innenfor det personlige området',
