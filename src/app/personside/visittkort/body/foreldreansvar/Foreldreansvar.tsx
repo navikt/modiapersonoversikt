0bdf792c64bb24b875e33698d04f260b0f686139
Andreas Bergman <andreasbergman93@gmail.com>
2021-03-14 22:03:51 +0100

    [OPP-1282] fiks av navn etter test brakk

--- src/app/personside/visittkort/body/foreldreansvar/Foreldreansvar.tsx
+++ src/app/personside/visittkort/body/foreldreansvar/Foreldreansvar.tsx
@@ -3,19 +3,24 @@ import { VisittkortGruppe } from '../VisittkortStyles';
 import Utropstegn from '../../../../../svg/Utropstegn';
 import { Foreldreansvar } from '../../../../../models/foreldreansvar/foreldreansvar';
 import { Element, Normaltekst } from 'nav-frontend-typografi';
+import VisittkortElement from '../VisittkortElement';
 
 function ForeldreansvarVisittkort(props: { foreldreansvar: Foreldreansvar }) {
     return (
         <VisittkortGruppe ikon={<Utropstegn />} tittel="Foreldreansvar">
-            <Element>
-                {props.foreldreansvar.ansvarlig ? props.foreldreansvar.ansvarlig.sammensatt : 'Navn ikke tilgjengelig'}
-            </Element>
-            <Normaltekst>Ansvar: {props.foreldreansvar.ansvar}</Normaltekst>
-            <Normaltekst>
-                {props.foreldreansvar.ansvarligUtenIdentifikator.navn} (
-                {props.foreldreansvar.ansvarligUtenIdentifikator.foedselsdato})
-            </Normaltekst>
-            <Normaltekst>{props.foreldreansvar.ansvarligUtenIdentifikator.statsborgerskap}</Normaltekst>
+            <VisittkortElement>
+                <Element>
+                    {props.foreldreansvar.ansvarlig
+                        ? props.foreldreansvar.ansvarlig.sammensatt
+                        : 'Navn ikke tilgjengelig'}
+                </Element>
+                <Normaltekst>Ansvar: {props.foreldreansvar.ansvar}</Normaltekst>
+                <Normaltekst>
+                    {props.foreldreansvar.ansvarligUtenIdentifikator.navn?.sammensatt} (
+                    {props.foreldreansvar.ansvarligUtenIdentifikator.foedselsdato})
+                </Normaltekst>
+                <Normaltekst>{props.foreldreansvar.ansvarligUtenIdentifikator.statsborgerskap}</Normaltekst>
+            </VisittkortElement>
         </VisittkortGruppe>
     );
 }
