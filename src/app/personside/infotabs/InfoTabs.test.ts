f64f86a3dea9db3511067a15898fa4a3e55db68b
Andreas Bergman <andreasbergman93@gmail.com>
2021-04-07 12:53:43 +0200

    [KAIZEN-0] fix av test med ny config

--- src/app/personside/infotabs/InfoTabs.test.ts
+++ src/app/personside/infotabs/InfoTabs.test.ts
@@ -1,18 +1,18 @@
-import { INFOTABS, InfotabsType } from './InfoTabEnum';
+import { InfotabConfig, INFOTABS } from './InfoTabEnum';
 import { getOpenTabFromRouterPath } from './utils/useOpenTab';
 
 test('henter riktig tab fra routerpath', () => {
     const path = 'http://localhost:7777/modiapersonoversikt/person/1000000000/' + INFOTABS.UTBETALING.path + '/';
 
-    const apenLamell: InfotabsType = getOpenTabFromRouterPath(path);
+    const apenLamell: InfotabConfig = getOpenTabFromRouterPath(path);
 
-    expect(apenLamell.toLowerCase()).toEqual(INFOTABS.UTBETALING.path);
+    expect(apenLamell.path).toEqual(INFOTABS.UTBETALING.path);
 });
 
 test('åpner oversikt som default hvis url ikke matcher en infotab', () => {
     const path = 'http://localhost:7777/modiapersonoversikt/person/1000000000/' + 'tull&tøys' + '/';
 
-    const apenLamell: InfotabsType = getOpenTabFromRouterPath(path);
+    const apenLamell: InfotabConfig = getOpenTabFromRouterPath(path);
 
-    expect(apenLamell.toLowerCase()).toEqual(INFOTABS.OVERSIKT.path);
+    expect(apenLamell.path).toEqual(INFOTABS.OVERSIKT.path);
 });
