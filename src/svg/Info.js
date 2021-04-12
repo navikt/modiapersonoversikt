d6f72a9498aed6ac34de28bd861f1137b636448a
Andreas Bergman <andreasbergman93@gmail.com>
2021-04-12 15:38:54 +0200

    [KAIZEN-0] leger til info-ikon paa foreldreansvar

---
+++ src/svg/Info.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export default function Infotegn(props) {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 24 24" fill="#0067C5">
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M12 0c6.627 0 12 5.373 12 12s-5.373 12-12 12S0 18.627 0 12 5.373 0 12 0zm0 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zM9 19v-2h2v-5H9v-2h4v7h2v2H9zm3-14a1.5 1.5 0 110 3 1.5 1.5 0 010-3z"
+                fill="#0067C5"
+            ></path>
+        </svg>
+    );
+}
