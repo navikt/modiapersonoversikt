d897db5d92a1734ca0fe1aaa30ba5891c17d1eee
Nicklas Utgaard <nutgaard@gmail.com>
2021-06-02 13:00:44 +0200

    [OPP-1324] etablere featuretoggle for visning av "sett til egen oppgaveliste"

--- src/mock/featureToggle-mock.ts
+++ src/mock/featureToggle-mock.ts
@@ -5,6 +5,8 @@ export function mockFeatureToggle(toggleId: FeatureToggles): FeatureToggleRespon
     switch (toggleId) {
         case FeatureToggles.VisTilbakemelding:
             return true;
+        case FeatureToggles.SettTilEgenOppgaveliste:
+            return true;
         default:
             return Math.random() > 0.5;
     }
