3f60fb38bafd75bdbbc31ab40903589202c6ae11
Eirik Dahlen <dahleneirik@gmail.com>
2021-09-23 14:58:28 +0200

    [KAIZEN-0] refaktorere komponent som ikke bruker propsene sine

--- src/app/personside/visittkort/body/lenkebrukerprofil/LenkeBrukerprofilContainer.tsx
+++ src/app/personside/visittkort/body/lenkebrukerprofil/LenkeBrukerprofilContainer.tsx
@@ -1,13 +1,6 @@
 import * as React from 'react';
 import LenkeBrukerprofilVisning from './LenkeBrukerprofil';
-import { Person } from '../../../../../models/person/person';
 import styled from 'styled-components/macro';
-
-interface OwnProps {
-    person: Person;
-}
-
-type Props = OwnProps;
 
 const PlaceBottomRight = styled.div`
     display: flex;
@@ -16,14 +9,12 @@ const PlaceBottomRight = styled.div`
     flex-grow: 1;
 `;
 
-class LenkeBrukerprofilContainer extends React.Component<Props> {
-    render() {
-        return (
-            <PlaceBottomRight>
-                <LenkeBrukerprofilVisning />
-            </PlaceBottomRight>
-        );
-    }
+function LenkeBrukerprofilContainer() {
+    return (
+        <PlaceBottomRight>
+            <LenkeBrukerprofilVisning />
+        </PlaceBottomRight>
+    );
 }
 
 export default LenkeBrukerprofilContainer;
