b353f5a42b1f855a3128a755932a861de82fc820
Eirik Dahlen <dahleneirik@gmail.com>
2021-09-29 23:34:50 +0200

    [OPP-1390] WIP kommentere ut logikk for å gi advarsel ved dødt barn

--- src/app/personside/visittkort-v2/header/status/AntallBarn.tsx
+++ src/app/personside/visittkort-v2/header/status/AntallBarn.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components/macro';
-import { ReactComponent as AdvarselIkonSvg } from 'nav-frontend-ikoner-assets/assets/advarsel-sirkel-fyll.svg';
+// import { ReactComponent as AdvarselIkonSvg } from 'nav-frontend-ikoner-assets/assets/advarsel-sirkel-fyll.svg';
 import { ForelderBarnRelasjon } from '../../PersondataDomain';
 import { hentBarn, hentBarnUnder21 } from '../../person-utils';
 
@@ -11,34 +11,34 @@ interface Props {
 const ListElementMedIkon = styled.span`
     display: inline-flex;
 `;
-const AdvarselIkon = styled(AdvarselIkonSvg)`
-    width: 1rem;
-    margin-top: -0.125rem;
-    margin-left: 0.125rem;
-    margin-right: 0.125rem;
-`;
+// const AdvarselIkon = styled(AdvarselIkonSvg)`
+//     width: 1rem;
+//     margin-top: -0.125rem;
+//     margin-left: 0.125rem;
+//     margin-right: 0.125rem;
+// `;
 
-function lagAdvarsel(barn: ForelderBarnRelasjon[]): React.ReactNode {
-    const dode = barn.some(b => erDod(b.tilPerson.personstatus));
-    if (dode) {
-        return <AdvarselIkon title="Ett eller flere av barnene har status som død" />;
-    }
-    return null;
-}
+// function lagAdvarselOmDodtBarn(barn: ForelderBarnRelasjon[]): React.ReactNode {
+//     const dode = barn.some(b => erDod(b.tilPerson.personstatus));
+//     if (dode) {
+//         return <AdvarselIkon title="Ett eller flere av barnene har status som død" />;
+//     }
+//     return null;
+// }
 
 export function AntallBarn({ forelderBarnRelasjon }: Props) {
-    const barnUnder21 = hentBarnUnder21(forelderBarnRelasjon);
     const barn = hentBarn(forelderBarnRelasjon);
     if (barn.length === 0) {
         return null;
     }
+    const barnUnder21 = hentBarnUnder21(forelderBarnRelasjon);
     if (barnUnder21.length === 0) {
         return <li title="Barn under 21 år">Ingen barn under 21 år</li>;
     }
-    const advarsel = lagAdvarsel(barnUnder21);
+    // const advarselOmDodtBarn = lagAdvarselOmDodtBarn(barnUnder21);
     return (
         <ListElementMedIkon title="Barn under 21 år">
-            {advarsel}
+            {/* {advarselOmDodtBarn} */}
             {barnUnder21.length} barn under 21 år
         </ListElementMedIkon>
     );
