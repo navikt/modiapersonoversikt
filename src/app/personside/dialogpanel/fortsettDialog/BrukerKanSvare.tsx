a67aa9f2fd8cce3ce2873376ea2bebce7c6cb079
Nicklas Utgaard <nutgaard@gmail.com>
2021-10-07 19:25:33 +0200

    [KAIZEN-0] fikset bug ved feilmelding
    ved flytting av "velg sak" inn til modalen ble ikke feilmeldinger korrekt flyttet ut.
    
    Dette ble fanget opp når man lager en ny tråd, men feilen var fortsatt tilstede ved fortsettelse av tråden.

--- src/app/personside/dialogpanel/fortsettDialog/BrukerKanSvare.tsx
+++ src/app/personside/dialogpanel/fortsettDialog/BrukerKanSvare.tsx
@@ -3,8 +3,8 @@ import Oppgaveliste from '../sendMelding/Oppgaveliste';
 import DialogpanelVelgSak from '../sendMelding/DialogpanelVelgSak';
 import styled from 'styled-components/macro';
 import { FortsettDialogValidator } from './validatorer';
-import { AlertStripeInfo } from 'nav-frontend-alertstriper';
 import { FortsettDialogState } from './FortsettDialogTypes';
+import { SkjemaelementFeilmelding } from 'nav-frontend-skjema';
 
 interface Props {
     formState: FortsettDialogState;
@@ -19,6 +19,7 @@ const Style = styled.div`
 `;
 
 function BrukerKanSvare(props: Props) {
+    const visFeilmelding = !FortsettDialogValidator.sak(props.formState) && props.formState.visFeilmeldinger;
     return (
         <Style>
             <AlertStripeInfo>Gir varsel, bruker kan svare</AlertStripeInfo>
@@ -27,11 +28,14 @@ function BrukerKanSvare(props: Props) {
                 setOppgaveliste={oppgaveliste => props.updateFormState({ oppgaveListe: oppgaveliste })}
             />
             {props.visVelgSak && (
-                <DialogpanelVelgSak
-                    setValgtSak={sak => props.updateFormState({ sak: sak })}
-                    valgtSak={props.formState.sak}
-                    visFeilmelding={!FortsettDialogValidator.sak(props.formState) && props.formState.visFeilmeldinger}
-                />
+                <>
+                    <DialogpanelVelgSak
+                        setValgtSak={sak => props.updateFormState({ sak: sak })}
+                        valgtSak={props.formState.sak}
+                        visFeilmelding={visFeilmelding}
+                    />
+                    {visFeilmelding ? <SkjemaelementFeilmelding>Du må velge sak </SkjemaelementFeilmelding> : null}
+                </>
             )}
         </Style>
     );
