0699dca4c8f0334d20426782910dbad3d81d68a0
vildestefferud <qwartz.ms@hotmail.com>
2021-10-08 14:17:30 +0200

    [OPP-1390] Legger til telefonnummer i kontaktinformasjon

--- src/app/personside/visittkort-v2/body/kontaktinformasjon/Kontaktinformasjon.tsx
+++ src/app/personside/visittkort-v2/body/kontaktinformasjon/Kontaktinformasjon.tsx
@@ -5,7 +5,7 @@ import { VisittkortGruppe } from '../VisittkortStyles';
 import KontaktinformasjonDodsbo from './dodsbo/Dodsbo';
 import Adresse from './adresse/Adresse';
 import Epost from './epost/Epost';
-// import Telefon from './telefon/Telefon';
+import Telefon from './telefon/Telefon';
 
 interface Props {
     person: Person;
@@ -17,8 +17,8 @@ export default function Kontaktinformasjon({ person }: Props) {
             <KontaktinformasjonDodsbo person={person} />
             <Adresse person={person} />
             <Epost person={person} />
-            {/* <Telefon person={person} />
-            <Bankkonto person={person} /> */}
+            <Telefon person={person} />
+            {/* <Bankkonto person={person} /> */}
         </VisittkortGruppe>
     );
 }
---
+++ src/app/personside/visittkort-v2/body/kontaktinformasjon/telefon/Telefon.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { Normaltekst } from 'nav-frontend-typografi';
+import EtikettGraa from '../../../../../../components/EtikettGraa';
+import PhoneIkon from '../../../../../../svg/Phone';
+import { formaterDato } from '../../../../../../utils/string-utils';
+import { formaterMobiltelefonnummer } from '../../../../../../utils/telefon-utils';
+import VisittkortElement from '../../VisittkortElement';
+import { DigitalKontaktinformasjon, Person } from '../../../PersondataDomain';
+
+interface Props {
+    person: Person;
+}
+
+function MobiltelefonVisning({ kontaktinformasjon }: { kontaktinformasjon: DigitalKontaktinformasjon }) {
+    if (kontaktinformasjon.reservasjon === 'true') {
+        return (
+            <>
+                <Normaltekst>Reservert</Normaltekst>
+                <EtikettGraa>I Kontakt- og reservasjonsregisteret</EtikettGraa>
+            </>
+        );
+    } else if (kontaktinformasjon.mobiltelefonnummer) {
+        const formatertDato = kontaktinformasjon.mobiltelefonnummer.sistOppdatert
+            ? formaterDato(kontaktinformasjon.mobiltelefonnummer.sistOppdatert)
+            : null;
+        const formatertTelefonnummer = kontaktinformasjon.mobiltelefonnummer.value
+            ? formaterMobiltelefonnummer(kontaktinformasjon.mobiltelefonnummer.value)
+            : null;
+        return (
+            <>
+                <Normaltekst>{formatertTelefonnummer}</Normaltekst>
+                <EtikettGraa>Endret {formatertDato} i Kontakt- og reservasjonsregisteret</EtikettGraa>
+            </>
+        );
+    } else {
+        return <Normaltekst>Ikke registrert</Normaltekst>;
+    }
+}
+
+function Telefon(props: Props) {
+    const kontaktinformasjon = props.person.kontaktOgReservasjon;
+
+    if (!kontaktinformasjon) {
+        return null;
+    }
+
+    return (
+        <VisittkortElement beskrivelse="Telefon" ikon={<PhoneIkon />}>
+            <MobiltelefonVisning kontaktinformasjon={kontaktinformasjon} />
+        </VisittkortElement>
+    );
+}
+
+export default Telefon;
