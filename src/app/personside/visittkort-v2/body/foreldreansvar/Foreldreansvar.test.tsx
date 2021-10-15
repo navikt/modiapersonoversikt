d43c6b19d44172614a81b050dafb0abe2078066b
Eirik Dahlen <dahleneirik@gmail.com>
2021-10-15 10:44:36 +0200

    [OPP-1390] legge til test for foreldreansvar

---
+++ src/app/personside/visittkort-v2/body/foreldreansvar/Foreldreansvar.test.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+import { aremark } from '../../../../../mock/persondata/aremark';
+import TestProvider from '../../../../../test/Testprovider';
+import Foreldreansvar from './Foreldreansvar';
+
+test('viser foreldreansvar', () => {
+    const visittkortbody = renderer.create(
+        <TestProvider>
+            <Foreldreansvar foreldreansvar={aremark.foreldreansvar} />
+        </TestProvider>
+    );
+
+    expect(visittkortbody.toJSON()).toMatchSnapshot();
+});
