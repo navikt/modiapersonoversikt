98e1d350810bb22aecd66d032c2e39cef50ae9a4
Nicklas Utgaard <nutgaard@gmail.com>
2022-01-05 08:49:10 +0100

    [OPP-1197] legge til egen ft for bruk av pdl-personsok fra frontend
    muliggjør at man kan skru på pdl-personsok for enkeltbrukere og ikke bare som eksperiment

--- src/components/featureToggle/toggleIDs.ts
+++ src/components/featureToggle/toggleIDs.ts
@@ -1,4 +1,5 @@
 export enum FeatureToggles {
     VisTilbakemelding = 'vis-tilbakemelding',
     BrukSalesforceDialoger = 'bruker-salesforce-dialoger',
+    BrukPdlPersonsok = 'bruker-pdl-personsok'
 }
