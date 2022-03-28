76441c5de4679aab624de7963df18ac430a1eca5
vildestefferud <qwartz.ms@hotmail.com>
2022-03-28 09:23:44 +0200

    [OPP-1437] Legger til melding om hva som har feilet

--- src/app/personside/visittkort-v2/header/FeilendeSystemerModal.tsx
+++ src/app/personside/visittkort-v2/header/FeilendeSystemerModal.tsx
@@ -5,6 +5,7 @@ import KnappBase from 'nav-frontend-knapper';
 import styled from 'styled-components/macro';
 import { Normaltekst, Systemtittel } from 'nav-frontend-typografi';
 import { ReactComponent as ErrorIkon } from '../../../../svg/alvorlig-advarsel.svg';
+import { InformasjonElement } from '../PersondataDomain';
 
 const Modal = styled(RawModal)`
     text-align: center;
@@ -20,11 +21,66 @@ interface Props {
 
 function FeilendeSystemerModal(props: Props) {
     const [open, setOpen] = useState(props.feilendeSystemer.isNotEmpty());
-    const feilendeSystem = props.feilendeSystemer.map((it, index) => (
-        <li key={index} className="blokk-xxs">
-            <Normaltekst>{it}</Normaltekst>
-        </li>
-    ));
+    const feilendeSystem = props.feilendeSystemer.map((it, index) => {
+        if (it === InformasjonElement.EGEN_ANSATT) {
+            return (
+                <li key={index} className="blokk-xxs">
+                    <Normaltekst>Vi får ikke hentet informasjon om Egen Ansatt fra NOM.</Normaltekst>
+                </li>
+            );
+        } else if (it === InformasjonElement.BANKKONTO) {
+            return (
+                <li key={index} className="blokk-xxs">
+                    <Normaltekst>Vi får ikke hentet informasjon om bankkonto fra TPS.</Normaltekst>
+                </li>
+            );
+        } else if (it === InformasjonElement.DKIF) {
+            return (
+                <li key={index} className="blokk-xxs">
+                    <Normaltekst>Vi får ikke hentet digital kontaktinformasjon fra DKIF.</Normaltekst>
+                </li>
+            );
+        } else if (it === InformasjonElement.PDL_GT) {
+            return (
+                <li key={index} className="blokk-xxs">
+                    <Normaltekst>Vi får ikke hentet informasjon om geografisk tilknytning fra PDL.</Normaltekst>
+                </li>
+            );
+        } else if (it === InformasjonElement.NORG_KONTAKTINFORMASJON) {
+            return (
+                <li key={index} className="blokk-xxs">
+                    <Normaltekst>Vi får ikke hentet kontaktinformasjonen fra NORG.</Normaltekst>
+                </li>
+            );
+        } else if (it === InformasjonElement.NORG_NAVKONTOR) {
+            return (
+                <li key={index} className="blokk-xxs">
+                    <Normaltekst>Vi får ikke hentet informasjon om NAV kontor fra NORG.</Normaltekst>
+                </li>
+            );
+        } else if (it === InformasjonElement.VEILEDER_ROLLER) {
+            return (
+                <li key={index} className="blokk-xxs">
+                    <Normaltekst>Vi får ikke hentet informasjon om veileders roller.</Normaltekst>
+                </li>
+            );
+        } else if (it === InformasjonElement.PDL_TREDJEPARTSPERSONER) {
+            return (
+                <li key={index} className="blokk-xxs">
+                    <Normaltekst>
+                        Vi får ikke hentet informasjon om tredjepartsperson (foreldre/barn/ektefelle/advokat/etc) fra
+                        PDL.
+                    </Normaltekst>
+                </li>
+            );
+        } else {
+            return (
+                <li key={index} className="blokk-xxs">
+                    <Normaltekst>Vi får ikke hentet informasjon om {it}.</Normaltekst>
+                </li>
+            );
+        }
+    });
     return (
         <Modal
             isOpen={open}
