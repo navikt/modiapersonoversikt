0160d6a2fd207466d6bed66e7031cfc62014db61
vildestefferud <qwartz.ms@hotmail.com>
2022-06-28 09:21:04 +0200

    [KAIZEN-0] Legger til egen endringstekst for bankkonto
    Siden bankkonto blir hentet fra TPS, så har vi ikke samme informasjon tilgjengelig på den. Vi trenger derfor å bruke den gamle måten vi viste endringstekst på, hvor vi også håndterte mapping av ident

---
+++ src/app/personside/visittkort-v2/body/endringsTekstTPS/EndringstekstTPS.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import EtikettGraa from '../../../../../components/EtikettGraa';
+import { formaterDato } from '../../../../../utils/string-utils';
+import { SistEndret } from '../../PersondataDomain';
+
+interface Props {
+    sistEndret: SistEndret | null;
+}
+function EndringstekstTPS({ sistEndret }: Props) {
+    if (!sistEndret) {
+        return null;
+    }
+
+    const formatertdato = formaterDato(new Date(sistEndret.tidspunkt));
+    const endretAv = endretAvTekst(sistEndret.ident);
+
+    return (
+        <EtikettGraa>
+            Endret {formatertdato} {endretAv}
+        </EtikettGraa>
+    );
+}
+export default EndringstekstTPS;
+
+export const FOLKEREGISTERET = 'SKD';
+
+export function endretAvTekst(rawString: string): string {
+    if (endretAvBruker(rawString) || endretIPSelv(rawString)) {
+        return 'av bruker';
+    } else if (
+        endretIFagsystem(rawString) ||
+        endretIPesys(rawString) ||
+        endretIArena(rawString) ||
+        endretAvKonvertItSystem(rawString)
+    ) {
+        return 'av NAV';
+    } else if (rawString.match('AAA2101, SKD')) {
+        return 'av Skatteetaten';
+    } else if (endretIFolkeregisteret(rawString)) {
+        return 'i Folkeregisteret';
+    } else {
+        return rawString;
+    }
+}
+
+function endretIFolkeregisteret(rawString: string) {
+    return (
+        rawString.match(FOLKEREGISTERET) ||
+        rawString.toLowerCase() === 'folkeregisteret' ||
+        rawString.toLowerCase() === 'freg'
+    );
+}
+
+function endretIFagsystem(rawString: string) {
+    return rawString && (endretAvNyTypeIdent(rawString) || endretAvGammelTypeIdent(rawString));
+}
+
+function endretAvNyTypeIdent(rawString: string) {
+    return rawString.toUpperCase().match('[A-Z][0-9]{6}');
+}
+
+function endretAvGammelTypeIdent(rawString: string) {
+    return rawString.toUpperCase().match('[A-Z]{3}[0-9]{4}, [A-Z]{2}[0-9]{2}');
+}
+
+function endretIPSelv(rawString: string) {
+    return rawString.match('Srvpsel');
+}
+
+function endretIArena(rawString: string) {
+    return rawString.match(/arena/i);
+}
+
+function endretAvKonvertItSystem(rawString: string) {
+    return rawString.match(/konvert, IT[0-9]{2}/i);
+}
+
+function endretIPesys(rawString: string) {
+    return rawString.match('srvPensjon');
+}
+
+function endretAvBruker(rawString: string) {
+    return rawString.match('[0-9]{7}');
+}
