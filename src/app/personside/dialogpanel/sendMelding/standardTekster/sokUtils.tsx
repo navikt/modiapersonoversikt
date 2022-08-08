ef5ecf34dbcf1d666d38cecbd17058328e7fb0ef
Nicklas Utgaard <nutgaard@gmail.com>
2022-08-08 15:10:59 +0200

    [KAIZEN-0] gjøre kall til modia-apper via proxy
    kall til skrivestotte, draft, innstillinger og modiacontextholder gjøres via proxy slik at cookies kan låses ned til kun å gjelde modiapersonoversikt

--- src/app/personside/dialogpanel/sendMelding/standardTekster/sokUtils.tsx
+++ src/app/personside/dialogpanel/sendMelding/standardTekster/sokUtils.tsx
@@ -3,7 +3,7 @@ import { parseTekst } from '@navikt/tag-input';
 import * as StandardTekster from './domain';
 
 export function rapporterBruk(tekst: StandardTekster.Tekst): void {
-    fetch(`/modiapersonoversikt-skrivestotte/skrivestotte/statistikk/${tekst.id}`, { method: 'POST' });
+    fetch(`/proxy/modia-skrivestotte/skrivestotte/statistikk/${tekst.id}`, { method: 'POST' });
 }
 
 export function sokEtterTekster(
@@ -17,23 +17,23 @@ export function sokEtterTekster(
 
     const { tags: queryTags, text } = parseTekst(query);
 
-    const tags = queryTags.map(tag => tag.toLowerCase());
+    const tags = queryTags.map((tag) => tag.toLowerCase());
     const words = text
         .split(' ')
-        .map(word => word.toLowerCase().replace('#', ''))
-        .filter(it => it);
+        .map((word) => word.toLowerCase().replace('#', ''))
+        .filter((it) => it);
 
     return tekster
-        .filter(tekst => {
-            const searchTags = tekst.tags.map(tag => tag.toLowerCase());
-            return tags.every(tag => searchTags.includes(tag));
+        .filter((tekst) => {
+            const searchTags = tekst.tags.map((tag) => tag.toLowerCase());
+            return tags.every((tag) => searchTags.includes(tag));
         })
-        .filter(tekst => {
+        .filter((tekst) => {
             const matchtext = [tekst.overskrift, Object.values(tekst.innhold).join('\u0000'), tekst.tags.join('\u0000')]
                 .join('\u0000')
                 .toLowerCase();
 
-            return words.every(word => matchtext.includes(word));
+            return words.every((word) => matchtext.includes(word));
         });
 }
 
