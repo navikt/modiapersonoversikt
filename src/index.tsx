d324ad1a003dd3cb23bf2463509b71f962357f7f
Nicklas Utgaard <nutgaard@gmail.com>
2022-09-19 15:09:01 +0200

    [KAIZEN-0] enable sentry uten react-router integrasjon

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import './extra-polyfills';
-// import './sentry';
+import './sentry';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.less';
--- src/sentry.ts
+++ src/sentry.ts
@@ -5,11 +5,7 @@ if (process.env.NODE_ENV === 'production') {
     const isProd = window.location.host === 'app.adeo.no';
     Sentry.init({
         dsn: 'https://5f3951672e1b49b5a8bca188bf4ad44f@sentry.gc.nav.no/148',
-        integrations: [
-            new BrowserTracing({
-                routingInstrumentation: Sentry.reactRouterV5Instrumentation(window.history)
-            })
-        ],
+        integrations: [new BrowserTracing()],
         environment: isProd ? 'prod' : 'preprod',
         release: '$env{APP_VERSION}',
         // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
