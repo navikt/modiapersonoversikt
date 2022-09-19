b862422b5d4a431ad135defc5f0c4708825f243b
Nicklas Utgaard <nutgaard@gmail.com>
2022-09-19 13:28:09 +0200

    [KAIZEN-0] sette basename for standard router

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,7 +2,10 @@ import * as React from 'react';
 import { Router as OriginalRouter } from 'react-router-dom';
 import { createBrowserHistory, createHashHistory } from 'history';
 
-export const history = process.env.REACT_APP_USE_HASH_ROUTER === 'true' ? createHashHistory() : createBrowserHistory();
+export const history =
+    process.env.REACT_APP_USE_HASH_ROUTER === 'true'
+        ? createHashHistory()
+        : createBrowserHistory({ basename: process.env.PUBLIC_URL });
 function Router(props: { children?: React.ReactNode }) {
     return <OriginalRouter history={history}>{props.children}</OriginalRouter>;
 }
