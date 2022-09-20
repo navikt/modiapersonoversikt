4a6155f9325905b1b30fc63a7348a85f4e103c29
Nicklas Utgaard <nutgaard@gmail.com>
2022-09-20 10:10:01 +0200

    [KAIZEN-0] skru på sentry som default

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/react';
 import { BrowserTracing } from '@sentry/tracing';
 
-if (process.env.NODE_ENV === 'production' && window.location.search.includes('enable-sentry')) {
+if (process.env.NODE_ENV === 'production') {
     const isProd = window.location.host === 'app.adeo.no';
     Sentry.init({
         dsn: 'https://5f3951672e1b49b5a8bca188bf4ad44f@sentry.gc.nav.no/148',
