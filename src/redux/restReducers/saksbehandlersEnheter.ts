d86e10364b722ac740135cce03ce9b7068887da7
Nicklas Utgaard <nutgaard@gmail.com>
2022-10-07 11:35:33 +0200

    [KAIZEN-0] fjerne restreducer for saksbehandlers enheter

--- src/redux/restReducers/saksbehandlersEnheter.ts
+++ src/redux/restReducers/saksbehandlersEnheter.ts
@@ -1,7 +1,7 @@
 import { AppState } from '../reducers';
 import { apiBaseUri } from '../../api/config';
 import { createRestResourceReducerAndActions } from '../../rest/utils/restResource';
-import { SaksbehandlersEnheter } from '../../models/saksbehandlersEnheter';
+import { SaksbehandlersEnheter } from '../../rest/resources/saksbehandlersEnheter';
 
 function getSaksbehandlersEnheterUri(state: AppState): string {
     return `${apiBaseUri}/hode/enheter`;
