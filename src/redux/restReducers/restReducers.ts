24ad4746f3121263935adcd7d8ff12da343f1ee3
Nicklas Utgaard <nutgaard@gmail.com>
2022-10-10 09:46:38 +0200

    [KAIZEN-0] fjerne restreducer for oppfølging

--- src/redux/restReducers/restReducers.ts
+++ src/redux/restReducers/restReducers.ts
@@ -1,22 +1,18 @@
 import utbetalingerReducer from './utbetalinger';
-import oppfolgingReducer from './oppfolging';
 import { RestResource } from '../../rest/utils/restResource';
 import { UtbetalingerResponse } from '../../models/utbetalinger';
-import { DetaljertOppfolging } from '../../models/oppfolging';
 import { combineResettableReducers } from '../reducer-utils';
 import utbetalingerOversikt from './utbetalingerOversikt';
 
 export interface RestEndepunkter {
     utbetalinger: RestResource<UtbetalingerResponse>;
     utbetalingerOversikt: RestResource<UtbetalingerResponse>;
-    oppfolging: RestResource<DetaljertOppfolging>;
 }
 
 export default combineResettableReducers<RestEndepunkter>(
     {
         utbetalinger: utbetalingerReducer,
-        utbetalingerOversikt: utbetalingerOversikt,
-        oppfolging: oppfolgingReducer
+        utbetalingerOversikt: utbetalingerOversikt
     },
     []
 );
