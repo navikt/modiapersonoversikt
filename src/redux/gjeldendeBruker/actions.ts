ad690fa35c8e3db726a7bc3352a4bfc728f77722
Nicklas Utgaard <nutgaard@gmail.com>
2022-10-19 16:34:00 +0200

    [KAIZEN-0] satte opp purge av react-query cache

--- src/redux/gjeldendeBruker/actions.ts
+++ src/redux/gjeldendeBruker/actions.ts
@@ -1,26 +1,9 @@
 import { SetNyGjeldendeBrukerAction, SetNyGjeldendeBrukerActionTypes } from './types';
 import { AsyncAction, AsyncDispatch } from '../ThunkTypes';
-import { cache } from '@nutgaard/use-fetch';
 import { reset } from '../reducer-utils';
 import { resetKeepScroll } from '../../utils/hooks/useKeepScroll';
 import { AppState } from '../reducers';
 import { resetKeepQueryParams } from '../../utils/hooks/useKeepQueryParams';
-import { createTrie, searchTrie } from '../../utils/trie';
-
-const trieDelimiter = /\/|\|\|.*/;
-const protectedCache = createTrie(
-    [
-        '/modiapersonoversikt-api/rest/hode/me',
-        '/modiapersonoversikt-api/rest/hode/enheter',
-        '/modiapersonoversikt-api/rest/baseurls',
-        '/modiapersonoversikt-api/rest/enheter/oppgavebehandlere/alle',
-        '/modiapersonoversikt-api/rest/dialogoppgave/v2/tema',
-        '/modiapersonoversikt-api/rest/tilgang/auth',
-        '/modiapersonoversikt/proxy/modia-skrivestotte/skrivestotte',
-        '/modiapersonoversikt/proxy/modia-innstillinger/api/innstillinger'
-    ],
-    trieDelimiter
-);
 
 // Det er neppe denne du har lyst til å bruke
 // Denne vil ikke oppdatere url med nytt fnr
@@ -29,21 +12,6 @@ export default function setGjeldendeBrukerIRedux(fødselsnummer: string): AsyncA
     return (dispatch: AsyncDispatch, getState: () => AppState) => {
         if (getState().gjeldendeBruker.fødselsnummer !== fødselsnummer) {
             dispatch(reset());
-
-            cache
-                .keys()
-                .filter((key) => {
-                    const res = !searchTrie(protectedCache, key, trieDelimiter);
-                    if (!res) {
-                        console.log('[FetchCache] protected ', key);
-                    }
-                    return res;
-                })
-                .forEach((key) => {
-                    console.log('[FetchCache] remove ', key);
-                    cache.remove(key);
-                });
-
             resetKeepScroll();
             resetKeepQueryParams();
 
