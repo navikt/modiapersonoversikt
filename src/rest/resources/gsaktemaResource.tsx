0272a289b82c86d8578de5a2e7846dc85e2cf90a
Nicklas Utgaard <nutgaard@gmail.com>
2022-10-19 13:40:32 +0200

    [KAIZEN-0] bytte ut useFetch for react-query for gsaktema
    react-query er mer brukt, aktivt forvaltet og støtter refetching mye mer elegant.

---
+++ src/rest/resources/gsaktemaResource.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { apiBaseUri } from '../../api/config';
+import { GsakTema } from '../../models/meldinger/oppgave';
+import { applyDefaults, DefaultConfig, RendererOrConfig, useRQRest } from '../useRest';
+import { CenteredLazySpinner } from '../../components/LazySpinner';
+import AlertStripe from 'nav-frontend-alertstriper';
+import { QueryClient, useQuery, UseQueryResult } from '@tanstack/react-query';
+import { FetchError, get } from '../../api/api';
+
+const queryKey = ['gsaktema'];
+const url = `${apiBaseUri}/dialogoppgave/v2/tema`;
+const defaults: DefaultConfig = {
+    ifPending: <CenteredLazySpinner />,
+    ifError: <AlertStripe type="advarsel">Kunne ikke laste inn tema</AlertStripe>
+};
+
+const resource = {
+    prefetch(queryClient: QueryClient) {
+        queryClient.prefetchQuery(queryKey, () => get(url));
+    },
+    useFetch(): UseQueryResult<GsakTema[], FetchError> {
+        return useQuery(queryKey, () => get(url));
+    },
+    useRenderer(renderer: RendererOrConfig<GsakTema[]>) {
+        const response = this.useFetch();
+        return useRQRest(response, applyDefaults(defaults, renderer));
+    }
+};
+
+export default resource;
