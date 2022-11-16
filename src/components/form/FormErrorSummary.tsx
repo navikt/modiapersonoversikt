380f287a54890461c8ec5cd299604a91475dd239
Jesper Paulsen <jesper.gayosso.paulsen@nav.no>
2022-11-16 12:16:25 +0100

    [OPP-1504] Kun vis feilmeldingsoppsumering hvis det er feilmeldinger å vise

--- src/components/form/FormErrorSummary.tsx
+++ src/components/form/FormErrorSummary.tsx
@@ -1,16 +1,20 @@
 import { FieldValues } from 'react-hook-form/dist/types/fields';
 import { UseFormReturn } from 'react-hook-form/dist/types';
 import { useEffect, useRef } from 'react';
-import { Feiloppsummering } from 'nav-frontend-skjema';
+import { Feiloppsummering, FeiloppsummeringFeil } from 'nav-frontend-skjema';
 import * as React from 'react';
 import { FieldErrors } from 'react-hook-form/dist/types/errors';
 import { FeiloppsummeringProps } from 'nav-frontend-skjema';
 
 function buildErrorFields<TFieldValues extends FieldValues>(errors: FieldErrors<TFieldValues>) {
-    return Object.entries(errors).map(([skjemaelementId, feil]) => ({
-        skjemaelementId,
-        feilmelding: feil?.message
-    }));
+    const res: FeiloppsummeringFeil[] = [];
+    for (const [skjemaelementId, feil] of Object.entries(errors)) {
+        if (feil) {
+            res.push({ skjemaelementId, feilmelding: feil.message });
+        }
+    }
+
+    return res;
 }
 
 interface Props<TFieldValues extends FieldValues> extends Omit<FeiloppsummeringProps, 'feil' | 'customFeilRender'> {
@@ -22,17 +26,17 @@ function FormErrorSummary<TFieldValues extends FieldValues>(props: Props<TFieldV
     const ref = useRef<HTMLDivElement>(null);
     const { form, ...errorSummaryProps } = props;
 
-    const submitCount = form?.formState.submitCount;
+    const submitCount = form?.formState.submitCount ?? 0;
     const feil = buildErrorFields(form?.formState.errors ?? ([] as FieldErrors<TFieldValues>));
 
-    const skalViseFeilmelding = submitCount && feil.length;
+    const skalViseFeilmelding = submitCount > 0 && feil.length > 0;
 
     useEffect(() => {
-        if (skalViseFeilmelding && ref.current) {
+        if (ref.current) {
             ref.current?.focus();
             ref.current?.scrollIntoView();
         }
-    }, [submitCount, skalViseFeilmelding, ref]);
+    }, [submitCount, ref]);
 
     if (!skalViseFeilmelding) {
         return null;
