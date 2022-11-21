ceca953d2ea8ffe5ee6464b9452e534298fbe7eb
Jesper Paulsen <jesper.gayosso.paulsen@nav.no>
2022-11-21 12:39:25 +0100

    [OPP-1504] Fiks personsøk skjema

--- src/app/personsok/personsokUtils.ts
+++ src/app/personsok/personsokUtils.ts
@@ -24,8 +24,7 @@ export type PersonSokFormStateV3 = {
 };
 
 export function resolver(values: PersonSokFormStateV3) {
-    let kontonummer: FieldError | undefined;
-    let utenlandskID: FieldError | undefined;
+    const errors: Record<string, FieldError | undefined> = {};
 
     const andreFelter = [
         values.navn,
@@ -41,64 +40,53 @@ export function resolver(values: PersonSokFormStateV3) {
     const andreFelterErSatt = andreFelter.some((it) => it?.length);
 
     if (values.kontonummer && !validerLengdeOgTallPaKontonummer(values.kontonummer)) {
-        kontonummer = buildFieldError('Kontonummer må kun bestå av tall og være 11 siffer');
+        errors.kontonummer = buildFieldError('Kontonummer må kun bestå av tall og være 11 siffer');
     } else if (values.kontonummer && !erGyldigNorskKontonummer(values.kontonummer)) {
-        kontonummer = buildFieldError('Kontonummer må være et gyldig norsk kontonummer');
+        errors.kontonummer = buildFieldError('Kontonummer må være et gyldig norsk kontonummer');
     } else if (values.kontonummer && andreFelterErSatt) {
-        kontonummer = buildFieldError('Kan ikke kombinere søk på kontonummer med andre felt');
+        errors.kontonummer = buildFieldError('Kan ikke kombinere søk på kontonummer med andre felt');
     }
-
-    let fodselsdatoFra: FieldError | undefined;
-    let fodselsdatoTil: FieldError | undefined;
 
     const fra = dayjs(values.fodselsdatoFra).toDate();
     const til = dayjs(values.fodselsdatoTil).toDate();
 
     if (values.fodselsdatoFra && !isISODateString(values.fodselsdatoFra)) {
-        fodselsdatoFra = buildFieldError('Fra-dato må være en gyldig dato');
+        errors.fodselsdatoFra = buildFieldError('Fra-dato må være en gyldig dato');
     } else if (fra > til) {
-        fodselsdatoFra = buildFieldError('Fra-dato kan ikke være senere enn til-dato');
+        errors.fodselsdatoFra = buildFieldError('Fra-dato kan ikke være senere enn til-dato');
     }
     if (values.fodselsdatoTil && !isISODateString(values.fodselsdatoTil)) {
-        fodselsdatoTil = buildFieldError('Til-dato må være en gyldig dato');
+        errors.fodselsdatoTil = buildFieldError('Til-dato må være en gyldig dato');
     } else if (til > new Date()) {
-        fodselsdatoTil = buildFieldError('Du kan ikke velge til-dato frem i tid');
+        errors.fodselsdatoTil = buildFieldError('Du kan ikke velge til-dato frem i tid');
     }
-    const alderFra = values.alderFra && !erTall(values.alderFra) ? buildFieldError('Alder må være tall') : undefined;
-    const alderTil = values.alderTil && !erTall(values.alderTil) ? buildFieldError('Alder må være tall') : undefined;
-    const kjonn = undefined;
 
-    let _minimumskrav: FieldError | undefined;
+    if (values.alderFra && !erTall(values.alderFra)) {
+        errors.alderFra = buildFieldError('Alder må være tall');
+    }
+
+    if (values.alderTil && !erTall(values.alderTil)) {
+        errors.alderTil = buildFieldError('Alder må være tall');
+    }
 
     if (!values.utenlandskID && !values.adresse && !values.kontonummer && !values.navn) {
-        _minimumskrav = buildFieldError(
+        errors._minimumskrav = buildFieldError(
             'Du må minimum fylle inn navn, adresse, kontonummer eller utenlandsk ID for å gjøre søk'
         );
     }
 
-    const errors: Record<string, FieldError | undefined> = {
-        kontonummer,
-        utenlandskID,
-        fodselsdatoFra,
-        fodselsdatoTil,
-        alderFra,
-        alderTil,
-        kjonn,
-        _minimumskrav
-    };
-
     return { values, errors };
 }
 
-export function stringToNumber(input: string): number | undefined {
-    if (input.length === 0) {
+export function stringToNumber(input?: string): number | undefined {
+    if (!input || input.length === 0) {
         return undefined;
     }
     return parseInt(input);
 }
 
-function emptyString(input: string): string | undefined {
-    if (input.length === 0) {
+function emptyString(input?: string): string | undefined {
+    if (!input || input.length === 0) {
         return undefined;
     }
     return input;
