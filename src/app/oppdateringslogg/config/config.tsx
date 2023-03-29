caa65889f23b2fd4659d90177de6cc425ea142a5
abrha <abrha.kiros.abrha@nav.no>
2023-03-29 11:06:36 +0200

    [KAIZEN-0] oppdateringslogg for endringer på meldingsvisning

--- src/app/oppdateringslogg/config/config.tsx
+++ src/app/oppdateringslogg/config/config.tsx
@@ -493,13 +493,13 @@ export function lagOppdateringsloggConfig(): OppdateringsloggInnslag[] {
             id: 27,
             tittel: 'Endringer på meldingsvisning',
             dato: new Date('2023-02-23 07:00'),
-            aktiv: false,
+            aktiv: true,
             ingress: null,
             beskrivelse: (
                 <>
                     <Normaltekst>
-                        Det er gjort endringer på meldingsvisning. Nå er det kun to valg, referat og samtale. Se
-                        fagmelding på{' '}
+                        Det er gjort endringer på meldingsvisning. Nå er det kun tre valg, referat, samtale og
+                        infomelding. Se fagmelding på{' '}
                         <a
                             href="https://navno.sharepoint.com/sites/fag-og-ytelser-fagsystemer/SitePages/Endringer-i-Modia-personoversikt.aspx"
                             target="_blank"
