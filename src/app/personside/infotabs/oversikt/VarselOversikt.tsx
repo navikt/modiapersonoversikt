adb74cdb07230102007f1522baa823ad47064aed
Jesper Paulsen <jesper.gayosso.paulsen@nav.no>
2023-04-12 15:57:42 +0200

    [KAIZEN-0] Fjerne revarsling toggle

--- src/app/personside/infotabs/oversikt/VarselOversikt.tsx
+++ src/app/personside/infotabs/oversikt/VarselOversikt.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
 import styled from 'styled-components/macro';
 import theme from '../../../../styles/personOversiktTheme';
-import Varsel from '../varsel/Varsel';
 import { AlertStripeInfo } from 'nav-frontend-alertstriper';
 import { ReactNode } from 'react';
 import { useOnMount } from '../../../../utils/customHooks';
 import { Normaltekst } from 'nav-frontend-typografi';
 import VarslerLoader, { VarslerRendererProps } from '../varsel/varsel-loader';
-import VarselNy from '../varsel/VarselNy';
-import IfFeatureToggleOn from '../../../../components/featureToggle/IfFeatureToggleOn';
-import IfFeatureToggleOff from '../../../../components/featureToggle/IfFeatureToggleOff';
-import { FeatureToggles } from '../../../../components/featureToggle/toggleIDs';
+import Varsel from '../varsel/Varsel';
 
 const ListStyle = styled.ol`
     > *:not(:first-child) {
@@ -23,16 +19,7 @@ interface Props {
 }
 
 function VarselOversikt(props: Props) {
-    return (
-        <>
-            <IfFeatureToggleOn toggleID={FeatureToggles.visRevarslingDetaljer}>
-                <VarslerLoader component={VarselVisningNy} {...props} />
-            </IfFeatureToggleOn>
-            <IfFeatureToggleOff toggleID={FeatureToggles.visRevarslingDetaljer}>
-                <VarslerLoader component={VarselVisning} {...props} />
-            </IfFeatureToggleOff>
-        </>
-    );
+    return <VarslerLoader component={VarselVisning} {...props} />;
 }
 
 function VarselVisning(props: VarslerRendererProps & Props) {
@@ -53,29 +40,6 @@ function VarselVisning(props: VarslerRendererProps & Props) {
         <ListStyle>
             {forsteVarsler.map((varsel, index) => (
                 <Varsel key={index} varsel={varsel} />
-            ))}
-        </ListStyle>
-    );
-}
-
-function VarselVisningNy(props: VarslerRendererProps & Props) {
-    const forsteVarsler = props.varsler.slice(0, 2);
-    useOnMount(() => {
-        props.setHeaderContent(
-            <Normaltekst>
-                {forsteVarsler.length} / {props.varsler.length}
-            </Normaltekst>
-        );
-    });
-
-    if (props.varsler.length === 0) {
-        return <AlertStripeInfo>Ingen varsler på bruker</AlertStripeInfo>;
-    }
-
-    return (
-        <ListStyle>
-            {forsteVarsler.map((varsel, index) => (
-                <VarselNy key={index} varsel={varsel} />
             ))}
         </ListStyle>
     );
--- src/app/personside/infotabs/varsel/DittNavVarsler.tsx
+++ src/app/personside/infotabs/varsel/DittNavVarsler.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { VarselRow } from './VarselRow';
 import styled from 'styled-components';
 import theme from '../../../../styles/personOversiktTheme';
 import { emptyReplacement } from './varsel-utils';
 import { ENDASH, formaterDato } from '../../../../utils/string-utils';
-import { DittNavEvent } from '../../../../models/varsel';
+import { DittNavEvent, FeiletVarsling } from '../../../../models/varsel';
+import { VarselRow } from './VarselRow';
 
 const GraattDefinisjonsListe = styled.dl`
     ${theme.graattPanel}
@@ -19,6 +19,19 @@ const GraattDefinisjonsListe = styled.dl`
         margin-bottom: 0.5rem;
         margin-left: 7.125rem;
     }
+`;
+
+const BoldTekst = styled.span`
+    font-weight: bold;
+`;
+
+const FeilteVarslinerListeStyling = styled.div`
+    margin-top: 0.5rem;
+`;
+
+const FeilteVarslingerListeWrapper = styled.div`
+    margin-top: 1rem;
+    margin-bottom: 1rem;
 `;
 
 function DittNavInformasjonsLinje({ tittel, tekst }: { tittel: string; tekst: string }) {
@@ -50,6 +63,90 @@ export function DittNavEventVarsel({ varsel }: { varsel: DittNavEvent }) {
         <VarselRow datoer={datoer} tittel={tittel} kanaler={kanaler} varsel={varsel}>
             <GraattDefinisjonsListe>
                 <DittNavInformasjonsLinjer produsent={varsel.produsent} tekst={varsel.tekst} link={varsel.link} />
+            </GraattDefinisjonsListe>
+        </VarselRow>
+    );
+}
+
+function FeilteVarslingerListe({ tittel, feilteVarslinger }: { tittel: string; feilteVarslinger: FeiletVarsling[] }) {
+    return (
+        <FeilteVarslingerListeWrapper>
+            <BoldTekst>{tittel}</BoldTekst>
+            <FeilteVarslinerListeStyling>
+                {feilteVarslinger.map((varsling) => (
+                    <li key={`${varsling.tidspunkt} - ${varsling.kanal}`}>
+                        {formaterDato(varsling.tidspunkt)} - {varsling.kanal}: {varsling.feilmelding}
+                    </li>
+                ))}
+            </FeilteVarslinerListeStyling>
+        </FeilteVarslingerListeWrapper>
+    );
+}
+
+export function DittNavEventVarselV2({ varsel }: { varsel: DittNavEvent }) {
+    const varslingsTidspunkt = varsel.varslingsTidspunkt;
+
+    if (!varslingsTidspunkt || !varslingsTidspunkt.tidspunkt) {
+        return <DittNavEventVarsel varsel={varsel} />;
+    }
+
+    const aktiv = varsel.aktiv ? '' : ' (Ferdigstilt)';
+    const datoer = [formaterDato(varslingsTidspunkt.tidspunkt)];
+    if (varslingsTidspunkt.renotifikasjonTidspunkt) {
+        datoer.push(formaterDato(varslingsTidspunkt.renotifikasjonTidspunkt));
+    }
+
+    const tittel = `Notifikasjon${aktiv}: ${varsel.tekst}`;
+    const kanaler = [
+        'DITT_NAV',
+        ...varsel.eksternVarslingKanaler,
+        ...varslingsTidspunkt.renotifikasjonsKanaler
+    ].unique();
+
+    return (
+        <VarselRow
+            datoer={datoer}
+            tittel={tittel}
+            kanaler={kanaler}
+            varsel={varsel}
+            harFeilteVarsel={varslingsTidspunkt.harFeilteVarslinger || varslingsTidspunkt.harFeilteRevarslinger}
+        >
+            <GraattDefinisjonsListe>
+                <div>
+                    <DittNavInformasjonsLinjer produsent={varsel.produsent} tekst={varsel.tekst} link={varsel.link} />
+                    <DittNavInformasjonsLinje
+                        tittel="Varslet: "
+                        tekst={`${formaterDato(varslingsTidspunkt.tidspunkt)} - ${varslingsTidspunkt.sendteKanaler.join(
+                            ', '
+                        )}`}
+                    />
+                    {varslingsTidspunkt.renotifikasjonTidspunkt && (
+                        <DittNavInformasjonsLinje
+                            tittel="Revarslet: "
+                            tekst={`${formaterDato(
+                                varslingsTidspunkt.renotifikasjonTidspunkt
+                            )} - ${varslingsTidspunkt.renotifikasjonsKanaler.join(', ')}`}
+                        />
+                    )}
+                    {varslingsTidspunkt.harFeilteVarslinger && (
+                        <>
+                            <hr />
+                            <FeilteVarslingerListe
+                                tittel="Varslingsfeil"
+                                feilteVarslinger={varslingsTidspunkt.feilteVarsliner}
+                            />
+                        </>
+                    )}
+                    {varslingsTidspunkt.harFeilteRevarslinger && (
+                        <>
+                            <hr />
+                            <FeilteVarslingerListe
+                                tittel="Revarslingsfeil"
+                                feilteVarslinger={varslingsTidspunkt.feilteRevarslinger}
+                            />
+                        </>
+                    )}
+                </div>
             </GraattDefinisjonsListe>
         </VarselRow>
     );
