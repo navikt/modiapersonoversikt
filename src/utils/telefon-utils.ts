cf1cd2ef54cd2b52abc446c3f66ae0fdebf40ae7
Mathias S. Müller <mathias.sundt.muller@nav.no>
2023-09-29 11:32:46 +0200

    [KAIZEN-0] Oppdatere telefonnummerformat

--- src/utils/telefon-utils.ts
+++ src/utils/telefon-utils.ts
@@ -23,7 +23,7 @@ export function formaterTelefonnummer(telefonnummer: string) {
     const utenSpace = removeWhitespace(telefonnummer);
     if (utenSpace.length !== 8) {
         return telefonnummer;
-    } else if ('489'.includes(utenSpace[0])) {
+    } else if (utenSpace.startsWith('800')) {
         return formatNumber('### ## ###', utenSpace);
     } else {
         return formatNumber('## ## ## ##', utenSpace);
