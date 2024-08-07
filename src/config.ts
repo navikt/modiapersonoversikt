be2a1771275307d3450934a370eefbb297cbcc23
Ludvig Hozman <ludvig123@gmail.com>
2024-08-07 13:20:18 +0200

    Fiks feil miljø på hurtigtaster

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 const env = window?.__ENV__ ?? {};
 
 export default {
-    draftWsUrl: env.PUBLIC_DRAFT_WS_URL
+    draftWsUrl: env.PUBLIC_DRAFT_WS_URL,
+    isProd: env.PUBLIC_ENVIRONMENT === 'prod'
 };
