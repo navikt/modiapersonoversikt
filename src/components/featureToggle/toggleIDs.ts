9e72574f324e3e79f9162d833dd6a7cea76bbd41
Ludvig Hozman <ludvig123@gmail.com>
2024-10-08 16:01:37 +0200

    Fjern featureToggle for contextholderNext
    Denne er nå i dev, og toggle trengs ikke lenger

--- src/components/featureToggle/toggleIDs.ts
+++ src/components/featureToggle/toggleIDs.ts
@@ -4,6 +4,5 @@ export enum FeatureToggles {
     BrukNyDecorator = 'modiapersonoversikt.decorator-v3',
     JournalforUtenSvar = 'modiapersonoversikt.meldinger-uten-svar-kan-journalfores',
     VisDraftStatus = 'modiapersonoversikt.vis-draft-status',
-    BrukNyTiltakspenger = 'modiapersonoversikt.bruk-ny-tiltakspenger',
-    BrukContextholderNext = 'modiapersonoversikt.bruk-contextholder-next'
+    BrukNyTiltakspenger = 'modiapersonoversikt.bruk-ny-tiltakspenger'
 }
