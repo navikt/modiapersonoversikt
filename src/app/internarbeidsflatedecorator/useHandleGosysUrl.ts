d58cd2055318ab75d2d54458141d205cd60100c6
abrha <abrha.kiros.abrha@nav.no>
2024-11-04 15:21:34 +0100

    [KAIZEN-0] linting

--- src/app/internarbeidsflatedecorator/useHandleGosysUrl.ts
+++ src/app/internarbeidsflatedecorator/useHandleGosysUrl.ts
@@ -4,32 +4,32 @@ import { loggEvent } from '../../utils/logger/frontendLogger';
 import { INFOTABS } from '../personside/infotabs/InfoTabEnum';
 import { paths } from '../routes/routing';
 import { Oppgave } from '../../models/meldinger/oppgave';
-import {apiBaseUri, contextHolderBaseUri} from '../../api/config';
+import { apiBaseUri, contextHolderBaseUri } from '../../api/config';
 import { fetchToJson, hasData } from '../../utils/fetchToJson';
 import { useDispatch } from 'react-redux';
 import { replace } from 'connected-react-router';
-import {post} from "../../api/api";
+import { post } from '../../api/api';
 
 function useHandleGosysUrl() {
     const queryParams = useQueryParams<{
         sokFnr?: string;
         sokFnrCode?: string;
         oppgaveid?: string;
-        behandlingsid?: string
+        behandlingsid?: string;
     }>();
     const dispatch = useDispatch();
     const settGjeldendeBruker = useSettAktivBruker();
 
-    useOnMount( () => {
+    useOnMount(() => {
         if (queryParams.sokFnrCode) {
             post<{
-                fnr: string,
-                code: string
-            }>(`${contextHolderBaseUri}/fnr-code/retrieve`, {code: queryParams.sokFnrCode}).then((res) => {
-                handleLegacyUrls(res.fnr)
-            })
+                fnr: string;
+                code: string;
+            }>(`${contextHolderBaseUri}/fnr-code/retrieve`, { code: queryParams.sokFnrCode }).then((res) => {
+                handleLegacyUrls(res.fnr);
+            });
         } else {
-            handleLegacyUrls(queryParams.sokFnr)
+            handleLegacyUrls(queryParams.sokFnr);
         }
     });
 
@@ -37,7 +37,7 @@ function useHandleGosysUrl() {
         const linkTilValgtHenvendelse = `${paths.personUri}/${INFOTABS.MELDINGER.path}?traadId=${queryParams.behandlingsid}`;
         if (queryParams.oppgaveid && queryParams.behandlingsid && fnr) {
             fetchToJson<Oppgave>(`${apiBaseUri}/v2/oppgaver/oppgavedata/${queryParams.oppgaveid}`).then((response) => {
-                loggEvent('Oppgave', 'FraGosys', {success: hasData(response)});
+                loggEvent('Oppgave', 'FraGosys', { success: hasData(response) });
                 settGjeldendeBruker(fnr);
                 dispatch(replace(linkTilValgtHenvendelse));
             });
@@ -48,10 +48,10 @@ function useHandleGosysUrl() {
         } else if (queryParams.behandlingsid) {
             loggEvent('Henvendelse', 'FraGosys');
             dispatch(replace(linkTilValgtHenvendelse));
-        } else if(fnr){
+        } else if (fnr) {
             settGjeldendeBruker(fnr);
         }
-    }
+    };
 }
 
 export default useHandleGosysUrl;
