ce718890deeb683ae43f54fcfc5eeb26d842d532
Abrha K. Abrha <116550783+abrhanav@users.noreply.github.com>
2024-11-07 15:27:21 +0100

    [KAIZEN-0] Vis utbetalingsmetode (#2393)
    * [KAIZEN-0] Vis utbetalingsmetode

--- src/app/personside/infotabs/utbetalinger/utbetaling/SammensattUtbetaling.tsx
+++ src/app/personside/infotabs/utbetalinger/utbetaling/SammensattUtbetaling.tsx
@@ -101,8 +101,9 @@ function SammensattUtbetaling(props: Props) {
                     <FlexEnd>
                         <Normaltekst>{forfallsInfo}</Normaltekst>
                     </FlexEnd>
+                    <Normaltekst>Utbetaling til: {utbetaling.utbetaltTil}</Normaltekst>
                     <SpaceBetween>
-                        <Normaltekst>Utbetaling til: {utbetaling.utbetaltTil}</Normaltekst>
+                        <Normaltekst>Utbetalingsmetode: {utbetaling.metode}</Normaltekst>
                         <PrintKnapp onClick={visDetaljerAndPrint} />
                     </SpaceBetween>
                     <YtelsesListe aria-label={`Diverse ytelser`}>{ytelsesListe}</YtelsesListe>
