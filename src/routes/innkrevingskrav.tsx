f0784d56ad15260f345e65fca7a729fee0d74775
Ludvig Hozman <Ole.ludvig.lingjerde.hozman@nav.no>
2024-11-26 16:41:38 +0100

    Oppdatere innkrevingskrav side med nytt API-format (#2438)

--- src/routes/innkrevingskrav.tsx
+++ src/routes/innkrevingskrav.tsx
@@ -3,8 +3,9 @@ import InnkrevingskravSide from 'src/app/innkrevingskrav/InnkrevingskravSide';
 import { z } from 'zod';
 
 const innkrevingsKravSearchSchema = z.object({
-    kravId: z.preprocess(String, z.string().optional())
+    kravId: z.preprocess((s) => (s === undefined ? s : String(s)), z.string().optional())
 });
+
 export const Route = createFileRoute('/innkrevingskrav')({
     component: InnkrevingsKravRoute,
     validateSearch: innkrevingsKravSearchSchema
