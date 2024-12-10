d36a160884b2e6d2d731b16a87242c87d3eedb67
Ludvig Hozman <ludvig123@gmail.com>
2024-12-10 10:36:03 +0100

    Fiks URL håndtering fra gosys med henvendelseid

--- src/app/PersonOppslagHandler/LyttPåNyttFnrIReduxOgHentAllPersoninfo.tsx
+++ src/app/PersonOppslagHandler/LyttPåNyttFnrIReduxOgHentAllPersoninfo.tsx
@@ -1,11 +1,9 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { useGjeldendeBruker } from '../../redux/gjeldendeBruker/types';
 import { loggEvent } from '../../utils/logger/frontendLogger';
 import tildelteoppgaver from '../../rest/resources/tildelteoppgaverResource';
 
 function LyttPåNyttFnrIReduxOgHentAllPersoninfo() {
-    const dispatch = useDispatch();
     const fnr = useGjeldendeBruker();
     tildelteoppgaver.useFetch();
 
@@ -13,7 +11,7 @@ function LyttPåNyttFnrIReduxOgHentAllPersoninfo() {
         if (fnr.length !== 0) {
             loggEvent('OppslagNyPerson', 'HentAllPersoninfo');
         }
-    }, [dispatch, fnr]);
+    }, [fnr]);
     return null;
 }
 
