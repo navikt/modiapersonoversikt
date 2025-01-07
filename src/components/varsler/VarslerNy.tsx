827eebedb4aef38784174ffc5b7a539dfcb74b02
abrha <abrha.kiros.abrha@nav.no>
2025-01-07 17:56:54 +0100

    [KAIZEN-0] ny varsler side.

--- src/components/varsler/VarslerNy.tsx
+++ src/components/varsler/VarslerNy.tsx
@@ -1,8 +1,10 @@
 import { CheckmarkCircleFillIcon, ExclamationmarkTriangleFillIcon } from '@navikt/aksel-icons';
-import { Alert, HStack, Pagination, Table, VStack } from '@navikt/ds-react';
-import { type ReactNode, useState } from 'react';
+import { Alert, HStack, Pagination, Skeleton, Table, VStack } from '@navikt/ds-react';
+import { getRouteApi, useNavigate } from '@tanstack/react-router';
+import { type ReactNode, useEffect, useState } from 'react';
 import { emptyReplacement, getVarselTekst } from 'src/app/personside/infotabs/varsel/varsel-utils';
 import VarselMeldinger from 'src/app/personside/infotabs/varsel/varselDetaljer/VarselMeldinger';
+import QueryErrorBoundary from 'src/components/QueryErrorBoundary';
 import { useVarslerData } from 'src/lib/clients/modiapersonoversikt-api';
 import {
     type DittNavEvent,
@@ -16,203 +18,221 @@ import {
 import { datoSynkende } from 'src/utils/date-utils';
 import { ENDASH, formaterDato } from 'src/utils/string-utils';
 
-function VarslerNy() {
-    const [page, setPage] = useState(1);
-    const rowsPerPage = 22;
+const routeApi = getRouteApi('/new/person/varsler');
 
-    const varslerResponse = useVarslerData();
-    const varslerResult: VarslerResult = varslerResponse.data || { feil: [], varsler: [] };
-    const datoExtractor = (varsel: UnifiedVarsel) => {
-        if (isDittNavEvent(varsel)) {
-            return varsel.forstBehandlet;
-        }
-        return varsel.mottattTidspunkt;
-    };
+const datoExtractor = (varsel: UnifiedVarsel) => {
+    if (isDittNavEvent(varsel)) {
+        return varsel.forstBehandlet;
+    }
+    return varsel.mottattTidspunkt;
+};
 
-    const varselElementer = varslerResult.varsler.sort(datoSynkende(datoExtractor));
-    const varselPagniated = varselElementer.slice((page - 1) * rowsPerPage, page * rowsPerPage);
+const DittNavInformasjonsLinje = ({ tittel, tekst }: { tittel: string; tekst: string }) => {
+    return (
+        <HStack gap="4">
+            <div className={'font-bold'}>{tittel}</div>
+            <div>{tekst}</div>
+        </HStack>
+    );
+};
 
-    const DittNavInformasjonsLinje = ({ tittel, tekst }: { tittel: string; tekst: string }) => {
-        return (
-            <HStack gap="4">
-                <div className={'font-bold'}>{tittel}</div>
-                <div>{tekst}</div>
-            </HStack>
-        );
-    };
+const DittNavInformasjonsLinjer = (varsel: { produsent: string; tekst: string; link: string }) => {
+    return (
+        <VStack gap="4" className={'space-y-2 bg-gray-200 p-4'}>
+            <DittNavInformasjonsLinje tittel="Produsert av:" tekst={emptyReplacement(varsel.produsent, ENDASH)} />
+            <DittNavInformasjonsLinje tittel="Tekst:" tekst={emptyReplacement(varsel.tekst, ENDASH)} />
+            <DittNavInformasjonsLinje tittel="Link:" tekst={emptyReplacement(varsel.link, ENDASH)} />
+        </VStack>
+    );
+};
 
-    const DittNavInformasjonsLinjer = (varsel: { produsent: string; tekst: string; link: string }) => {
-        return (
-            <VStack gap="4" className={'space-y-2 bg-gray-200 p-4'}>
-                <DittNavInformasjonsLinje tittel="Produsert av:" tekst={emptyReplacement(varsel.produsent, ENDASH)} />
-                <DittNavInformasjonsLinje tittel="Tekst:" tekst={emptyReplacement(varsel.tekst, ENDASH)} />
-                <DittNavInformasjonsLinje tittel="Link:" tekst={emptyReplacement(varsel.link, ENDASH)} />
-            </VStack>
-        );
-    };
-
-    const FeilteVarslingerListe = ({
-        tittel,
-        feilteVarslinger
-    }: { tittel: string; feilteVarslinger: FeiletVarsling[] }) => {
-        return (
-            <div className={'my-2'}>
-                <div className={'font-bold'}>{tittel}</div>
-                <div>
-                    {feilteVarslinger.map((varsling) => (
-                        <li key={`${varsling.tidspunkt} - ${varsling.kanal}`}>
-                            {formaterDato(varsling.tidspunkt)} - {varsling.kanal}: {varsling.feilmelding}
-                        </li>
-                    ))}
-                </div>
+const FeilteVarslingerListe = ({
+    tittel,
+    feilteVarslinger
+}: { tittel: string; feilteVarslinger: FeiletVarsling[] }) => {
+    return (
+        <div className={'my-2'}>
+            <div className={'font-bold'}>{tittel}</div>
+            <div>
+                {feilteVarslinger.map((varsling) => (
+                    <li key={`${varsling.tidspunkt} - ${varsling.kanal}`}>
+                        {formaterDato(varsling.tidspunkt)} - {varsling.kanal}: {varsling.feilmelding}
+                    </li>
+                ))}
             </div>
-        );
-    };
+        </div>
+    );
+};
 
-    const DittNavInformasjonsLinjerV2 = (varsel: DittNavEvent) => {
-        const varslingsTidspunkt = varsel.varslingsTidspunkt;
-
-        return (
-            <>
-                <DittNavInformasjonsLinjer produsent={varsel.produsent} tekst={varsel.tekst} link={varsel.link} />
-                <DittNavInformasjonsLinje
-                    tittel="Varslet: "
-                    tekst={`${formaterDato(varslingsTidspunkt.tidspunkt)} - ${varslingsTidspunkt.sendteKanaler.join(
-                        ', '
-                    )}`}
-                />
-                {varslingsTidspunkt.renotifikasjonTidspunkt && (
-                    <DittNavInformasjonsLinje
-                        tittel="Revarslet: "
-                        tekst={`${formaterDato(
-                            varslingsTidspunkt.renotifikasjonTidspunkt
-                        )} - ${varslingsTidspunkt.renotifikasjonsKanaler.join(', ')}`}
-                    />
-                )}
-                {varslingsTidspunkt.harFeilteVarslinger && (
-                    <>
-                        <hr />
-                        <FeilteVarslingerListe
-                            tittel="Varslingsfeil"
-                            feilteVarslinger={varslingsTidspunkt.feilteVarsliner}
-                        />
-                    </>
-                )}
-                {varslingsTidspunkt.harFeilteRevarslinger && (
-                    <>
-                        <hr />
-                        <FeilteVarslingerListe
-                            tittel="Revarslingsfeil"
-                            feilteVarslinger={varslingsTidspunkt.feilteRevarslinger}
-                        />
-                    </>
-                )}
-            </>
-        );
-    };
-
-    const dataExtractor = (
-        varsel: UnifiedVarselModell
-    ): { datoer: string[]; tittel: string; kanaler: string[]; harFeilteVarsel?: boolean; detaljer?: ReactNode } => {
-        if (isDittNavEvent(varsel)) {
-            const varslingsTidspunkt = varsel.varslingsTidspunkt;
-            const aktiv = varsel.aktiv ? '' : ' (Ferdigstilt)';
-            const tittel = `Notifikasjon${aktiv}: ${varsel.tekst}`;
-
-            if (!varslingsTidspunkt || !varslingsTidspunkt.tidspunkt) {
-                const datoer = [formaterDato(varsel.forstBehandlet)];
-                const kanaler = ['DITT_NAV', ...varsel.eksternVarslingKanaler];
-
-                const detaljer = (
-                    <DittNavInformasjonsLinjer produsent={varsel.produsent} tekst={varsel.tekst} link={varsel.link} />
-                );
-
-                return { datoer, tittel, kanaler, detaljer };
-            }
-
-            const datoer = [formaterDato(varslingsTidspunkt.tidspunkt)];
-            if (varslingsTidspunkt.renotifikasjonTidspunkt) {
-                datoer.push(formaterDato(varslingsTidspunkt.renotifikasjonTidspunkt));
-            }
-
-            const kanaler = [
-                'DITT_NAV',
-                ...varsel.eksternVarslingKanaler,
-                ...varslingsTidspunkt.renotifikasjonsKanaler
-            ].unique();
-
-            const harFeilteVarsel = varslingsTidspunkt.harFeilteVarslinger || varslingsTidspunkt.harFeilteRevarslinger;
-            const detaljer = <DittNavInformasjonsLinjerV2 varsel={varsel} />;
-
-            return { datoer, tittel, kanaler, detaljer, harFeilteVarsel };
-        }
-
-        const meldingsliste = varsel.meldingListe?.sort(datoSynkende((melding) => melding.utsendingsTidspunkt)) || [];
-        const datoer = meldingsliste.map((melding) => formaterDato(melding.utsendingsTidspunkt)).unique();
-        const tittel = getVarselTekst(varsel);
-        const kanaler = meldingsliste.map((melding) => melding.kanal).unique();
-        const detaljer = <VarselMeldinger sortertMeldingsliste={meldingsliste} />;
-
-        return { datoer, tittel, kanaler, detaljer };
-    };
+const DittNavInformasjonsLinjerV2 = (varsel: DittNavEvent) => {
+    const varslingsTidspunkt = varsel.varslingsTidspunkt;
 
     return (
-        <div className={'flex flex-col w-2/3 max-h-screen overflow-auto pb-6'}>
-            <Alert variant="info" className={'my-4'} fullWidth={true} contentMaxWidth={false}>
-                Varsler vises kun ett år tilbake i tid. Dersom man trenger å se informasjon om eldre varsler kan man
-                lage en sak i porten for manuell uthenting.
-            </Alert>
-            {varslerResult.feil.length > 0 && (
-                <Alert variant="error" className="blokk-xs my-4">
-                    {varsler.feil.join('. ')}
-                </Alert>
-            )}
-            <Table size={'small'} zebraStripes className={'border border-gray-400 mb-2'}>
-                <Table.Header textSize={'small'}>
-                    <Table.Row>
-                        <Table.HeaderCell />
-                        <Table.HeaderCell className={'w-36'}>Dato</Table.HeaderCell>
-                        <Table.HeaderCell className={'w-24'}>Status</Table.HeaderCell>
-                        <Table.HeaderCell>Type</Table.HeaderCell>
-                        <Table.HeaderCell className={'w-48'}>Kanal</Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {varselPagniated.map((item: VarselModell) => {
-                        const data = dataExtractor(item);
-                        return (
-                            <>
-                                <Table.ExpandableRow key={data.tittel} content={data.detaljer}>
-                                    <Table.DataCell align="left" textSize={'small'}>
-                                        {data.datoer}
-                                    </Table.DataCell>
-                                    <Table.DataCell align="left" textSize={'small'}>
-                                        {data.harFeilteVarsel ? (
-                                            <ExclamationmarkTriangleFillIcon fontSize="1.5rem" />
-                                        ) : (
-                                            <CheckmarkCircleFillIcon fontSize="1.5rem" />
-                                        )}
-                                    </Table.DataCell>
-                                    <Table.DataCell align="left" textSize={'small'}>
-                                        {data.tittel}
-                                    </Table.DataCell>
-                                    <Table.DataCell align="left" textSize={'small'}>
-                                        {data.kanaler.join(', ')}
-                                    </Table.DataCell>
-                                </Table.ExpandableRow>
-                            </>
-                        );
-                    })}
-                </Table.Body>
-            </Table>
-            <Pagination
-                className={'border border-gray-300'}
-                page={page}
-                onPageChange={setPage}
-                count={Math.ceil(varselElementer.length / rowsPerPage)}
-                size="small"
+        <>
+            <DittNavInformasjonsLinjer produsent={varsel.produsent} tekst={varsel.tekst} link={varsel.link} />
+            <DittNavInformasjonsLinje
+                tittel="Varslet: "
+                tekst={`${formaterDato(varslingsTidspunkt.tidspunkt)} - ${varslingsTidspunkt.sendteKanaler.join(', ')}`}
             />
-        </div>
+            {varslingsTidspunkt.renotifikasjonTidspunkt && (
+                <DittNavInformasjonsLinje
+                    tittel="Revarslet: "
+                    tekst={`${formaterDato(
+                        varslingsTidspunkt.renotifikasjonTidspunkt
+                    )} - ${varslingsTidspunkt.renotifikasjonsKanaler.join(', ')}`}
+                />
+            )}
+            {varslingsTidspunkt.harFeilteVarslinger && (
+                <>
+                    <hr />
+                    <FeilteVarslingerListe
+                        tittel="Varslingsfeil"
+                        feilteVarslinger={varslingsTidspunkt.feilteVarsliner}
+                    />
+                </>
+            )}
+            {varslingsTidspunkt.harFeilteRevarslinger && (
+                <>
+                    <hr />
+                    <FeilteVarslingerListe
+                        tittel="Revarslingsfeil"
+                        feilteVarslinger={varslingsTidspunkt.feilteRevarslinger}
+                    />
+                </>
+            )}
+        </>
+    );
+};
+
+const dataExtractor = (
+    varsel: UnifiedVarselModell
+): { datoer: string[]; tittel: string; kanaler: string[]; harFeilteVarsel?: boolean; detaljer?: ReactNode } => {
+    if (isDittNavEvent(varsel)) {
+        const varslingsTidspunkt = varsel.varslingsTidspunkt;
+        const aktiv = varsel.aktiv ? '' : ' (Ferdigstilt)';
+        const tittel = `Notifikasjon${aktiv}: ${varsel.tekst}`;
+
+        if (!varslingsTidspunkt || !varslingsTidspunkt.tidspunkt) {
+            const datoer = [formaterDato(varsel.forstBehandlet)];
+            const kanaler = ['DITT_NAV', ...varsel.eksternVarslingKanaler];
+
+            const detaljer = (
+                <DittNavInformasjonsLinjer produsent={varsel.produsent} tekst={varsel.tekst} link={varsel.link} />
+            );
+
+            return { datoer, tittel, kanaler, detaljer };
+        }
+
+        const datoer = [formaterDato(varslingsTidspunkt.tidspunkt)];
+        if (varslingsTidspunkt.renotifikasjonTidspunkt) {
+            datoer.push(formaterDato(varslingsTidspunkt.renotifikasjonTidspunkt));
+        }
+
+        const kanaler = [
+            'DITT_NAV',
+            ...varsel.eksternVarslingKanaler,
+            ...varslingsTidspunkt.renotifikasjonsKanaler
+        ].unique();
+
+        const harFeilteVarsel = varslingsTidspunkt.harFeilteVarslinger || varslingsTidspunkt.harFeilteRevarslinger;
+        const detaljer = <DittNavInformasjonsLinjerV2 varsel={varsel} />;
+
+        return { datoer, tittel, kanaler, detaljer, harFeilteVarsel };
+    }
+
+    const meldingsliste = varsel.meldingListe?.sort(datoSynkende((melding) => melding.utsendingsTidspunkt)) || [];
+    const datoer = meldingsliste.map((melding) => formaterDato(melding.utsendingsTidspunkt)).unique();
+    const tittel = getVarselTekst(varsel);
+    const kanaler = meldingsliste.map((melding) => melding.kanal).unique();
+    const detaljer = <VarselMeldinger sortertMeldingsliste={meldingsliste} />;
+
+    return { datoer, tittel, kanaler, detaljer };
+};
+
+function VarslerNy() {
+    const rowsPerPage = 22;
+    const { page } = routeApi.useSearch();
+    const navigate = routeApi.useNavigate();
+    const varslerResponse = useVarslerData();
+    const varslerResult: VarslerResult = varslerResponse.data || { feil: [], varsler: [] };
+
+    const varselElementer = varslerResult.varsler.sort(datoSynkende(datoExtractor));
+    const maxPage = Math.ceil(varselElementer.length / rowsPerPage);
+    const varselPagniated = varselElementer.slice((page - 1) * rowsPerPage, page * rowsPerPage);
+
+    const onPageClick = (pageNumber: number) => {
+        navigate({ search: { page: pageNumber } });
+    };
+
+    useEffect(() => {
+        if (page > maxPage) {
+            navigate({ search: { page: 1 } });
+        }
+    }, [maxPage]);
+
+    return (
+        <QueryErrorBoundary
+            error={varslerResponse.error}
+            loading={varslerResponse.isLoading}
+            loader={
+                <>
+                    <Skeleton variant="text" width="100%" />
+                    <Skeleton variant="text" width="100%" />
+                    <Skeleton variant="text" width="100%" />
+                </>
+            }
+        >
+            <div className={'flex flex-col w-2/3 max-h-screen overflow-auto pb-6'}>
+                <Alert variant="info" className={'my-4'} fullWidth={true} contentMaxWidth={false}>
+                    Varsler vises kun ett år tilbake i tid. Dersom man trenger å se informasjon om eldre varsler kan man
+                    lage en sak i porten for manuell uthenting.
+                </Alert>
+                {varslerResult.feil.length > 0 && (
+                    <Alert variant="error" className="blokk-xs my-4">
+                        {varsler.feil.join('. ')}
+                    </Alert>
+                )}
+                <Table size={'small'} zebraStripes className={'border border-gray-300 mb-2'}>
+                    <Table.Header textSize={'small'}>
+                        <Table.Row>
+                            <Table.HeaderCell />
+                            <Table.HeaderCell className={'w-36'}>Dato</Table.HeaderCell>
+                            <Table.HeaderCell className={'w-24'}>Status</Table.HeaderCell>
+                            <Table.HeaderCell>Type</Table.HeaderCell>
+                            <Table.HeaderCell className={'w-48'}>Kanal</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {varselPagniated.map((item: VarselModell) => {
+                            const data = dataExtractor(item);
+                            return (
+                                <>
+                                    <Table.ExpandableRow key={data.tittel} content={data.detaljer}>
+                                        <Table.DataCell align="left" textSize={'small'}>
+                                            {data.datoer}
+                                        </Table.DataCell>
+                                        <Table.DataCell align="left" textSize={'small'}>
+                                            {data.harFeilteVarsel ? (
+                                                <ExclamationmarkTriangleFillIcon fontSize="1.5rem" />
+                                            ) : (
+                                                <CheckmarkCircleFillIcon fontSize="1.5rem" />
+                                            )}
+                                        </Table.DataCell>
+                                        <Table.DataCell align="left" textSize={'small'}>
+                                            {data.tittel}
+                                        </Table.DataCell>
+                                        <Table.DataCell align="left" textSize={'small'}>
+                                            {data.kanaler.join(', ')}
+                                        </Table.DataCell>
+                                    </Table.ExpandableRow>
+                                </>
+                            );
+                        })}
+                    </Table.Body>
+                </Table>
+                <Pagination page={page} onPageChange={onPageClick} count={maxPage} size="small" />
+            </div>
+        </QueryErrorBoundary>
     );
 }
 
--- src/routes/new/person/varsler.lazy.tsx
+++ src/routes/new/person/varsler.lazy.tsx
@@ -1,6 +1,10 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
 import VarslerNy from 'src/components/varsler/VarslerNy';
+import { z } from 'zod';
+
+const varslerSearchSchema = z.object({ page: z.number().catch(1) });
 
 export const Route = createLazyFileRoute('/new/person/varsler')({
-    component: VarslerNy
+    component: VarslerNy,
+    validateSearch: varslerSearchSchema
 });
