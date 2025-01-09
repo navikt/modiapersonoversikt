fc08fad6fa260efe302fc9bd5c56622c6133044b
abrha <abrha.kiros.abrha@nav.no>
2025-01-09 14:03:13 +0100

    [KAIZEN-0] fiks for bug i varsler routing

--- src/routes/new/person/varsler.lazy.tsx
+++ src/routes/new/person/varsler.lazy.tsx
@@ -1,10 +1,6 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
 import VarslerNy from 'src/components/varsler/VarslerNy';
-import { z } from 'zod';
-
-const varslerSearchSchema = z.object({ page: z.number().catch(1) });
 
 export const Route = createLazyFileRoute('/new/person/varsler')({
-    component: VarslerNy,
-    validateSearch: varslerSearchSchema
+    component: VarslerNy
 });
