225dafd1c459f96506a4a001ca35525278c45c0c
Odd Gunnar <oddgunnarfatland@gmail.com>
2025-01-27 10:38:05 +0100

    Fjern fra utbetaling

--- src/routes/new/person/utbetaling.lazy.tsx
+++ src/routes/new/person/utbetaling.lazy.tsx
@@ -1,10 +1,9 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
-import { LukkbarNyMelding } from 'src/components/melding/LukkbarNyMelding';
 
 export const Route = createLazyFileRoute('/new/person/utbetaling')({
     component: RouteComponent
 });
 
 function RouteComponent() {
-    return <LukkbarNyMelding />;
+    return <div>Hello "/new/person/utbetaling"!</div>;
 }
