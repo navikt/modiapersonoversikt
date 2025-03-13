fbb74bf12b0f53cb9050b36c566df2cd0f9f2db0
Ludvig Hozman <ludvig123@gmail.com>
2025-03-13 15:28:55 +0100

    Fiks feil i type for enheter

--- src/lib/hooks/useEnhetsnavn.ts
+++ src/lib/hooks/useEnhetsnavn.ts
@@ -2,5 +2,5 @@ import { useEnheter } from 'src/lib/clients/modiapersonoversikt-api';
 
 export const useEnhetsnavn = (enhetId?: string) => {
     const enheter = useEnheter().data?.enhetliste ?? [];
-    return enheter.find((enhet) => enhet.id === enhetId)?.navn ?? 'Ukjent enhet';
+    return enheter.find((enhet) => enhet.enhetId === enhetId)?.navn ?? 'Ukjent enhet';
 };
