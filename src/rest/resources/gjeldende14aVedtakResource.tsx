71a24ffdf8a9ab1ad5caf8975ab0c367e1962bb3
Abrha K. Abrha <116550783+abrhanav@users.noreply.github.com>
2025-03-17 15:19:43 +0100

    [KAIZEN-0] vis gjeldende 14a vedtak i oppfølging (#2614)

--- src/rest/resources/gjeldende14aVedtakResource.tsx
+++ src/rest/resources/gjeldende14aVedtakResource.tsx
@@ -2,14 +2,14 @@ import { type UseQueryResult, useQuery } from '@tanstack/react-query';
 import { type FetchError, post } from 'src/api/api';
 import { apiBaseUri } from 'src/api/config';
 import { usePersonAtomValue } from 'src/lib/state/context';
-import type { Siste14aVedtakResponse } from 'src/models/oppfolging';
+import type { Gjeldende14aVedtakResponse } from 'src/models/oppfolging';
 
 const resource = {
-    useFetch(): UseQueryResult<Siste14aVedtakResponse, FetchError> {
+    useFetch(): UseQueryResult<Gjeldende14aVedtakResponse, FetchError> {
         const fnr = usePersonAtomValue();
         return useQuery({
-            queryKey: ['siste14AVedtak', fnr],
-            queryFn: () => post(`${apiBaseUri}/v2/oppfolging/siste14AVedtak`, { fnr })
+            queryKey: ['gjeldende14AVedtak', fnr],
+            queryFn: () => post(`${apiBaseUri}/v2/oppfolging/hent-gjeldende-14a-vedtak`, { fnr })
         });
     }
 };
