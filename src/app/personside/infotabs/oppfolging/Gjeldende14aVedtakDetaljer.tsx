f0e1bc8296a43a57c7773f0dea04e4b1c2ce5e1b
Abrha K. Abrha <116550783+abrhanav@users.noreply.github.com>
2025-03-17 15:43:35 +0100

    [KAIZEN-0] vis gjeldende 14a vedtak i oppfølging (#2615)

--- src/app/personside/infotabs/oppfolging/Gjeldende14aVedtakDetaljer.tsx
+++ src/app/personside/infotabs/oppfolging/Gjeldende14aVedtakDetaljer.tsx
@@ -14,9 +14,9 @@ function Gjeldende14aVedtakDetaljer() {
     const headerId = useRef(guid());
     const detaljer = gjeldende14aVedtakData?.gjeldende14aVedtak;
     const responsePlaceholder = {
-        returnOnError: 'Kunne ikke laste siste 14a vedtak',
-        returnOnNotFound: 'Kunne ikke finne siste 14a vedtak',
-        returnOnForbidden: 'Du har ikke tilgang til siste 14a vedtak'
+        returnOnError: 'Kunne ikke laste gjeldende 14a vedtak',
+        returnOnNotFound: 'Kunne ikke finne gjeldende 14a vedtak',
+        returnOnForbidden: 'Du har ikke tilgang til gjeldende 14a vedtak'
     };
 
     const gjeldende14aVedtakDetailElement = () => {
