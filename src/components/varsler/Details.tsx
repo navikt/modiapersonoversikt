9e1a2338fd0565a735201c31b50ff2abea37b347
abrha <abrha.kiros.abrha@nav.no>
2025-04-15 13:09:01 +0200

    [KAIZEN-0] ny varsler side

---
+++ src/components/varsler/Details.tsx
@@ -0,0 +1,136 @@
+import { ErrorMessage, HStack, Heading, Skeleton, VStack } from '@navikt/ds-react';
+import { getRouteApi } from '@tanstack/react-router';
+import { Suspense } from 'react';
+import Card from 'src/components/Card';
+import ErrorBoundary from 'src/components/ErrorBoundary';
+import { type VarselData, useFilterVarlser } from 'src/components/varsler/List/utils';
+import type { FeiletVarsling, Varsel } from 'src/lib/types/modiapersonoversikt-api';
+import { ENDASH, emptyReplacement, formaterDato } from 'src/utils/string-utils';
+
+const routeApi = getRouteApi('/new/person/varsler');
+
+const FeilteVarslingerListe = ({
+    tittel,
+    feilteVarslinger
+}: {
+    tittel: string;
+    feilteVarslinger: FeiletVarsling[];
+}) => {
+    return (
+        <div className="my-1">
+            <div className="font-bold">{tittel}</div>
+            <div>
+                {feilteVarslinger.map((varsling) => (
+                    <div key={`${varsling.tidspunkt} - ${varsling.kanal}`}>
+                        <ErrorMessage size="small" showIcon>
+                            {formaterDato(varsling.tidspunkt)} - {varsling.kanal}: {varsling.feilmelding}
+                        </ErrorMessage>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+const DittNavInformasjonsLinje = ({
+    tittel,
+    tekst,
+    className
+}: {
+    tittel: string;
+    tekst: string;
+    className?: string;
+}) => {
+    return (
+        <HStack gap="4">
+            <div className="font-bold">{tittel}</div>
+            <div className={`${className} text-base`}>{tekst}</div>
+        </HStack>
+    );
+};
+
+const DittNavInformasjonsLinjer = ({
+    varsel,
+    kanaler
+}: {
+    varsel: Varsel;
+    kanaler: string[];
+}) => {
+    return (
+        <VStack gap="1" className="p-2">
+            <Heading level="3" size="xsmall">
+                {varsel.tekst}
+            </Heading>
+            <DittNavInformasjonsLinje tittel="Produsert av:" tekst={emptyReplacement(varsel.produsent, ENDASH)} />
+            <DittNavInformasjonsLinje tittel="Kanaler:" tekst={emptyReplacement(kanaler?.join(', '), ENDASH)} />
+        </VStack>
+    );
+};
+
+const DittNavInformasjonsLinjerV2 = ({
+    varsel,
+    kanaler
+}: {
+    varsel: Varsel;
+    kanaler: string[];
+}) => {
+    const varslingsTidspunkt = varsel.varslingsTidspunkt;
+
+    return (
+        <>
+            <DittNavInformasjonsLinjer varsel={varsel} kanaler={kanaler} />
+            <VStack gap="1" className="px-2">
+                <DittNavInformasjonsLinje
+                    tittel="Varslet: "
+                    tekst={
+                        varslingsTidspunkt?.tidspunkt
+                            ? `${formaterDato(varslingsTidspunkt.tidspunkt)} - ${varslingsTidspunkt.sendteKanaler.join(', ')}`
+                            : '-'
+                    }
+                />
+                {varslingsTidspunkt?.renotifikasjonTidspunkt && (
+                    <DittNavInformasjonsLinje
+                        tittel="Revarslet: "
+                        tekst={`${formaterDato(
+                            varslingsTidspunkt.renotifikasjonTidspunkt
+                        )} - ${varslingsTidspunkt.renotifikasjonsKanaler.join(', ')}`}
+                    />
+                )}
+                {varslingsTidspunkt?.harFeilteVarslinger && (
+                    <FeilteVarslingerListe
+                        tittel="Varslingsfeil"
+                        feilteVarslinger={varslingsTidspunkt.feilteVarsliner}
+                    />
+                )}
+                {varslingsTidspunkt?.harFeilteRevarslinger && (
+                    <FeilteVarslingerListe
+                        tittel="Revarslingsfeil"
+                        feilteVarslinger={varslingsTidspunkt.feilteRevarslinger}
+                    />
+                )}
+            </VStack>
+        </>
+    );
+};
+
+const varselDetailExtractor = (varsel: VarselData) => {
+    if (varsel.erVarselV2) {
+        return <DittNavInformasjonsLinjerV2 varsel={varsel.event} kanaler={varsel.kanaler} />;
+    }
+
+    return <DittNavInformasjonsLinjer varsel={varsel.event} kanaler={varsel.kanaler} />;
+};
+
+export const VarselDetail = () => {
+    const { id } = routeApi.useSearch();
+    const varlser = useFilterVarlser();
+    const selectedVarsler = varlser.find((item) => item.eventId === id);
+
+    return (
+        <ErrorBoundary boundaryName="vaslerDetaljer">
+            <Suspense fallback={<Skeleton variant="rounded" height="200" />}>
+                <Card>{selectedVarsler && varselDetailExtractor(selectedVarsler)}</Card>
+            </Suspense>
+        </ErrorBoundary>
+    );
+};
--- src/routes/new/person/varsler.tsx
+++ src/routes/new/person/varsler.tsx
@@ -1,8 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { keepQueryParams } from 'src/utils/keepQueryParams';
 import { z } from 'zod';
 
-const varslerSearchSchema = z.object({ page: z.number().catch(1) });
+const varselSearchSchema = z.object({
+    id: z.string().optional()
+});
 
 export const Route = createFileRoute('/new/person/varsler')({
-    validateSearch: varslerSearchSchema
+    validateSearch: varselSearchSchema,
+    search: {
+        middlewares: [keepQueryParams(['id'])]
+    }
 });
