7b64e46eae8c98ca67d77477bbd3eeedd8dd58cd
Nummedal <sigrun.asheim.nummedal@nav.no>
2025-07-31 15:51:53 +0200

    Lag standardstekst modal responsive

--- src/components/melding/standardtekster/StandardTeksterModal.tsx
+++ src/components/melding/standardtekster/StandardTeksterModal.tsx
@@ -32,7 +32,7 @@ function StandardTekstModal({
                 width="50rem"
                 header={{ heading: 'Velg standardtekst', closeButton: false }}
             >
-                <Modal.Body className="overflow-y-hidden ">
+                <Modal.Body className="sm:overflow-y-hidden ">
                     <StandardTekster modalRef={modalRef} velgTekst={setTekst} />
                 </Modal.Body>
             </Modal>
