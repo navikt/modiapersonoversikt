84ebe4093c59ce3e66e9c1f52636685712a09aaf
Nummedal <sigrun.asheim.nummedal@nav.no>
2025-08-08 13:34:42 +0200

    Fjern ubrukte feature toggles

--- src/components/featureToggle/toggleIDs.ts
+++ src/components/featureToggle/toggleIDs.ts
@@ -2,11 +2,7 @@ export enum FeatureToggles {
     VisPromptMeldingSending = 'modiapersonoversikt.vis-promt-naar-melding-sendes',
     VisSiste14aVedtak = 'modiapersonoversikt.vis-siste-14a-vedtak',
     JournalforUtenSvar = 'modiapersonoversikt.meldinger-uten-svar-kan-journalfores',
-    VisDraftStatus = 'modiapersonoversikt.vis-draft-status',
     BrukNyTiltakspenger = 'modiapersonoversikt.bruk-ny-tiltakspenger',
-    FnrSokForInnkreving = 'modiapersonoversikt.fnr-sok-for-innkreving',
-    OrgnrSokForInnkreving = 'modiapersonoversikt.orgnr-sok-for-innkreving',
-    NyAvansertSok = 'modiapersonoversikt.ny-avansert-sok',
     NyModiaKnapp = 'modiapersonoversikt.ny-modia-knapp',
     TilgangsMaskin = 'modiapersonoversikt.tilgangsmaskin'
 }
--- src/test/testStore.ts
+++ src/test/testStore.ts
@@ -87,10 +87,6 @@ export function setupReactQueryMocks() {
         [FeatureToggles.VisPromptMeldingSending]: true,
         [FeatureToggles.VisSiste14aVedtak]: true,
         [FeatureToggles.BrukNyTiltakspenger]: true,
-        [FeatureToggles.VisDraftStatus]: true,
-        [FeatureToggles.NyAvansertSok]: true,
-        [FeatureToggles.FnrSokForInnkreving]: true,
-        [FeatureToggles.OrgnrSokForInnkreving]: true,
         [FeatureToggles.NyModiaKnapp]: true,
         [FeatureToggles.TilgangsMaskin]: true
     });
