41dc5174e918ee4fcf717a431133812eba593acb
Nummedal <sigrun.asheim.nummedal@nav.no>
2025-08-08 13:37:01 +0200

    Fjern feature toggles fra mock

--- src/mock/featureToggle-mock.ts
+++ src/mock/featureToggle-mock.ts
@@ -3,19 +3,9 @@ import type { FeatureToggleResponse } from '../models/featureToggle';
 
 export function mockFeatureToggle(toggleId: FeatureToggles): FeatureToggleResponse {
     switch (toggleId) {
-        case FeatureToggles.BrukNyDecorator:
-            return true;
         case FeatureToggles.JournalforUtenSvar:
             return true;
-        case FeatureToggles.VisDraftStatus:
-            return true;
         case FeatureToggles.BrukNyTiltakspenger:
-            return true;
-        case FeatureToggles.FnrSokForInnkreving:
-            return true;
-        case FeatureToggles.OrgnrSokForInnkreving:
-            return true;
-        case FeatureToggles.NyAvansertSok:
             return true;
         case FeatureToggles.NyModiaKnapp:
             return true;
