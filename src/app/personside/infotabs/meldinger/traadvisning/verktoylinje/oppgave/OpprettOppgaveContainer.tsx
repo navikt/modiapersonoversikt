07f428ca2f695c74e50dbc47a18241d34c8fb843
Nummedal <sigrun.asheim.nummedal@nav.no>
2025-08-12 10:27:18 +0200

    Fjern ubrukt prop

--- src/app/personside/infotabs/meldinger/traadvisning/verktoylinje/oppgave/OpprettOppgaveContainer.tsx
+++ src/app/personside/infotabs/meldinger/traadvisning/verktoylinje/oppgave/OpprettOppgaveContainer.tsx
@@ -1,12 +1,11 @@
-import ErrorBoundary from '../../../../../../../components/ErrorBoundary';
-import { CenteredLazySpinner } from '../../../../../../../components/LazySpinner';
-import type { Traad } from '../../../../../../../models/meldinger/meldinger';
-import gsaktemaResource from '../../../../../../../rest/resources/gsaktemaResource';
+import ErrorBoundary from 'src/components/ErrorBoundary';
+import { CenteredLazySpinner } from 'src/components/LazySpinner';
+import type { Traad } from 'src/models/meldinger/meldinger';
+import gsaktemaResource from 'src/rest/resources/gsaktemaResource';
 import OppgaveSkjema from './OppgaveSkjema';
 
 interface Props {
     lukkPanel: () => void;
-    onSuccessCallback?: () => void;
     valgtTraad: Traad;
 }
 
@@ -14,13 +13,8 @@ function OpprettOppgaveContainer(props: Props) {
     return gsaktemaResource.useRenderer({
         ifPending: <CenteredLazySpinner aria-label="Laster tema" type="L" />,
         ifData: (gsaktema) => (
-            <ErrorBoundary boundaryName={'OpprettOppgaveContainer'}>
-                <OppgaveSkjema
-                    gsakTema={gsaktema}
-                    lukkPanel={props.lukkPanel}
-                    onSuccessCallback={props.onSuccessCallback}
-                    valgtTraad={props.valgtTraad}
-                />
+            <ErrorBoundary boundaryName="OpprettOppgaveContainer">
+                <OppgaveSkjema gsakTema={gsaktema} lukkPanel={props.lukkPanel} valgtTraad={props.valgtTraad} />
             </ErrorBoundary>
         )
     });
