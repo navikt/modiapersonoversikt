d072e24f160cd09cd006044d9f781f154d9c9728
Nummedal <sigrun.asheim.nummedal@nav.no>
2025-08-28 14:10:50 +0200

    Fjern detaljbokser fra oversikttab

--- src/components/oversikt/OversiktNy.tsx
+++ src/components/oversikt/OversiktNy.tsx
@@ -1,45 +1,9 @@
 import { ExternalLinkIcon } from '@navikt/aksel-icons';
-import { BodyLong, BodyShort, Button, HGrid, HStack, Heading, VStack } from '@navikt/ds-react';
+import { BodyShort, Button, HGrid, HStack, Heading, VStack } from '@navikt/ds-react';
 import Card from 'src/components/Card';
 import { Link } from 'src/components/Link';
 
-const menuItems = [
-    {
-        title: 'Oppfølging',
-        href: '/new/person/oppfolging',
-        beskrivelse: 'Kort beskrivelse av hva oppfølging er'
-    },
-    {
-        title: 'Kommunikasjon',
-        href: '/new/person/meldinger',
-        beskrivelse: 'Kort beskrivelse av hva kommunikasjon er'
-    },
-    {
-        title: 'Utbetaling',
-        href: '/new/person/utbetaling',
-        beskrivelse: 'Kort beskrivelse av hva utbetaling er'
-    },
-    {
-        title: 'Ytelser',
-        href: '/new/person/ytelser',
-        beskrivelse: 'Kort beskrivelse av hva ytelser er'
-    },
-    {
-        title: 'Dokumenter',
-        href: '/new/person/saker',
-        beskrivelse: 'Kort beskrivelse av hva dokumenter er'
-    },
-    {
-        title: 'Oppgaver',
-        href: '/new/person/oppgaver',
-        beskrivelse: 'Kort beskrivelse av hva oppgaver er'
-    },
-    {
-        title: 'Varsler',
-        href: '/new/person/varsler',
-        beskrivelse: 'Kort beskrivelse av hva varsler er'
-    }
-] as const;
+const menuItems = [] as const;
 
 const OversiktDetailCard = ({
     title,
@@ -80,14 +44,6 @@ const OversiktDetailCard = ({
 function OversiktNy() {
     return (
         <VStack gap="4" paddingBlock="2" overflow="auto">
-            <Card padding="4">
-                <VStack align="center">
-                    <Heading as="h2" size="medium">
-                        Modia personoversikt
-                    </Heading>
-                    <BodyLong spacing>Kort beskrivelse av hva modia er</BodyLong>
-                </VStack>
-            </Card>
             <HGrid gap="4" columns={{ sm: 1, md: 2, lg: 3 }}>
                 {menuItems.map((item) => {
                     return (
