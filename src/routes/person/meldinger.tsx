a3cc0949850eda8cb91d49f1eec5cb3e6212d0be
Nummedal <sigrun.asheim.nummedal@nav.no>
2025-11-05 11:46:36 +0100

    Fiks circular import bug

--- src/routes/person/meldinger.tsx
+++ src/routes/person/meldinger.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 import MeldingerContainer from 'src/app/personside/infotabs/meldinger/MeldingerContainer';
-import { keepQueryParams } from 'src/utils/keepQueryParams';
+import { keepQueryParamsSimple } from 'src/utils/keepQueryParamsSimple';
 import { z } from 'zod';
 
 const meldingerSearchSchema = z.object({
@@ -11,6 +11,6 @@ export const Route = createFileRoute('/person/meldinger')({
     component: MeldingerContainer,
     validateSearch: meldingerSearchSchema,
     search: {
-        middlewares: [keepQueryParams(['traadId'])]
+        middlewares: [keepQueryParamsSimple(['traadId'])]
     }
 });
