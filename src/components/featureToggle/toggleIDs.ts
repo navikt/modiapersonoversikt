8ab68d7ae7f6d09c2948bf08690cce9d3087dee1
Nummedal <sigrun.asheim.nummedal@nav.no>
2025-11-20 14:23:20 +0100

    Legg til featuretoggle for alle infotrygd ytelser

--- src/components/featureToggle/toggleIDs.ts
+++ src/components/featureToggle/toggleIDs.ts
@@ -2,5 +2,8 @@ export enum FeatureToggles {
     VisPromptMeldingSending = 'modiapersonoversikt.vis-promt-naar-melding-sendes',
     VisSiste14aVedtak = 'modiapersonoversikt.vis-siste-14a-vedtak',
     JournalforUtenSvar = 'modiapersonoversikt.meldinger-uten-svar-kan-journalfores',
-    NyModiaKnapp = 'modiapersonoversikt.ny-modia-knapp'
+    NyModiaKnapp = 'modiapersonoversikt.ny-modia-knapp',
+    InfotrygdForeldrepenger = 'modiapersonoversikt.infotrygd-foreldrepenger',
+    InfotrygdSykepenger = 'modiapersonoversikt.infotrygd-sykepenger',
+    InfotrygdPleiepenger = 'modiapersonoversikt.infotrygd-pleiepenger'
 }
--- src/mock/featureToggle-mock.ts
+++ src/mock/featureToggle-mock.ts
@@ -7,6 +7,12 @@ export function mockFeatureToggle(toggleId: FeatureToggles): FeatureToggleRespon
             return true;
         case FeatureToggles.NyModiaKnapp:
             return true;
+        case FeatureToggles.InfotrygdForeldrepenger:
+            return true;
+        case FeatureToggles.InfotrygdPleiepenger:
+            return true;
+        case FeatureToggles.InfotrygdSykepenger:
+            return true;
         default:
             return Math.random() > 0.5;
     }
