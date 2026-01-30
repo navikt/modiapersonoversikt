aecc7a9305d1f96ec64d78e2a275104d272b92a0
Nummedal <sigrun.asheim.nummedal@nav.no>
2026-01-30 10:13:51 +0100

    Start ny dokumenter page

--- src/app/personside/infotabs/utils/useOpenTab.ts
+++ src/app/personside/infotabs/utils/useOpenTab.ts
@@ -3,7 +3,11 @@ import { INFOTABS, type InfotabConfig } from '../InfoTabEnum';
 
 export function getOpenTabFromRouterPath(currentPath: string): InfotabConfig {
     const pathfragments = currentPath.toLowerCase().split('/');
-    const infotabsWithOppgave = { ...INFOTABS, OPPGAVER: { tittel: 'Oppgaver', path: 'oppgaver' } };
+    const infotabsWithOppgave = {
+        ...INFOTABS,
+        OPPGAVER: { tittel: 'Oppgaver', path: 'oppgaver' },
+        DOKUMENTER: { tittel: 'Dokumenter', path: 'dokumenter' }
+    };
     const infotab: InfotabConfig | undefined = Object.values(infotabsWithOppgave).find((infotab) =>
         pathfragments.includes(infotab.path)
     );
