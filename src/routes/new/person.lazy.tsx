9d726668a8e77411cadad2f102712ba3e3738fcb
Nummedal <sigrun.asheim.nummedal@nav.no>
2026-02-13 14:52:38 +0100

    Sett overflow-show til overflow auto

--- src/routes/new/person.lazy.tsx
+++ src/routes/new/person.lazy.tsx
@@ -25,7 +25,7 @@ function PersonRoute() {
 function PersonLayout() {
     return (
         <VStack className="new-modia h-dvh overflow-hidden" flexGrow="1">
-            <VStack className="shrink-0 px-1 mt-1 overflow-show">
+            <VStack className="shrink-0 px-1 mt-1 overflow-auto">
                 <PersonLinje />
             </VStack>
             <HStack minHeight="0" gap="1" padding="1" wrap={false} className="h-full">
