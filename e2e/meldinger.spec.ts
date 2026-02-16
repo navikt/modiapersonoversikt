8e963d7d423136b434d81871c22234261029745c
Nummedal <sigrun.asheim.nummedal@nav.no>
2026-02-16 09:01:47 +0100

    Fjer tanstack knapp i meldingertest

--- e2e/meldinger.spec.ts
+++ e2e/meldinger.spec.ts
@@ -45,6 +45,7 @@ test('Send ny melding', async ({ page }) => {
     await page.getByLabel('Temagruppe').selectOption('Pensjon');
 
     await page.addStyleTag({ content: '#ny-modia-knapp-wrapper { display: none !important; }' });
+    await page.addStyleTag({ content: '.TanStackRouterDevtools { display: none !important; }' });
     await page.getByTestId('svar-knapp').click();
 
     const newTraad = page.getByTestId('traaditem').first();
