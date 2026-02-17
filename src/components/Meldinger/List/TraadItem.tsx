2e9c7b0e87fd06195e35a43e67d0bbd47214584e
Nummedal <sigrun.asheim.nummedal@nav.no>
2026-02-17 09:10:05 +0100

    Fiks tagger på traaden

--- src/components/Meldinger/List/TraadItem.tsx
+++ src/components/Meldinger/List/TraadItem.tsx
@@ -1,4 +1,13 @@
-import { BellIcon, ChatElipsisIcon } from '@navikt/aksel-icons';
+import {
+    BellIcon,
+    ChatElipsisIcon,
+    EnterIcon,
+    PencilIcon,
+    TabsRemoveIcon,
+    TasklistIcon,
+    TrashIcon,
+    XMarkOctagonIcon
+} from '@navikt/aksel-icons';
 import { Detail, HStack, Label, Link, Tag, VStack } from '@navikt/ds-react';
 import { getRouteApi } from '@tanstack/react-router';
 import { atom, useAtomValue } from 'jotai';
@@ -27,7 +36,7 @@ function TildeltSaksbehandler({ traadId }: { traadId: string }) {
 
     if (tildeltTilBruker.map((oppgave) => oppgave.traadId).includes(traadId)) {
         return (
-            <Tag size="xsmall" variant="info-moderate">
+            <Tag size="xsmall" variant="success-moderate" icon={<TasklistIcon aria-hidden />}>
                 Tildelt meg
             </Tag>
         );
@@ -38,42 +47,41 @@ function TildeltSaksbehandler({ traadId }: { traadId: string }) {
 function UnderArbeid({ traadId }: { traadId: string }) {
     const isUnderArbeid = useAtomValue(useMemo(() => atom((get) => get(dialogUnderArbeidAtom) === traadId), [traadId]));
 
-    if (isUnderArbeid)
-        return (
-            <Tag size="xsmall" variant="info-moderate">
-                Under arbeid
-            </Tag>
-        );
-
-    return null;
+    if (!isUnderArbeid) return null;
+    return (
+        <Tag size="xsmall" variant="info-moderate" icon={<PencilIcon aria-hidden />}>
+            Under arbeid
+        </Tag>
+    );
 }
 
 function Feilsendt({ traad }: { traad: TraadDto }) {
-    if (erFeilsendt(traad)) {
-        return (
-            <Tag size="xsmall" variant="warning-moderate">
-                Feilsendt
-            </Tag>
-        );
-    }
-    return null;
+    if (!erFeilsendt(traad)) return null;
+    return (
+        <Tag size="xsmall" icon={<XMarkOctagonIcon aria-hidden />} variant="alt1-moderate">
+            Feilsendt
+        </Tag>
+    );
 }
 
 function Slettet({ melding }: { melding: Melding }) {
-    if (melding.temagruppe === Temagruppe.InnholdSlettet) {
-        return (
-            <Tag size="xsmall" variant="error-moderate">
-                Slettet
-            </Tag>
-        );
-    }
-
-    return null;
+    if (melding.temagruppe !== Temagruppe.InnholdSlettet) return null;
+    return (
+        <Tag size="xsmall" variant="error-moderate" icon={<TrashIcon aria-hidden />}>
+            Slettet
+        </Tag>
+    );
 }
 
 function Antallmeldinger({ traad }: { traad: TraadDto }) {
     return (
-        <Tag size="xsmall" variant="neutral-moderate" title="Antall meldinger" icon={<ChatElipsisIcon aria-hidden />}>
+        <Tag
+            size="xsmall"
+            variant="neutral-moderate"
+            title="Antall meldinger"
+            className="flex shrink-1"
+            icon={<ChatElipsisIcon aria-hidden />}
+        >
             {traad.meldinger.length}
         </Tag>
     );
@@ -83,8 +91,34 @@ function UbesvartMelding({ traad }: { traad: TraadDto }) {
     const ubesvart = erUbesvartHenvendelseFraBruker(traad);
     if (!ubesvart) return null;
     return (
-        <Tag size="xsmall" variant="alt2-moderate" title="Traad er ubesvart" icon={<BellIcon aria-hidden />}>
-            Ny
+        <Tag
+            size="xsmall"
+            variant="alt2-moderate"
+            className="text-nowrap"
+            data-color="meta-lime"
+            title="Tråd er ubesvart"
+            icon={<BellIcon aria-hidden />}
+        >
+            Ny melding
+        </Tag>
+    );
+}
+
+function AvsluttetMelding({ traad }: { traad: TraadDto }) {
+    const avsluttet = !!traad.avsluttetDato;
+    if (!avsluttet) return null;
+    return (
+        <Tag size="xsmall" variant="info-moderate" title="Tråd er avsluttet" icon={<EnterIcon aria-hidden />}>
+            Avsluttet
+        </Tag>
+    );
+}
+
+function SendtTilSladding({ traad }: { traad: TraadDto }) {
+    if (!traad.sladding) return null;
+    return (
+        <Tag size="xsmall" variant="neutral-moderate" data-color="brand-magenta" icon={<TabsRemoveIcon aria-hidden />}>
+            Sladdet
         </Tag>
     );
 }
@@ -146,23 +180,27 @@ export const TraadItem = ({ traad }: { traad: TraadDto }) => {
             >
                 <VStack>
                     <HStack justify="space-between" gap="1" wrap={false}>
-                        <Label size="small" as="h3">
-                            <Detail visuallyHidden>Tema:</Detail>
-                            {temagruppeTekst(traad.temagruppe as Temagruppe)} ({tittel})
-                        </Label>
-                        <HStack gap="1" align="start" justify="end" wrap={false}>
-                            <UbesvartMelding traad={traad} />
+                        <VStack>
+                            <Label size="small" as="h3">
+                                <Detail className="text-nowrap" visuallyHidden>
+                                    Tema:
+                                </Detail>
+                                {temagruppeTekst(traad.temagruppe as Temagruppe)} ({tittel})
+                            </Label>
+                            <Detail>{datoTekst}</Detail>
+                        </VStack>
+                        <HStack gap="1" align="start" justify="end" maxHeight="max-content">
                             <Antallmeldinger traad={traad} />
+                            <UbesvartMelding traad={traad} />
+                            <UnderArbeid traadId={traad.traadId} />
+                            <Feilsendt traad={traad} />
+                            <Slettet melding={sisteMelding} />
+                            <TildeltSaksbehandler traadId={traad.traadId} />
+                            <AvsluttetMelding traad={traad} />
+                            <SendtTilSladding traad={traad} />
                         </HStack>
                     </HStack>
-                    <Detail>{datoTekst}</Detail>
                     <Detail truncate>{sisteMelding.fritekst}</Detail>
-                    <HStack gap="1" wrap>
-                        <UnderArbeid traadId={traad.traadId} />
-                        <Feilsendt traad={traad} />
-                        <Slettet melding={sisteMelding} />
-                        <TildeltSaksbehandler traadId={traad.traadId} />
-                    </HStack>
                 </VStack>
             </Card>
         </Link>
