4cea331d815bc3872d0095a18821adefb60fcb37
Nummedal <sigrun.asheim.nummedal@nav.no>
2026-02-27 14:57:05 +0100

    Lag periodeselector for nytt datofilter

--- src/components/DateFilters/DatePeriodSelector.tsx
+++ src/components/DateFilters/DatePeriodSelector.tsx
@@ -32,7 +32,7 @@ export const getPeriodFromOption = (periodeValg: PeriodType): DateRange => {
     }
 };
 
-const getOptionFromPeriod = (range: DateRange): PeriodType => {
+export const getOptionFromPeriod = (range: DateRange): PeriodType => {
     const { from, to } = range;
     const now = dayjs();
 
