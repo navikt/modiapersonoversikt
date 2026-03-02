89fefa7985ade9e41c1e9db82ced8510996ae78c
martinsomm-nav <martin.dagfinn.boler.sommerseth@nav.no>
2026-03-02 08:53:01 +0100

    Lagt på Skeleton ved loading av varsler

--- src/components/varsler/index.tsx
+++ src/components/varsler/index.tsx
@@ -1,5 +1,6 @@
-import { Heading, VStack } from '@navikt/ds-react';
+import { Heading, HStack, Skeleton, VStack } from '@navikt/ds-react';
 import { AlertBanner } from 'src/components/AlertBanner';
+import Card from 'src/components/Card';
 import ErrorBoundary from 'src/components/ErrorBoundary';
 import { VarslerListe } from 'src/components/varsler/List';
 import { useFilterVarsler } from 'src/components/varsler/List/utils';
@@ -13,15 +14,37 @@ export const VarslerPage = () => {
 };
 
 const VarslerPageContent = () => {
-    const { errorMessages } = useFilterVarsler();
+    const { errorMessages, isError, isLoading } = useFilterVarsler();
+
+    if (isError) {
+        return <AlertBanner alerts={errorMessages} />;
+    }
 
     return (
-        <VStack gap="1" minHeight="0" overflow="auto">
-            <Heading size="small" visuallyHidden level="2">
-                Varsler
-            </Heading>
-            <AlertBanner alerts={errorMessages} />
-            <VarslerListe />
-        </VStack>
+        <ErrorBoundary boundaryName="VarslerTabell" errorText="Det oppstod en feil undervisning av varsler">
+            <Card padding="4" className="h-full overflow-auto">
+                <AlertBanner alerts={errorMessages} />
+                <VStack gap="1" minHeight="0" overflow="auto">
+                    <HStack align="center" gap="2">
+                        <Heading size="small" visuallyHidden level="2">
+                            Varsler
+                        </Heading>
+                    </HStack>
+                    <VStack gap="4">
+                        {isLoading ? (
+                            <VStack gap="2" marginInline="0 2">
+                                {Array(12)
+                                    .keys()
+                                    .map((i) => (
+                                        <Skeleton key={i} variant="rectangle" height={68} />
+                                    ))}
+                            </VStack>
+                        ) : (
+                            <VarslerListe />
+                        )}
+                    </VStack>
+                </VStack>
+            </Card>
+        </ErrorBoundary>
     );
 };
