4460e2180030433a7278f36e6821acc29a4d098b
martinsomm-nav <martin.dagfinn.boler.sommerseth@nav.no>
2026-03-03 09:55:50 +0100

    Lagt på tooltip på darkmode-knapp

--- src/components/theme/ThemeToggle.tsx
+++ src/components/theme/ThemeToggle.tsx
@@ -1,5 +1,5 @@
 import { ThemeIcon } from '@navikt/aksel-icons';
-import { Button } from '@navikt/ds-react';
+import { Button, Tooltip } from '@navikt/ds-react';
 import { useAtom } from 'jotai';
 import { useCallback } from 'react';
 import { themeAtom } from 'src/lib/state/theme';
@@ -7,18 +7,21 @@ import { themeAtom } from 'src/lib/state/theme';
 export const ThemeIconToggle = () => {
     const [theme, setTheme] = useAtom(themeAtom);
     const endreTilTemaTekst = theme === 'light' ? 'mørkt' : 'lyst';
+    const tooltipTekst = `Endre til ${endreTilTemaTekst} tema`;
 
     const changeTheme = useCallback(() => {
         setTheme((v) => (v === 'light' ? 'dark' : 'light'));
     }, [setTheme]);
 
     return (
-        <Button
-            size="small"
-            variant="tertiary-neutral"
-            onClick={changeTheme}
-            title={`Bytt til ${endreTilTemaTekst} tema`}
-            icon={<ThemeIcon aria-hidden />}
-        />
+        <Tooltip content={tooltipTekst} placement="right">
+            <Button
+                size="small"
+                variant="tertiary-neutral"
+                onClick={changeTheme}
+                title={`Bytt til ${endreTilTemaTekst} tema`}
+                icon={<ThemeIcon aria-hidden />}
+            />
+        </Tooltip>
     );
 };
