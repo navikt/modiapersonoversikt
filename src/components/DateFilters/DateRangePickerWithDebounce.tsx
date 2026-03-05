769a9bbb92e8fde124fcc6697465a070d4a597f7
Nummedal <sigrun.asheim.nummedal@nav.no>
2026-03-05 10:35:53 +0100

    Restet periode i filter

--- src/components/DateFilters/DateRangePickerWithDebounce.tsx
+++ src/components/DateFilters/DateRangePickerWithDebounce.tsx
@@ -1,33 +1,30 @@
-import { HStack, InlineMessage, Select, VStack } from '@navikt/ds-react';
+import { HStack, InlineMessage, VStack } from '@navikt/ds-react';
 import dayjs from 'dayjs';
 import { debounce } from 'lodash';
 import { useState } from 'react';
 import { getOptionFromPeriod, getPeriodFromOption } from 'src/components/DateFilters/DatePeriodSelector';
+import { SelectPeriod } from 'src/components/DateFilters/SelectPeriod';
 import { SingleDatePicker } from 'src/components/DateFilters/SingleDatePicker';
 import { type DateRange, PeriodType } from 'src/components/DateFilters/types';
 
 export const DateRangePickerWithDebounce = ({
     dateRange,
     onRangeChange,
-    showPeriodSelector = true
+    onPeriodChange,
+    period = PeriodType.CUSTOM
 }: {
     dateRange: DateRange;
     onRangeChange: (val?: DateRange) => void;
-    onPeriodChange?: (val?: PeriodType) => void;
-    showPeriodSelector?: boolean;
+    onPeriodChange: (val: PeriodType) => void;
+    period: PeriodType;
 }) => {
     const [error, setError] = useState<string | undefined>();
-    const [selectedPeriode, setSelectedPeriode] = useState<PeriodType | ''>(PeriodType.LAST_30_DAYS);
-
-    const [selectedDateRange, setSelectedDateRange] = useState<DateRange>(
-        selectedPeriode ? getPeriodFromOption(selectedPeriode) : dateRange
-    );
 
     const debounceSetDate = debounce((newRange: DateRange) => {
         onRangeChange(newRange);
         setError(undefined);
         const period = getOptionFromPeriod(newRange);
-        setSelectedPeriode(period === PeriodType.CUSTOM ? '' : period);
+        onPeriodChange(period);
     }, 500);
 
     const debouncedValidate = debounce((validation) => {
@@ -35,51 +32,33 @@ export const DateRangePickerWithDebounce = ({
         if (validation.isBefore || validation.isBefore) setError('Fra dato kan ikke være senere enn til dato');
     }, 500);
 
+    const periodChange = (value: string) => {
+        if (value === '') {
+            onPeriodChange(PeriodType.CUSTOM);
+        } else {
+            const period = value as PeriodType;
+            onPeriodChange(period);
+            onRangeChange(getPeriodFromOption(period));
+        }
+    };
+
     return (
         <VStack>
             <HStack gap="2" wrap={false}>
-                {showPeriodSelector && (
-                    <Select
-                        label="Periode"
-                        size="small"
-                        value={selectedPeriode ?? ''}
-                        onChange={(e) => {
-                            const value = e.target.value;
-                            if (value === '') {
-                                setSelectedPeriode('');
-                            } else {
-                                const period = value as PeriodType;
-                                setSelectedPeriode(period);
-                                setSelectedDateRange(getPeriodFromOption(period));
-                            }
-                        }}
-                    >
-                        <option value="">- Velg periode -</option>
-                        <option key={PeriodType.LAST_30_DAYS} value={PeriodType.LAST_30_DAYS}>
-                            Siste 30 dager
-                        </option>
-                        <option key={PeriodType.THIS_YEAR} value={PeriodType.THIS_YEAR}>
-                            Inneværende år
-                        </option>
-                        <option key={PeriodType.LAST_YEAR} value={PeriodType.LAST_YEAR}>
-                            I fjor
-                        </option>
-                    </Select>
-                )}
-
+                <SelectPeriod onPeriodChange={periodChange} selectedPeriod={period} />
                 <SingleDatePicker
-                    date={selectedDateRange.from.toDate()}
+                    date={dateRange.from.toDate()}
                     label="Fra"
-                    onDateChange={(date) => debounceSetDate({ from: dayjs(date), to: selectedDateRange.to })}
+                    onDateChange={(date) => debounceSetDate({ from: dayjs(date), to: dateRange.to })}
                     onValidate={debouncedValidate}
-                    maxDate={selectedDateRange.to.toDate()}
+                    maxDate={dateRange.to.toDate()}
                 />
                 <SingleDatePicker
-                    date={selectedDateRange.to.toDate()}
+                    date={dateRange.to.toDate()}
                     label="Til"
-                    onDateChange={(date) => debounceSetDate({ from: selectedDateRange.from, to: dayjs(date) })}
+                    onDateChange={(date) => debounceSetDate({ from: dateRange.from, to: dayjs(date) })}
                     onValidate={debouncedValidate}
-                    minDate={selectedDateRange.from.toDate()}
+                    minDate={dateRange.from.toDate()}
                 />
             </HStack>
             {error && (
