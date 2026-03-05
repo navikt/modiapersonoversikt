e8fe437d2aa88f25814ac865ef185ef4c13740f9
Nummedal <sigrun.asheim.nummedal@nav.no>
2026-03-05 12:53:30 +0100

    fiks import

--- src/components/Dokumenter/DokmentEgetVindu/index.tsx
+++ src/components/Dokumenter/DokmentEgetVindu/index.tsx
@@ -2,7 +2,7 @@ import { HStack } from '@navikt/ds-react';
 import { getRouteApi } from '@tanstack/react-router';
 import { AlertStripeFeil } from 'nav-frontend-alertstriper';
 import DokumentVisning from 'src/components/Dokumenter/DokumentVisning';
-import { getSaksdokumentUrl } from 'src/components/saker/utils';
+import { getSaksdokumentUrl } from 'src/components/Dokumenter/utils';
 import { useOnMount } from 'src/utils/customHooks';
 
 const routeApi = getRouteApi('/new/dokument');
