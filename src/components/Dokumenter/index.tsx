8f79e95c47f38d863b51c25a4b57efa764b92bc7
Nummedal <sigrun.asheim.nummedal@nav.no>
2026-03-05 11:46:38 +0100

    Legg til infotekst på dokumentersiden

--- src/components/Dokumenter/index.tsx
+++ src/components/Dokumenter/index.tsx
@@ -1,4 +1,4 @@
-import { Heading, HStack, Skeleton, type SortState, VStack } from '@navikt/ds-react';
+import { Detail, Heading, Skeleton, type SortState, VStack } from '@navikt/ds-react';
 import { AlertBanner } from 'src/components/AlertBanner';
 import Card from 'src/components/Card';
 import { DokumenterTabell } from 'src/components/Dokumenter/DokumenterTabell';
@@ -23,11 +23,16 @@ export const DokumenterPage = () => {
             <Card padding="4" className="h-full overflow-auto">
                 <AlertBanner alerts={errorMessages} />
                 <VStack gap="8">
-                    <HStack align="center" gap="2">
+                    <VStack gap="2">
                         <Heading level="2" size="medium">
                             Dokumenter
                         </Heading>
-                    </HStack>
+                        <Detail className="text-ax-text-neutral-subtle" spacing={false}>
+                            Modia viser elektroniske dokumenter brukeren har sendt inn via nav.no etter 9. desember
+                            2014.
+                            <br /> Dokumenter som er journalført vises fra og med 4.juni 2016
+                        </Detail>
+                    </VStack>
                     <VStack gap="4">
                         <DokumenterFilter />
                         {isLoading ? (
