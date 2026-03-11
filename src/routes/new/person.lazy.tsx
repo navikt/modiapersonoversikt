1625e73f5f1a763b322855e717cf64d0c74ff20d
martinsomm-nav <martin.dagfinn.boler.sommerseth@nav.no>
2026-03-11 11:59:35 +0100

    Legger på tilgangssjekk ved uthenting av person i ny modia

--- src/routes/new/person.lazy.tsx
+++ src/routes/new/person.lazy.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, VStack } from '@navikt/ds-react';
+import { Alert, Box, HStack, Skeleton, VStack } from '@navikt/ds-react';
 import { createLazyFileRoute, Navigate, Outlet } from '@tanstack/react-router';
 import { useAtomValue } from 'jotai';
 import { Suspense } from 'react';
@@ -6,7 +6,9 @@ import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
 import { LukkbarNyMelding } from 'src/components/melding/LukkbarNyMelding';
 import { PersonLinje } from 'src/components/PersonLinje';
 import { PersonSidebarMenu } from 'src/components/PersonSidebar';
+import BegrensetTilgangBegrunnelse from 'src/components/person/BegrensetTilgangBegrunnelse';
 import { aktivBrukerAtom } from 'src/lib/state/context';
+import tilgangskontroll from 'src/rest/resources/tilgangskontrollResource';
 
 export const Route = createLazyFileRoute('/new/person')({
     component: PersonRoute
@@ -18,6 +20,28 @@ function PersonRoute() {
     if (!aktivBruker) {
         return <Navigate to="/" />;
     }
+
+    return <PersonRouteMedTilgang fnr={aktivBruker} />;
+}
+
+function PersonRouteMedTilgang({ fnr }: { fnr: string }) {
+    const tilgang = tilgangskontroll.useFetch(fnr);
+
+    if (tilgang.isPending) return <Skeleton variant="rectangle" height="4rem" />;
+    if (tilgang.isError)
+        return (
+            <div className="flex-1">
+                <Alert variant="error">Beklager. Det oppsto en feil ved sjekk av tilgang til bruker.</Alert>
+            </div>
+        );
+    if (!tilgang.data.harTilgang)
+        return (
+            <div className="flex-1">
+                <Alert variant="warning">
+                    <BegrensetTilgangBegrunnelse begrunnelseType={tilgang.data.ikkeTilgangArsak} />
+                </Alert>
+            </div>
+        );
 
     return <PersonLayout />;
 }
