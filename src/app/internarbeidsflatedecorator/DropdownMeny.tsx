92ae11939077b4fb76217d7d8537b201c7a28d22
martinsomm-nav <martin.dagfinn.boler.sommerseth@nav.no>
2026-03-16 10:07:28 +0100

    Fjernet ikoner og endret headere

--- src/app/internarbeidsflatedecorator/DropdownMeny.tsx
+++ src/app/internarbeidsflatedecorator/DropdownMeny.tsx
@@ -1,4 +1,4 @@
-import { BookIcon, ChatExclamationmarkIcon, SparklesIcon } from '@navikt/aksel-icons';
+import { SparklesIcon } from '@navikt/aksel-icons';
 import { ActionMenu, Button } from '@navikt/ds-react';
 import { NyModiaSwitch } from 'src/components/NyModia';
 
@@ -9,14 +9,14 @@ export const DropdownMeny = () => (
                 <Button className="py-[0.5rem]" data-color="neutral" variant="secondary" icon={<SparklesIcon />} />
             </ActionMenu.Trigger>
             <ActionMenu.Content>
-                <ActionMenu.Group label="Visningsalternativer">
+                <ActionMenu.Group label="Bytt versjon">
                     <NyModiaSwitch />
                 </ActionMenu.Group>
                 <ActionMenu.Divider />
                 <ActionMenu.Group label="Veiledning og tilbakemelding">
-                    <ActionMenu.Item icon={<SparklesIcon />}>Introduksjon</ActionMenu.Item>
-                    <ActionMenu.Item icon={<BookIcon />}>Brukerveiledning</ActionMenu.Item>
-                    <ActionMenu.Item icon={<ChatExclamationmarkIcon />}>Gi tilbakemelding</ActionMenu.Item>
+                    <ActionMenu.Item>Introduksjon</ActionMenu.Item>
+                    <ActionMenu.Item>Brukerveiledning</ActionMenu.Item>
+                    <ActionMenu.Item>Gi tilbakemelding</ActionMenu.Item>
                 </ActionMenu.Group>
             </ActionMenu.Content>
         </ActionMenu>
