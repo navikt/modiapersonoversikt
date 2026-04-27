9ac3e2aa5977802a2aa41a6b441893306df6342c
Nummedal <sigrun.asheim.nummedal@nav.no>
2026-04-27 15:36:40 +0200

    Lag client for contextholder

---
+++ src/lib/clients/contextholder.ts
@@ -0,0 +1,21 @@
+import { useMutation } from '@tanstack/react-query';
+import { type FetchError, post } from 'src/api/api';
+
+interface ContextResponse {
+    aktivBruker: string;
+    aktivEnhet: string;
+}
+
+function url(): string {
+    return `${import.meta.env.BASE_URL}proxy/modiacontextholder/api/context`;
+}
+
+export const useSetUserContext = () => {
+    return useMutation<ContextResponse, FetchError, { fnr: string }>({
+        mutationFn: ({ fnr }) =>
+            post(url(), {
+                eventType: 'NY_AKTIV_BRUKER',
+                verdi: fnr
+            })
+    });
+};
