231416ec01823bb7eac5a401d0ab6086e6b048f0
Nummedal <sigrun.asheim.nummedal@nav.no>
2026-05-18 15:38:56 +0200

    Generaliser personovernsinformasjon

--- src/routes/personvern.tsx
+++ src/routes/personvern.tsx
@@ -31,7 +31,7 @@ function PersonvernPage() {
     return (
         <Box height="100svh" overflow="auto">
             <InternalHeader>
-                <InternalHeader.Title>Modia personoversikt</InternalHeader.Title>
+                <InternalHeader.Title>Modia</InternalHeader.Title>
             </InternalHeader>
             <Box paddingBlock="space-48" paddingInline="space-16">
                 <VStack gap="space-32" className="max-w-3xl mx-auto">
@@ -40,7 +40,7 @@ function PersonvernPage() {
                     </Heading>
                     <Box padding="space-40" borderRadius="12">
                         <VStack gap="space-32">
-                            <Section title="Personvern og sikkerhet i Modia Personoversikt">
+                            <Section title="Personvern og sikkerhet i Modia">
                                 <BodyLong>
                                     Modia Personoversikt er en intern arbeidsflate for veiledere og saksbehandlere i
                                     NAV. Denne personvernerklæringen er knyttet til behandlingen av personopplysninger
@@ -50,7 +50,6 @@ function PersonvernPage() {
                                     .
                                 </BodyLong>
                             </Section>
-
                             <Section title="Bruk av informasjonskapsler (cookies)">
                                 <BodyLong>Når du besøker nettsiden bruker vi informasjonskapsler (cookies).</BodyLong>
                                 <BodyLong>
@@ -70,17 +69,16 @@ function PersonvernPage() {
                                     m.m. Slik kan vi forbedre flyten og opplevelsen for alle som bruker nettsiden.
                                 </BodyLong>
                                 <BodyLong>
-                                    Opplysningene brukes til å kartlegge hvordan og hvor mye Modia Personoversikt
-                                    brukes, uten å identifisere IP-adresser. Vi bruker verktøyet Umami og Lumi i
-                                    analysearbeidet.
+                                    Opplysningene brukes til å kartlegge hvordan og hvor mye Modia brukes, uten å
+                                    identifisere IP-adresser. Vi bruker verktøyet Umami og Skyra i analysearbeidet.
                                 </BodyLong>
-                                <SubSection title="Lumi">
-                                    Lumi benyttes for å hente inn tilbakemeldinger fra brukerne. Når du sender inn en
-                                    tilbakemelding, lagres opplysningene i Lumi for å hjelpe oss med å forbedre
+                                <SubSection title="Skyra">
+                                    Skyra benyttes for å hente inn tilbakemeldinger fra brukerne. Når du sender inn en
+                                    tilbakemelding, lagres opplysningene i Skyra for å hjelpe oss med å forbedre
                                     tjenesten. Opplysningene som samles inn inkluderer tilbakemeldingen du gir,
                                     tidspunktet for tilbakemeldingen, og en anonym ID som ikke kan knyttes til deg
                                     personlig. Vi bruker denne informasjonen for å forstå brukernes behov og forbedre
-                                    Modia Personoversikt basert på tilbakemeldingene vi mottar.
+                                    Modia basert på tilbakemeldingene vi mottar.
                                 </SubSection>
                                 <SubSection title="Umami">
                                     Umami brukes til statistikk og analyse av hvordan Modia Personoversikt brukes. Umami
