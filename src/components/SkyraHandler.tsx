a4359e5157989ee3508722f35ed9bbe1d5e7c63a
martinsomm-nav <martin.dagfinn.boler.sommerseth@nav.no>
2026-05-27 11:11:54 +0200

    Legger på debug-logging for testing

--- src/components/SkyraHandler.tsx
+++ src/components/SkyraHandler.tsx
@@ -1,8 +1,13 @@
+import { useLocation } from '@tanstack/react-router';
 import { useEffect } from 'react';
 
 export function SkyraHandler() {
+    const { pathname } = useLocation();
+
     useEffect(() => {
+        console.log('[Skyra] Route changed, triggering reload:', pathname);
         window.skyra?.reload?.();
-    }, []);
+    }, [pathname]);
+
     return null;
 }
