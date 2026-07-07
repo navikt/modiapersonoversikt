205343467bf5c2852b2a223b94457d839fbf5be2
martinsomm-nav <martin.dagfinn.boler.sommerseth@nav.no>
2026-07-07 15:02:35 +0200

    Legger på flex for nedskalering av bilde ved mye tekst

--- src/app/oppdateringslogg/EnkeltOppdateringslogg.tsx
+++ src/app/oppdateringslogg/EnkeltOppdateringslogg.tsx
@@ -14,7 +14,7 @@ function OppdateringsloggBilde({ src }: { src?: string }) {
 
 export default function EnkeltOppdateringslogg({ enOppdateringslogg }: Props) {
     return (
-        <VStack gap="space-8">
+        <VStack gap="space-8" className="h-full">
             <OppdateringsloggBilde src={enOppdateringslogg.src} />
             <Heading size="small" level="3">
                 {enOppdateringslogg.tittel}
